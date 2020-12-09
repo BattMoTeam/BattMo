f1a0379fec8edbee6c2bb8b471ce92f62c89aa27
Xavier Raynaud <xavier.raynaud@sintef.no>
2020-12-09 11:30:27 +0100

    fix error in genSubGrid

--- genSubGrid.m
+++ genSubGrid.m
@@ -1,8 +1,9 @@
 function G = genSubGrid(G0, cells)
-    nc = 1 : G0.cells.num;
+    nc = G0.cells.num;
     rcells = (1 : nc)';
     rcells(cells) = [];
-    [G, cellmap, facemap, nodemap] = removeCells(G0, cells);
+    [G, cellmap, facemap, nodemap] = removeCells(G0, rcells);
+    G = computeGeometry(G);
     mappings = struct('parentGrid', G0     , ...
                       'cellmap'   , cellmap, ...
                       'facemap'   , facemap, ...
