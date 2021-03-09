b417a27321b6b4e1b0fca2734874b1dc653101e4
Xavier Raynaud <xavier.raynaud@sintef.no>
2021-03-09 15:45:22 +0100

    added 3Dextra model (extension from 2D)

---
+++ BatteryModel3Dextra.m
@@ -0,0 +1,139 @@
+classdef BatteryModel3Dextra < BatteryModel
+
+    methods
+        
+        function model = BatteryModel3Dextra(varargin)
+            model = model@BatteryModel(varargin{:});
+        end
+
+        
+        
+        function model = setupBatteryComponents(model)
+            
+            sepnx  = 30;
+            nenx   = 30;
+            penx   = 30;
+            ccnenx = 20;
+            ccpenx = 20;
+
+            nelyte = nenx + sepnx + penx;
+            
+            nxs = [ccnenx; nenx; sepnx; penx; ccpenx];
+            ny = 10;
+            nz = 1;
+            
+            xlength = 1e-6*[10; 100; 50; 80; 10];
+            ylength = 1e-2;
+            zlength = 1e-2;
+            
+            x = xlength./nxs;
+            x = rldecode(x, nxs);
+            x = [0; cumsum(x)];
+
+            y = ylength/ny;
+            y = rldecode(y, ny);
+            y = [0; cumsum(y)];
+            
+            
+            z = zlength/nz;
+            z = rldecode(z, nz);
+            z = [0; cumsum(z)];
+
+            G = tensorGrid(x, y, z);
+            G = computeGeometry(G);
+            
+            nx = sum(nxs);
+
+            dimGlobGrid = [nx; ny; nz];
+
+            %% setup elyte
+
+            startSubGrid = [ccnenx + 1; 1; 1];
+            dimSubGrid   = [nelyte; ny; nz];
+            elytecells   = pickTensorCells3D(startSubGrid, dimSubGrid, dimGlobGrid);
+
+            %% setup ne
+
+            startSubGrid = [ccnenx + 1; 1; 1];
+            dimSubGrid   = [nenx; ny; nz];
+            necells      = pickTensorCells3D(startSubGrid, dimSubGrid, dimGlobGrid);
+
+            %% setup pe
+
+            startSubGrid = [ccnenx + nenx + sepnx + 1; 1; 1];
+            dimSubGrid   = [penx; ny; nz];
+            pecells      = pickTensorCells3D(startSubGrid, dimSubGrid, dimGlobGrid);
+            
+            %% setup sep
+
+            startSubGrid = [ccnenx + nenx + 1; 1;  1];
+            dimSubGrid   = [sepnx; ny; nz];
+            sepcells     = pickTensorCells3D(startSubGrid, dimSubGrid, dimGlobGrid);
+
+            %% setup ccne
+
+            startSubGrid = [1; 1; 1];
+            dimSubGrid   = [ccnenx; ny; nz];
+            ccnecells    = pickTensorCells3D(startSubGrid, dimSubGrid, dimGlobGrid);
+
+            %% setup ccpe
+
+            startSubGrid = [ccnenx + nelyte + 1; 1; 1];
+            dimSubGrid   = [ccpenx; ny; nz];
+            ccpecells    = pickTensorCells3D(startSubGrid, dimSubGrid, dimGlobGrid);
+
+            %% 
+
+            model.G = G;
+            
+            submodels = {};
+            submodels{end + 1} = orgLiPF6('elyte'       , G, elytecells);
+            submodels{end + 1} = graphiteElectrode('ne' , G, necells);
+            submodels{end + 1} = nmc111Electrode('pe'   , G, pecells);
+            submodels{end + 1} = currentCollector('ccne', G, ccnecells);
+            submodels{end + 1} = currentCollector('ccpe', G, ccpecells);
+            submodels{end + 1} = celgard2500('sep'      , G, sepcells);
+            model.SubModels = submodels;
+            
+        end
+        
+        function coupTerm = setupCcneBcCoupTerm(model)
+
+            ccne = model.ccne;
+            G = ccne.G;
+
+            % We pick up the faces at the top of ccne
+            yf = G.faces.centroids(:, 2);
+            myf = max(yf);
+            faces = find(yf >= (1 - eps)*myf);
+            cells = sum(G.faces.neighbors(faces, :), 2);
+
+            compnames = {'ccne'};
+            coupTerm = couplingTerm('bc-ccne', compnames);
+            coupTerm.couplingfaces = faces;
+            coupTerm.couplingcells = cells;
+
+        end
+
+        function coupTerm = setupCcpeBcCoupTerm(model)
+
+            ccpe = model.ccpe;
+            G = ccpe.G;
+
+            % We pick up the faces at the bottom of ccpe
+            yf = G.faces.centroids(:, 2);
+            myf = max(yf);
+            faces = find(yf >= (1 - eps)*myf);
+            cells = sum(G.faces.neighbors(faces, :), 2);
+
+            compnames = {'ccpe'};
+            coupTerm = couplingTerm('bc-ccpe', compnames);
+            coupTerm.couplingfaces = faces;
+            coupTerm.couplingcells = cells;
+
+        end
+
+        
+    end
+    
+end
