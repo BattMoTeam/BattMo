3a695887b6a30eddcf015d6b3c2b801d93e46f3a
Xavier Raynaud <xavier.raynaud@sintef.no>
2021-03-16 16:39:22 +0100

    minor clean-up

--- Examples/plot_all.m
+++ Examples/plot_all.m
@@ -1,17 +1,22 @@
-%%
-states_new={}
-for i=1:numel(states)
+%% script that plots all the submodels with the potential
+
+%  clean-up states structure
+states_new = {}
+for i = 1:numel(states)
     if(not(isempty(states{i})))
-        states_new{i}=states{i};
+        states_new{i} = states{i}; 
     end
 end
-states=states_new;
-%%
-%close all
-args={'field','am.phi'}
-figure(1),plotResultsSubModel(model, 'pe',states,args{:}), title('pe')
-figure(2),plotResultsSubModel(model, 'ne',states,args{:}), title('ne')
-argsel={'field','phi'}
-figure(3),plotResultsSubModel(model, 'elyte',states,argsel{:}), title('elyte')
-figure(4),plotResultsSubModel(model, 'ccpe',states,argsel{:}), title('ccpe')
-figure(5),plotResultsSubModel(model, 'ccne',states,argsel{:}), title('ccne')
+states = states_new; 
+
+%% 
+% close all
+
+args = {'field', 'am.phi'}
+figure(1), plotResultsSubModel(model, 'pe', states, args{:}), title('pe')
+figure(2), plotResultsSubModel(model, 'ne', states, args{:}), title('ne')
+
+argsel = {'field', 'phi'}
+figure(3), plotResultsSubModel(model, 'elyte', states, argsel{:}), title('elyte')
+figure(4), plotResultsSubModel(model, 'ccpe', states, argsel{:}), title('ccpe')
+figure(5), plotResultsSubModel(model, 'ccne', states, argsel{:}), title('ccne')
--- Utilities/plotResultsSubModel.m
+++ Utilities/plotResultsSubModel.m
@@ -1,4 +1,6 @@
- function plotSubModel(model, name,states, varargin)
-    %% should proably remove all non cell arrays
-    plotToolbar(model.(name).G,cellfun(@(x) x.(name),states,'UniformOutput',false),varargin{:})
- end
+function plotResultsSubModel(model, name, states, varargin)
+    %% should probably remove all non cell arrays
+
+    plotToolbar(model.(name).G, cellfun(@(x) x.(name), states, 'UniformOutput', false), varargin{:})
+
+end
