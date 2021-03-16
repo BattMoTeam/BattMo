e2d9ad63adfbdd8bdcd24b25fd3c4c289cecf58a
Xavier Raynaud <xavier.raynaud@sintef.no>
2021-03-16 17:35:44 +0100

    minor clean-up

--- Examples/plot_all.m
+++ Examples/plot_all.m
@@ -1,7 +1,7 @@
 %% script that plots all the submodels with the potential
 
 %  clean-up states structure
-states_new = {}
+states_new = {};
 for i = 1:numel(states)
     if(not(isempty(states{i})))
         states_new{i} = states{i}; 
@@ -12,11 +12,11 @@ states = states_new;
 %% 
 % close all
 
-args = {'field', 'am.phi'}
+args = {'field', 'am.phi'};
 figure(1), plotResultsSubModel(model, 'pe', states, args{:}), title('pe')
 figure(2), plotResultsSubModel(model, 'ne', states, args{:}), title('ne')
 
-argsel = {'field', 'phi'}
+argsel = {'field', 'phi'};
 figure(3), plotResultsSubModel(model, 'elyte', states, argsel{:}), title('elyte')
 figure(4), plotResultsSubModel(model, 'ccpe', states, argsel{:}), title('ccpe')
 figure(5), plotResultsSubModel(model, 'ccne', states, argsel{:}), title('ccne')
