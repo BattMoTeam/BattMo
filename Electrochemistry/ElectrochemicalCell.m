c70c993edf547b9834575a951df048230bac8933
Xavier Raynaud <xavier.raynaud@sintef.no>
2021-03-22 15:07:39 +0100

    adjusted function names

---
+++ Electrochemistry/ElectrochemicalCell.m
@@ -0,0 +1,31 @@
+classdef ElectrochemicalCell < CompositeModel
+    %ELECTRODE Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        SpecificEnergy
+        OpenCircuitVoltage
+        SpecificChargeCapacity
+        Volume
+        ThermalExpansionCoefficient
+        EnergyDensity
+        ThermalConductivity
+        Density
+        ChargeCapacity
+        ElectricImpedance
+        StoredEnergy
+        Mass
+        ElectrochemicalComponent
+        SpecificHeatCapacity
+        ElectricPotential
+        HeatCapacity
+        ThermodynamicTemperature
+        InternalResistance
+        InternalConductance
+    end
+    
+    methods
+
+    end
+end
+
