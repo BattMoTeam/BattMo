34ca642340ddfef7bf5e194b33492915f8aece91
Xavier Raynaud <xavier.raynaud@sintef.no>
2021-03-25 00:11:38 +0100

    testgraph can write to file

--- Examples/testgraph.m
+++ Examples/testgraph.m
@@ -6,11 +6,22 @@ mrstModule add ad-core multimodel mrst-gui battery
 mrstVerbose off
             
 model = Battery_();
+% model = CurrentCollector_('cc');
+% model = model.initiateCompositeModel();
 
 model.adminmodel.printPropfunctions;
 
 [g, edgelabels] = setupGraph(model);
 
 plot(g, 'interpreter', 'none');
+
+f = fopen('test.csv', 'w');
+edges = g.Edges;
+edges = edges.Variables;
+for ind = 1 : size(edges, 1)
+    fprintf(f, '%s;%s\n', edges{ind, 1}, edges{ind, 2});
+end
+fclose(f);
+
 
 
