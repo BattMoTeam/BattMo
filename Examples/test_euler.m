f7f89f94e823b68653e94eb5eb527c6ae9853ae6
Xavier Raynaud <xavier.raynaud@sintef.no>
2021-03-26 16:04:05 +0100

    removed call to ComponentModel

--- Examples/test_euler.m
+++ Examples/test_euler.m
@@ -2,7 +2,7 @@ clear all
 close all
 
 % setup mrst modules
-mrstModule add ad-core multimodel mrst-gui battery
+mrstModule add ad-core mrst-gui battery
 mrstVerbose off
 
 modelcase = '2D';
--- Materials/Solid/Polymers/celgard2500.m
+++ Materials/Solid/Polymers/celgard2500.m
@@ -1,4 +1,4 @@
-classdef celgard2500 < ComponentModel
+classdef celgard2500 < PhysicalModel
     %UNTITLED10 Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -19,21 +19,18 @@ classdef celgard2500 < ComponentModel
     methods
         function model = celgard2500(name, G, cells)
             
-            model = model@ComponentModel(name);
+            model = model@PhysicalModel(G);
             
             model.thickness      = 10e-6;
-            model.porosity   = 0.55;
-            model.volumeFraction    = 1 - model.porosity;
-            model.rp     = 0.064e-6 ./ 2;
-            model.Gurley = 200;
+            model.porosity       = 0.55;
+            model.volumeFraction = 1 - model.porosity;
+            model.rp             = 0.064e-6 ./ 2;
+            model.Gurley         = 200;
             
             model.G = genSubGrid(G, cells);
 
         end
-        
-        function state = initializeState(model, state)
-            warning('to be implemented');
-        end
+
 
     end
 end
