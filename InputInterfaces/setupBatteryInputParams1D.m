564423d673a05deeac4fcf5fd76b26d9bdb59394
Xavier Raynaud <xavier.raynaud@sintef.no>
2021-04-06 18:47:46 +0200

    some refactoring in 1d setup

--- InputInterfaces/setupBatteryInputParams1D.m
+++ InputInterfaces/setupBatteryInputParams1D.m
@@ -14,7 +14,7 @@ function paramobj = setupBatteryInputParams1D(paramobj)
     params.ccnenx = 20*fac;
     params.ccpenx = 20*fac;
     
-    paramobj = setupGrid(paramobj, params);
+    paramobj = setupBatteryGrid(paramobj, params);
     paramobj = setupNegativeElectrode(paramobj, params);
     paramobj = setupPositiveElectrode(paramobj, params);
     paramobj = setupElectrolyte(paramobj, params);
@@ -22,7 +22,7 @@ function paramobj = setupBatteryInputParams1D(paramobj)
     
 end
 
-function paramobj = setupGrid(paramobj, params)
+function paramobj = setupBatteryGrid(paramobj, params)
     
     sepnx  = params.sepnx;
     nenx   = params.nenx;
@@ -161,3 +161,82 @@ function coupTerm = setupPositiveElectrodeElectrolyteCoupTerm(paramobj, params)
     
 end
 
+
+function paramobj = setupElectrodeInputParams1D(paramobj, params)
+    
+% Abbreviations used here:
+% eac : ElectrodeActiveComponent
+% cc  : CurrentCollector
+% bc  : Boundary Condition
+    
+
+% params struct should contain valid fields for ComponentInputParameters,
+%
+% and the fields
+%
+% - eac.cellind
+% - cc.cellind
+% - cc.bc_cell
+% - cc.bc_face
+% - cc.eac_cell
+% - cc.eac_face
+% - eac.cc_cell
+% - eac.cc_face
+    
+    paramobj = paramobj.setupGrid(params);
+    paramobj = setupElectrodeActiveComponent(paramobj, params);
+    paramobj = setupCurrentCollector(paramobj, params);
+    paramobj = setupElectrodeCouplingTerms(paramobj, params);
+    
+end
+
+function paramobj = setupElectrodeActiveComponent(paramobj, params)
+    
+    params_eac.globG = paramobj.globG;
+    params_eac.cellind = params.eac.cellind;
+    
+    paramobj.eac = setupGrid(paramobj.eac, params_eac);
+    
+end
+
+function paramobj = setupCurrentCollector(paramobj, params)
+    
+    params_cc.globG = paramobj.globG;
+    params_cc.cellind = params.cc.cellind;
+    
+    paramobj.cc = setupGrid(paramobj.cc, params_cc);
+    
+end       
+
+function paramobj = setupElectrodeCouplingTerms(paramobj, params)
+    
+    coupterm = setupCurrentCollectorElectrodeActiveComponentCoupTerm(paramobj, params);
+    paramobj.couplingTerm = coupterm;
+    
+    coupterm = setupCurrentCollectorBcCoupTerm(paramobj, params);
+    paramobj.cc.couplingTerm = coupterm;
+    
+end
+
+function coupTerm = setupCurrentCollectorElectrodeActiveComponentCoupTerm(paramobj, params)
+% Abbreviations used in this function:
+% eac : ElectrodeActiveComponent
+% cc  : CurrentCollector
+    
+    compnames = {'CurrentCollector', 'ElectrodeActiveComponent'};
+    coupTerm = couplingTerm('CurrentCollector-ElectrodeActiveComponent', compnames);
+    coupTerm.couplingfaces = [params.cc.eac_face, params.eac.cc_face];
+    coupTerm.couplingcells = [params.cc.eac_cell, params.eac.cc_cell];
+    
+end
+
+function coupTerm = setupCurrentCollectorBcCoupTerm(paramobj, params)
+% Abbreviations used in this function:
+% cc   : CurrentCollector
+    
+    compnames = {'CurrentCollector'};
+    coupTerm = couplingTerm('bc-CurrentCollector', compnames);
+    coupTerm.couplingfaces = params.cc.bc_face;
+    coupTerm.couplingcells = params.cc.bc_cell;
+    
+end
