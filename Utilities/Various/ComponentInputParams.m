c087c46f836473313dd7088e48d1ae42fe6dfcdc
Xavier Raynaud <xavier.raynaud@sintef.no>
2021-04-07 19:33:35 +0200

    refactoring for grid setup

--- Utilities/Various/ComponentInputParams.m
+++ Utilities/Various/ComponentInputParams.m
@@ -1,33 +1,5 @@
 classdef ComponentInputParams 
-    
     properties
-        
-        %% Global grid
-        globG
-        
-        %% Cell index of current grid (indexing from global grid)
-        cellind
-        
-        %% current Grid (contains also the mappings)
-        G
-                
+        G % grid
     end
-    
-    methods
-
-        
-        function paramobj = setupGrid(paramobj, params)
-            
-            globG = params.globG;
-            cellind = params.cellind;
-            
-            paramobj.globG = globG;
-            paramobj.cellind = cellind;
-            paramobj.G = genSubGrid(globG, cellind);
-        
-        end
-        
-        
-    end
-    
 end
