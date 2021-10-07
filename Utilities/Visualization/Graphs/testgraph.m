b203d1a81ca061ec789f904195cb5149a65e4d12
Xavier Raynaud <xav.raynaud@gmail.com>
2021-10-07 13:56:07 +0200

    updates in testgraph

--- Utilities/Visualization/Graphs/testgraph.m
+++ Utilities/Visualization/Graphs/testgraph.m
@@ -2,7 +2,7 @@ clear all
 close all
 
 % setup mrst modules
-mrstModule add ad-core multimodel mrst-gui battery bgl
+mrstModule add ad-core multimodel mrst-gui battery 
 mrstVerbose off
 
 set(0, 'DefaultAxesFontSize', 16);
@@ -11,34 +11,45 @@ set(0, 'DefaultFigurePosition', [278 109 1570 822]);
 
 gstyle = {'interpreter', 'none', 'LineWidth', 3, 'ArrowSize', 20, 'NodeFontSize', 14};
 
+%% Choose the model you want to use below by uncommenting it and commenting the others.
+
 % model = ActiveMaterial_('am');
 % model = ElectrodeActiveComponent_('eac');
 % model = ElectronicComponent_('eac');
 % model = ElectroChemicalComponent_('eac');
 % model = CurrentCollector_('cc');
-% model = Electrolyte_('cc');
-model = orgLiPF6_('cc');
+model = Electrolyte_('elyte');
+% model = orgLiPF6_('cc');
 % model = Electrode_('pn');
-% model = Electrolyte_('elyte');
 % model = Battery_();
 
+% setup the model
 model = model.initiateCompositeModel();
-model.adminmodel.printPropfunctions;
 
-% NB : require matlab_bgl module (must be installed separately)
-% setupOrderedGraph(model, 'orderedfunctions.m');
-
+% plot the computational graph
 [g, edgelabels] = setupGraph(model);
-
 plot(g, gstyle{:});
 
-f = fopen('test.sif', 'w');
-edges = g.Edges;
-edges = edges.Variables;
-for ind = 1 : size(edges, 1)
-    fprintf(f, '%s x %s\n', edges{ind, 1}, edges{ind, 2});
+% print the functions with input and output arguments
+model.adminmodel.printPropfunctions;
+
+% print the functions in call order (such as all the computational graph is spanned)
+% This require external routines that can be found in the MATLAB BGL code base (see https://se.mathworks.com/matlabcentral/fileexchange/10922-matlabbgl)
+doorder = false;
+try 
+    mrstModule add bgl
+    doorder = true;
+catch
+    fprintf('To order the graph function you need to install matlab bgl module.\n see https://se.mathworks.com/matlabcentral/fileexchange/10922-matlabbgl');
 end
-fclose(f);
+if doorder
+    fprintf('\n\n\n**** Functions ordered by argument dependancy:\n\n');
+    setupOrderedFunctions(model);
+end
+
+
+
+
 
 
 
