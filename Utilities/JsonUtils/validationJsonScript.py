1f56ff9dd08015be81989db4bf392c3e7f53b15e
Xavier Raynaud <xav.raynaud@gmail.com>
2021-10-14 14:22:16 +0200

    added schemas

--- Utilities/JsonUtils/validationJsonScript.py
+++ Utilities/JsonUtils/validationJsonScript.py
@@ -5,32 +5,43 @@ import jsonschema
 from pathlib import Path
 import resolveFileInputJson as rjson
 
+
 def batmoDir():
     return Path("/home/xavier/Matlab/Projects/project-batman/")
 
-schema_folder = batmoDir() / Path('Materials/Liquid/CarbonateBased/')
-schema_filename = schema_folder / 'binaryelectrolyte.schema.json'
+
+schema_folder = batmoDir() / Path('JsonSchemas')
+schema_filename = schema_folder / 'electrolyte.schema.json'
 
 base_uri = 'file://batmo/schemas/'
 
 with open(schema_filename) as schema_file:
-    schema = json.load(schema_file)
+    mainschema = json.load(schema_file)
 
 # example
-jsoninput = rjson.loadJsonBatmo('Materials/Liquid/CarbonateBased/orgLiPF6.json')
+jsoninput = rjson.loadJsonBatmo('Battery/lithiumbattery.json')
 
 resolver = jsonschema.RefResolver(base_uri=base_uri, referrer=schema)
 
-schema_filename = schema_folder / 'electrolyte.schema.json'
-with open(schema_filename) as schema_file:
-    refschema = json.load(schema_file)
-resolver.store["file://batmo/schemas/electrolyte"] = refschema
 
-schema_filename = schema_folder / 'separator.schema.json'
-with open(schema_filename) as schema_file:
-    refschema = json.load(schema_file)
-resolver.store["file://batmo/schemas/separator"] = refschema
+def addJsonSchema(jsonschemaName):
+    jsonchemaFilename = jsonschemaName + '.schema.json'
+    schema_filename = schema_folder / jsonchemaFilename
+    with open(schema_filename) as schema_file:
+        refschema = json.load(schema_file)
+    key = "file://batmo/schemas/" + jsonschemaName
+    resolver.store[key] = refschema
 
-v = jsonschema.Draft7Validator(schema, resolver=resolver)
+
+schemaList = ["activematerial", "battery", "binaryelectrolyte",
+              "currentcollector", "electrodeactivecomponent",
+              "electrode", "electrolyte", "separator", "thermalmodel"]
+for schema in schemaList:
+    try:
+        addJsonSchema(schema)
+    except:
+        print(schema)
+
+v = jsonschema.Draft7Validator(mainschema, resolver=resolver)
 
 v.is_valid(jsoninput)
