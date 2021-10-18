9a188113510f87e923124d5ce7a33f76a7b95a82
Xavier Raynaud <xavier.raynaud@sintef.no>
2021-10-18 12:49:19 +0200

    starting with chen json files

---
+++ Materials/Liquid/CarbonateBased/updateElectrolyteConductivityFunc_nyman2008.m
@@ -0,0 +1,5 @@
+function conductivity = updateElectrolyteConductivityFunc_nyman2008(c, T)
+    
+    conductivity = 0.1297 * (c / 1000) ** 3 - 2.51 * (c / 1000) ** 1.5 + 3.329 * (c / 1000);
+    
+end
