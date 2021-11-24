8135e83321a1408d049343110fb4b8d09e9d428d
hnil <Halvor.M.Nilsen@sintef.no>
2021-11-24 08:50:11 +0100

    added utility for making timesteps

---
+++ Utilities/getTimeSteps.m
@@ -0,0 +1,10 @@
+function times = getTimeSteps(dt0, n, total, fac)
+dt=[];
+dt1 = total/n;
+dt0_org = dt0
+nstart= ceil(-log(dt0_org)/log(fac));
+dt = [dt; repmat(dt1, nstart, 1).*fac.^[1:nstart]'/fac^nstart];
+% Time scaling can be adding using variable tfac
+dt = [dt; repmat(dt1, n, 1)]; 
+times = [0; cumsum(dt)];
+end
