03bf71e94fb79b9873e53716a84b4e0f457e3b47
Xavier Raynaud <xavier.raynaud@sintef.no>
2022-02-25 09:22:43 +0100

    fixes for chen

--- Materials/Solid/MetalOxides/updateOCPFunc_nmc111_Chen.m
+++ Materials/Solid/MetalOxides/updateOCPFunc_nmc111_Chen.m
@@ -1,4 +1,4 @@
-function OCP = updateOCPFunc_nmc111_Chen(c, T, cmax)
+function [OCP, dUdT] = updateOCPFunc_nmc111_Chen(c, T, cmax)
 
     % LG M50 NMC open circuit potential as a function of stochiometry, fit taken
     % from [1].
@@ -10,6 +10,7 @@ function OCP = updateOCPFunc_nmc111_Chen(c, T, cmax)
 
     sto = c./cmax;
     OCP = -0.8090*sto + 4.4875 - 0.0428*tanh(18.5138*(sto - 0.5542)) - 17.7326*tanh(15.7890*(sto - 0.3117)) + 17.5842*tanh(15.9308*(sto - 0.3120));
+    dUdT = 0;
     
 end
 
--- Materials/Solid/ReactiveNonMetals/updateOCPFunc_graphite_Chen.m
+++ Materials/Solid/ReactiveNonMetals/updateOCPFunc_graphite_Chen.m
@@ -1,4 +1,4 @@
-function OCP = updateOCPFunc_graphite_Chen(c, T, cmax)
+function [OCP, dUdT] = updateOCPFunc_graphite_Chen(c, T, cmax)
     % LG M50 graphite open circuit potential as a function of stochiometry, fit taken
     % from [1].
 
@@ -11,6 +11,8 @@ function OCP = updateOCPFunc_graphite_Chen(c, T, cmax)
 
     sto = c./cmax;
     OCP = 1.9793*exp(-39.3631*sto) + 0.2482 - 0.0909*tanh(29.8538*(sto - 0.1234)) - 0.04478*tanh(14.9159*(sto - 0.2769)) - 0.0205*tanh(30.4444*(sto - 0.6103));
+    
+    dUdT = 0;
     
 end
 
