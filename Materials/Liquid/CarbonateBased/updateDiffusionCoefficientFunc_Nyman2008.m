1e0fd1445ce4b7a13db83feb05dd7fdefde283dd
Xavier Raynaud <xavier.raynaud@sintef.no>
2022-03-01 17:14:07 +0100

    fixed function name

--- Materials/Liquid/CarbonateBased/updateDiffusionCoefficientFunc_Nyman2008.m
+++ Materials/Liquid/CarbonateBased/updateDiffusionCoefficientFunc_Nyman2008.m
@@ -1,7 +1,7 @@
-function D = updateDiffusionCoefficientFunc_nyman2008(c, T)
+function D = updateDiffusionCoefficientFunc_Nyman2008(c, T)
     
     D = 8.794e-11*(c/1000).^2 - 3.972e-10*(c/1000) + 4.862e-10;
-    
+
 end
 
 
