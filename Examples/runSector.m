f730561ef3db1253097ebd450ae1b57938fcccf4
hnil <Halvor.M.Nilsen@sintef.no>
2022-03-10 09:41:50 +0100

    modifications to runSector

--- Examples/runSector.m
+++ Examples/runSector.m
@@ -197,7 +197,18 @@ simulatePackedProblem(problem);
 [globvars, states, report] = getPackedSimulatorOutput(problem);
 
 
+%% plot thermal
+plotToolbar(model.G,states),view([0,-1,0])
+%%
+%%  Process output and recover the output voltage and current from the output states.
+ind = cellfun(@(x) not(isempty(x)), states); 
+states = states(ind);
+Enew = cellfun(@(x) x.(pe).(cc).E, states); 
+Inew = cellfun(@(x) x.(pe).(cc).I, states);
+time = cellfun(@(x) x.time, states); 
 
+%% Plot an animated summary of the results
+plotDashboard(model, states, 'step', 0);
 
 
 
