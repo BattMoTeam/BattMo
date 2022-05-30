18d9738764c52867e246b719d3fa7f7cab0ad50b
Xavier Raynaud <xav.raynaud@gmail.com>
2022-05-30 09:54:26 +0200

    added function to get graph selected by name

---
+++ Utilities/ComputationGraphUtils/getNodeDependencyListByName.m
@@ -0,0 +1,7 @@
+function nodeDependencyList = getNodeDependencyListByName(regstr, nodenames, A)
+    nodeList = regexp(nodenames, regstr, 'once');
+    nodeList = cellfun(@(x) ~isempty(x), nodeList);
+    nodeList = find(nodeList);
+    nodeDependencyList = getNodeDependencyList(nodeList, A);
+end
+
