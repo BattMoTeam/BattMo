b69d4c884f83c8c851b66fac25a2a9f34475c540
August Johansson <august.johansson@sintef.no>
2022-06-17 10:18:24 +0200

    clean up

--- Examples/runCR2016.m
+++ Examples/runCR2016.m
@@ -77,7 +77,7 @@ gen = CoinCellBatteryGenerator();
 % Now, we update the paramobj with the properties of the mesh.
 paramobj = gen.updateBatteryInputParams(paramobj, params);
 
-%%  Initialize the battery model. 
+%%  Initialize the battery model.
 % The battery model is initialized by sending paramobj to the Battery class
 % constructor. see :class:`Battery <Battery.Battery>`.
 model = Battery(paramobj);
@@ -85,7 +85,7 @@ model = Battery(paramobj);
 %% Compute the nominal cell capacity and choose a C-Rate
 % The nominal capacity of the cell is calculated from the active materials.
 % This value is then combined with the user-defined C-Rate to set the cell
-% operational current. 
+% operational current.
 C     = computeCellCapacity(model);
 CRate = 1;
 
@@ -96,25 +96,25 @@ fprintf('Li content %f g\n', masses.(ne).(am).val*1000);
 
 
 
-%% Setup the time step schedule 
+%% Setup the time step schedule
 % Smaller time steps are used to ramp up the current from zero to its
 % operational value. Larger time steps are then used for the normal
-% operation. 
+% operation.
 n         = 24 / 4;
 dt        = [];
 dt        = [dt; repmat(0.5e-4, n, 1).*1.5.^[1 : n]'];
 totalTime = 60*second /CRate; %1.4*hour/CRate;
-n         = 20 %40; 
-dt        = [dt; repmat(totalTime/n, n, 1)]; 
-times     = [0; cumsum(dt)]; 
-tt        = times(2 : end); 
-step      = struct('val', diff(times), 'control', ones(numel(tt), 1)); 
+n         = 20 %40;
+dt        = [dt; repmat(totalTime/n, n, 1)];
+times     = [0; cumsum(dt)];
+tt        = times(2 : end);
+step      = struct('val', diff(times), 'control', ones(numel(tt), 1));
 
 %% Setup the operating limits for the cell
 % The maximum and minimum voltage limits for the cell are defined using
 % stopping and source functions. A stopping function is used to set the
 % lower voltage cutoff limit. A source function is used to set the upper
-% voltage cutoff limit. 
+% voltage cutoff limit.
 tup = 0.1; % rampup value for the current function, see rampupSwitchControl
 srcfunc = @(time, I, E) rampupSwitchControl(time, tup, I, E, ...
                                             model.Control.Imax, ...
@@ -123,32 +123,32 @@ srcfunc = @(time, I, E) rampupSwitchControl(time, tup, I, E, ...
 control = struct('src', srcfunc, 'IEswitch', true);
 
 % This control is used to set up the schedule
-schedule = struct('control', control, 'step', step); 
+schedule = struct('control', control, 'step', step);
 
 %% Setup the initial state of the model
 % The initial state of the model is dispatched using the
-% model.setupInitialState()method. 
-initstate = model.setupInitialState(); 
+% model.setupInitialState()method.
+initstate = model.setupInitialState();
 
-%% Setup the properties of the nonlinear solver 
-nls = NonLinearSolver(); 
-nls.maxIterations = 10; 
-nls.errorOnFailure = false; 
+%% Setup the properties of the nonlinear solver
+nls = NonLinearSolver();
+nls.maxIterations = 10;
+nls.errorOnFailure = false;
 nls.timeStepSelector = StateChangeTimeStepSelector('TargetProps', ...
                                                   {{ctrl, 'E'}}, ...
                                                    'targetChangeAbs', 0.03);
 
 mrstModule add linearsolvers
 %nls.LinearSolver = AMGCLSolverAD('verbose', true, 'reduceToCell', false);
-%nls.LinearSolver = AGMGSolverAD('verbose', true, 'reduceToCell', true); 
-% nls.LinearSolver.tolerance = 1e-3; 
-% nls.LinearSolver.maxIterations = 30; 
-nls.maxIterations = 10; 
+%nls.LinearSolver = AGMGSolverAD('verbose', true, 'reduceToCell', true);
+% nls.LinearSolver.tolerance = 1e-3;
+% nls.LinearSolver.maxIterations = 30;
+nls.maxIterations = 10;
 nls.verbose = 10;
 
 
 
-model.nonlinearTolerance = 1e-5; 
+model.nonlinearTolerance = 1e-5;
 model.verbose = true;
 
 %% Plot
@@ -192,13 +192,13 @@ plotGrid(model.(elyte).G,       'facecolor', colors(6,:), 'edgealpha', 0.5, 'edg
 
 % %% Run simulation
 % mrstVerbose off
-% [wellSols, states, report] = simulateScheduleAD(initstate, model, schedule, 'OutputMinisteps', true, 'NonLinearSolver', nls); 
+% [wellSols, states, report] = simulateScheduleAD(initstate, model, schedule, 'OutputMinisteps', true, 'NonLinearSolver', nls);
 
 
 name = sprintf('cr2016-%s', tag);
 dataFolder = sprintf('BattMo_%s', date);
 problem = packSimulationProblem(initstate, model, schedule, dataFolder, 'Name', name, 'NonLinearSolver', nls);
-problem.SimulatorSetup.OutputMinisteps = true; 
+problem.SimulatorSetup.OutputMinisteps = true;
 
 resetSimulation = true;
 if resetSimulation
@@ -210,11 +210,11 @@ simulatePackedProblem(problem);
 
 
 %%  Process output and recover the output voltage and current from the output states.
-ind = cellfun(@(x) not(isempty(x)), states); 
+ind = cellfun(@(x) not(isempty(x)), states);
 states = states(ind);
-Enew = cellfun(@(x) x.(ctrl).E, states); 
+Enew = cellfun(@(x) x.(ctrl).E, states);
 Inew = cellfun(@(x) x.(ctrl).I, states);
-time = cellfun(@(x) x.time, states); 
+time = cellfun(@(x) x.time, states);
 
 %% Plot an animated summary of the results
 %plotDashboard(model, states, 'step', 0);
