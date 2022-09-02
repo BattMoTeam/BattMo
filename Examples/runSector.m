04696a0fcddb9525ca144e8c828be600f9310e6b
Xavier Raynaud <xav.raynaud@gmail.com>
2022-09-02 13:09:14 +0200

    fixes in runSector example

--- Examples/runSector.m
+++ Examples/runSector.m
@@ -193,21 +193,29 @@ problem.SimulatorSetup.OutputMinisteps = true;
 resetSimulation = true;
 if resetSimulation
     %% clear previously computed simulation
-    clearPackedSimulatorOutput(problem);
+    clearPackedSimulatorOutput(problem, 'prompt', false);
 end
 simulatePackedProblem(problem);
 [globvars, states, report] = getPackedSimulatorOutput(problem);
 
 
 %% plot thermal
-plotToolbar(model.G,states),view([0,-1,0])
-%%
+
+figure
+plotToolbar(model.G, states);
+view([0,-1,0]);
+
 %%  Process output and recover the output voltage and current from the output states.
+
 ind = cellfun(@(x) not(isempty(x)), states); 
 states = states(ind);
-E = cellfun(@(x) x.(pe).(cc).E, states); 
-I = cellfun(@(x) x.(pe).(cc).I, states);
+ctrl = 'Control';
+E = cellfun(@(x) x.(ctrl).E, states); 
+I = cellfun(@(x) x.(ctrl).I, states);
 time = cellfun(@(x) x.time, states); 
+
+figure
+plot(time, E);
 
 %% Plot an animated summary of the results
 %plotDashboard(model, states, 'step', 0);
