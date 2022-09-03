30a48702ff1f8cbef32d63e400138bf39b3b7dfd
August Johansson <august.johansson@sintef.no>
2022-09-03 22:55:35 +0200

    LFP example from Andreas. Data from Xu 2015 (?)
    Initial conditions are not tweaked, but match Andreas first posted
    results (Aug 29, 11:18).

---
+++ ParameterData/MaterialProperties/OrganicLiPF6Solutions/updateDiffusionCoefficientFunc_Xu.m
@@ -0,0 +1,36 @@
+function D = updateDiffusionCoefficientFunc_Xu(c, T)
+    
+    % Calculate diffusion coefficients constant for the diffusion coefficient calculation
+    cnst = [ -4.43, -54; 
+             -0.22, 0.0 ];
+
+    Tgi = [ 229; 5.0 ];
+    
+    % Diffusion coefficient, [m^2 s^-1]
+    %Removed 10⁻⁴ otherwise the same
+    D = 10 .^ ( ( cnst(1,1) + cnst(1,2) ./ ( T - Tgi(1) - Tgi(2) .* c .* 1e-3) + cnst(2,1) .* ...
+                          c .* 1e-3) );
+    
+end
+
+
+
+%{
+Copyright 2021-2022 SINTEF Industry, Sustainable Energy Technology
+and SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The Battery Modeling Toolbox BattMo
+
+BattMo is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BattMo is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BattMo.  If not, see <http://www.gnu.org/licenses/>.
+%}
