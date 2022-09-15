ac41fe2950734ea82891b232bd20670fe2cb9364
Xavier Raynaud <xav.raynaud@gmail.com>
2022-09-15 16:50:57 +0200

    added comment and method to print propfunction in ComputationalGraphFilter

--- Utilities/ComputationGraphUtils/ComputationalGraphFilter.m
+++ Utilities/ComputationGraphUtils/ComputationalGraphFilter.m
@@ -15,14 +15,20 @@ classdef ComputationalGraphFilter
             if isempty(model.propertyFunctionList)
                 model = model.registerVarAndPropfuncNames();
             end
-            cgf.model     = model;
-            g             = setupGraph(model);
-            cgf.graph     = g;
+            cgf.model = model;
+            g = setupGraph(model);
+            cgf.graph = g;
+            % In adjacency matrix A, 
+            % - column index      : output variable index (as in model.varNameList)
+            % - row index         : input variable (as in model.varNameList)
+            % - coefficient value : property function index as in model.propertyFunctionList
             cgf.A         = adjacency(g, 'weighted');
             cgf.nodenames = g.Nodes.Variables;
+
+            
         end
 
-        function propfuncs = findPropFunction(cgf, nodename)
+        function [propfuncs, nodenames] = findPropFunction(cgf, nodename)
             
             nodenames = cgf.nodenames;
             A         = cgf.A;
@@ -34,10 +40,35 @@ classdef ComputationalGraphFilter
             indPropfunctions = unique(indPropfunctions(:));
 
             propfuncs = model.propertyFunctionList(indPropfunctions(indPropfunctions > 0));
+
+            if numel(propfuncs) == 1
+                propfuncs = propfuncs{1};
+            end
+            
+        end
+
+        function printPropFunction(cgf, nodename)
+            propfuncs = cgf.findPropFunction(nodename);
+
+            if numel(propfuncs) == 1
+                propfuncs = {propfuncs};
+            end
+
+            for iprop = 1 : numel(propfuncs)
+                propfunc = propfuncs{iprop};
+                varname = propfunc.varname;
+                varname_s = varname.resolveIndex();
+                for ind = 1 : numel(varname_s)
+                    fullname = varname_s{ind}.getIndexedFieldname();
+                    fprintf('%s : ', fullname);
+                end
+                fprintf('%s\n', propfuncs{iprop}.getFunctionSignature());
+            end
+            
             
         end
         
-        function [g, edgelabels] = setupGraph(cgf, varargin)
+        function [g, edgelabels] = getFilteredGraph(cgf, varargin)
             
             opt = struct('type', 'ascendant', ...
                          'oneParentOnly', false);
@@ -52,19 +83,19 @@ classdef ComputationalGraphFilter
             if strcmp(opt.type, 'descendant')
                 A = A';
             end
-                        
+            
             if ~isempty(includeNodeNames)
-            
-            nodes = getNodeDependencyListByName(includeNodeNames, nodenames, A, 'oneParentOnly', opt.oneParentOnly);
+                
+                nodes = getNodeDependencyListByName(includeNodeNames, nodenames, A, 'oneParentOnly', opt.oneParentOnly);
 
-            nodenames = nodenames(nodes);
-            A = A(nodes, nodes);
-            
-            if ~isempty(excludeNodeNames)
-                removenodes = regexp(nodenames, excludeNodeNames, 'once');
-                removenodes = cellfun(@(x) ~isempty(x), removenodes);
-                nodenames = nodenames(~removenodes);
-                A = A(~removenodes, ~removenodes);
+                nodenames = nodenames(nodes);
+                A = A(nodes, nodes);
+                
+                if ~isempty(excludeNodeNames)
+                    removenodes = regexp(nodenames, excludeNodeNames, 'once');
+                    removenodes = cellfun(@(x) ~isempty(x), removenodes);
+                    nodenames = nodenames(~removenodes);
+                    A = A(~removenodes, ~removenodes);
                 end
 
             end
