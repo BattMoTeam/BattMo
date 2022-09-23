4413c58c1e15448d16468be33b0ff988b6ab8af6
Xavier Raynaud <xav.raynaud@gmail.com>
2022-09-23 13:40:06 +0200

    fix in mergeParameters

--- Utilities/mergeParameters.m
+++ Utilities/mergeParameters.m
@@ -6,7 +6,7 @@ function paramobj = mergeParameters(paramobj, paramname1, paramname2)
     if isempty(val1) && ~isempty(val2)
         paramobj = paramobj.setParam(paramname1, val2);
     elseif ~isempty(val1) && isempty(val2)
-        paramobj = paramobj.setParam(paramname1, val1);
+        paramobj = paramobj.setParam(paramname2, val1);
     elseif ~isempty(val1) && ~isempty(val2)
         %% TODO : add more precise error message that indicates which parameters are not consistent
         assert(val1 == val2, 'inputs are not consistent')
