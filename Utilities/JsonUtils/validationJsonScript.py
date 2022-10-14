292f8a149592e6bf44225d0bec7b4350e27a056c
Xavier Raynaud <xav.raynaud@gmail.com>
2022-10-14 11:59:33 +0200

    update in python validation script

--- Utilities/JsonUtils/validationJsonScript.py
+++ Utilities/JsonUtils/validationJsonScript.py
@@ -7,21 +7,14 @@ import resolveFileInputJson as rjson
 
 
 def batmoDir():
-    return Path("/home/xavier/Matlab/Projects/project-batman/")
+    return Path("/home/xavier/Matlab/Projects/battmo/")
 
 
-schema_folder = batmoDir() / Path('JsonSchemas')
-schema_filename = schema_folder / 'battery.schema.json'
+schema_folder = batmoDir() / Path('Utilities') / Path('JsonSchemas')
 
 base_uri = 'file://batmo/schemas/'
 
-with open(schema_filename) as schema_file:
-    mainschema = json.load(schema_file)
-
-# example
-jsoninput = rjson.loadJsonBatmo('Battery/lithiumbattery.json')
-
-resolver = jsonschema.RefResolver(base_uri=base_uri, referrer=schema)
+resolver = jsonschema.RefResolver(base_uri=base_uri, referrer={})
 
 
 def addJsonSchema(jsonschemaName):
@@ -33,12 +26,27 @@ def addJsonSchema(jsonschemaName):
     resolver.store[key] = refschema
 
 
-schemaList = ["activematerial", "battery", "binaryelectrolyte",
-              "currentcollector", "electrodeactivecomponent",
-              "electrode", "electrolyte", "separator", "thermalmodel"]
+schemaList = ["activematerial",
+              "battery",
+              "binaryelectrolyte",
+              "currentcollector",
+              "soliddiffusion",
+              "electrode",
+              "electrolyte",
+              "separator",
+              "thermalmodel"]
+
 for schema in schemaList:
     addJsonSchema(schema)
 
+# We validate the battery schema
+schema_filename = schema_folder / 'battery.schema.json'
+with open(schema_filename) as schema_file:
+    mainschema = json.load(schema_file)
+
 v = jsonschema.Draft7Validator(mainschema, resolver=resolver)
+
+
+jsoninput = rjson.loadJsonBatmo('Battery/lithiumbattery.json')
 
 v.is_valid(jsoninput)
