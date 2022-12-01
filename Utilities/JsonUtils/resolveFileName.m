a0fb2e562e0c0497f873f15cf486e38ba9cc1a38
Xavier Raynaud <xavier.raynaud@sintef.no>
2022-12-01 14:03:56 +0100

    minor improvement in resolveFileName

--- Utilities/JsonUtils/resolveFileName.m
+++ Utilities/JsonUtils/resolveFileName.m
@@ -3,7 +3,7 @@ function fullfilename = resolveFileName(filename)
 % If the filename is not an absolute path (starts with "/") or explicitly refer to the current directory (starts with
 % "./"), then use battmo directory path.
     
-    if strcmp(filename(1), '/') | strcmp(filename(1:2), './')
+    if strcmp(filename(1), '/') | strcmp(filename(1:2), './') |  strcmp(filename(1:3), '../')
         fullfilename = filename; 
     else
         fullfilename = fullfile(battmoDir(), filename);
