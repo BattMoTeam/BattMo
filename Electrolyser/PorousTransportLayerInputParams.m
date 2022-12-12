06e9802cdd2f99d68fbacb2c12a924f83d805656
Xavier Raynaud <xavier.raynaud@sintef.no>
2022-12-12 09:47:41 +0100

    changed name

---
+++ Electrolyser/PorousTransportLayerInputParams.m
@@ -0,0 +1,36 @@
+classdef AlkalineElectrodeInputParams < ElectronicComponentInputParams
+    
+    properties
+        
+        solidVolumeFraction % Solid volume fraction
+        leverettCoefficient % Leverett coefficient that enters in the computation of the capillary pressure
+        theta % Water contact angle, enters in the computation of the capillary pressure
+        permeability % Permeability [Darcy]
+        BruggemanCoefficient % Bruggeman coefficient
+        sp % species struct 
+        % sp.OH.MW    : Molecular weight [kg mol^-1]
+        % sp.OH.V0    : Partial molar volume
+        % sp.OH.D     : Diffustion coefficient
+        % sp.OH.t     : Transference coefficient 
+        % sp.OH.z     : Charge number
+        % sp.K.MW
+        % sp.K.V0
+        % sp.H2O.MW   : 
+        % sp.H2O.beta : Water equilibrium constant
+        % sp.H2O.kLV  : liquid-vapor exchange rate
+        % sp.H2O.mu0  : Standard chemical potential
+        
+        
+    end
+    
+    methods
+        
+        function paramobj = HydrogenElectrodeInputParams();
+            paramobj = paramobj@ElectronicComponentInputParams();
+            
+        end
+        
+    end
+    
+    
+end
