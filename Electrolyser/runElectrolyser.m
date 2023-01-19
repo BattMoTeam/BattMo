84a60ca3891ebcea43a9d9b8dcb84899c7510b39
Xavier Raynaud <xav.raynaud@gmail.com>
2023-01-19 09:49:15 +0100

    added script for electrolyser

---
+++ Electrolyser/runElectrolyser.m
@@ -0,0 +1,32 @@
+mrstModule add ad-core mpfa
+
+
+jsonstring = fileread('/home/xavier/Matlab/Projects/battmo/Electrolyser/Parameters/alkalineElectrolyser.json');
+jsonstruct = jsondecode(jsonstring);
+paramobj = ElectrolyserInputParams(jsonstruct);
+
+
+xs = ones(5, 1);
+nxs = 5 + (1 : 5)';
+
+gen = ElectrolyserGridGenerator1D(xs, nxs);
+
+paramobj = gen.updateElectrolyserInputParams(paramobj);
+
+inm = 'IonomerMembrane';
+her = 'HydrogenEvolutionElectrode';
+oer = 'OxygenEvolutionElectrode';
+ptl = 'PorousTransportLayer';
+exl = 'ExchangeLayer';
+ctl = 'CatalystLayer';
+
+model = Electrolyser(paramobj);
+
+doplotgraph = false;
+if doplotgraph
+    cgt = ComputationalGraphTool(model);
+    g = cgt.getComputationalGraph();
+    close all
+    plot(g);
+end
+
