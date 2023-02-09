c13e37deb1d8fe3a988ab49462b7f4e4cfc28054
August Johansson <august.johansson@sintef.no>
2023-02-09 14:04:48 +0100

    Improve validation script

--- Utilities/JsonUtils/validationJsonScript.py
+++ Utilities/JsonUtils/validationJsonScript.py
@@ -1,4 +1,6 @@
-# Example usage for add_local_schemas_to
+# Call from MATLAB as py.validationJsonScript(filename). Remember to
+# restart the pyenv for changes to be seen by MATLAB. This is done by
+# calling `terminate(pyenv)`.
 
 import json
 import jsonschema
@@ -6,49 +8,45 @@ from pathlib import Path
 import resolveFileInputJson as rjson
 import os
 
-schema_folder = rjson.getBattMoDir() / Path('Utilities') / Path('JsonSchemas')
-
-base_uri = 'file://battmo/schemas/'
-
+schema_folder = rjson.getBattMoDir() / Path("Utilities") / Path("JsonSchemas")
+base_uri = "file://battmo/schemas/"
 resolver = jsonschema.RefResolver(base_uri=base_uri, referrer={})
-
-verbose = True
+verbose = False
 
 
 def addJsonSchema(jsonSchemaName):
     """Add the jsonschema in the resolver"""
-    jsonSchemaFilename = jsonSchemaName + '.schema.json'
+    jsonSchemaFilename = jsonSchemaName + ".schema.json"
     schema_filename = schema_folder / jsonSchemaFilename
     if verbose:
         print(schema_filename)
     with open(schema_filename) as schema_file:
         refschema = json.load(schema_file)
-    key = "file://battmo/schemas/" + jsonSchemaName
+    key = base_uri + jsonSchemaName
     resolver.store[key] = refschema
 
 
-# We collect the schema.json files and add them in the resolver
-for (dirpath, dirnames, filenames) in os.walk(schema_folder):
-    for filename in filenames:
-        if filename.endswith('.schema.json'):
-            jsonSchemaName = filename.replace('.schema.json', '')
-            addJsonSchema(jsonSchemaName)
-
-# We validate the battery schema
-schema_filename = schema_folder / 'Simulation.schema.json'
-with open(schema_filename) as schema_file:
-    mainschema = json.load(schema_file)
-
-v = jsonschema.Draft202012Validator(mainschema, resolver=resolver)
-
-
 def validate(jsonfile):
-    # We cannot export json structs from matlab, hence we only allow
-    # file name inputs here
-    jsonstruct = rjson.loadJsonBattmo(jsonfile)
-    v.validate(jsonstruct)
-    is_valid = v.is_valid(jsonstruct)
-    if verbose:
-        print(jsonfile, "is valid?", is_valid)
-    return is_valid
 
+    # We collect the schema.json files and add them in the resolver
+    for (dirpath, dirnames, filenames) in os.walk(schema_folder):
+        for filename in filenames:
+            if filename.endswith(".schema.json"):
+                jsonSchemaName = filename.replace(".schema.json", "")
+                addJsonSchema(jsonSchemaName)
+
+    # We validate the battery schema
+    schema_filename = schema_folder / "Simulation.schema.json"
+    with open(schema_filename) as schema_file:
+        mainschema = json.load(schema_file)
+
+    if verbose:
+        print("Validate main schema", mainschema)
+    v = jsonschema.Draft202012Validator(mainschema, resolver=resolver)
+
+    # Validate the input jsonfile
+    if verbose:
+        print("Validate input file", jsonfile)
+    jsonstruct = rjson.loadJsonBattmo(jsonfile)
+
+    return v.is_valid(jsonstruct)
