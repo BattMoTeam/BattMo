c33e277c57e39f35e67bd064e0d86a108da74a24
Xavier Raynaud <xavier.raynaud@sintef.no>
2023-03-05 14:12:41 +0100

    update in runJsonScript

--- Examples/json_input/runJsonScript.m
+++ Examples/json_input/runJsonScript.m
@@ -1,74 +1,62 @@
-mrstDebug(20);
-
 mrstModule add ad-core mrst-gui mpfa agmg linearsolvers
-
 
 % load json struct for the material properties
 jsonfilename = fullfile('ParameterData', 'BatteryCellParameters', 'LithiumIonBatteryCell', ...
                         'lithium_ion_battery_nmc_graphite.json');
-
 jsonstruct_material = parseBattmoJson(jsonfilename);
 
 % load json struct for the geometrical properties
-% jsonfilename = fullfile('Examples','utils', 'data', 'geometry1d.json');
-jsonfilename = './utils/data/4680-geometry.json';
+jsonfilename = fullfile('Examples','utils', 'data', 'geometry1d.json');
 jsonstruct_geometry = parseBattmoJson(jsonfilename);
 
+% load json struct for the geometrical properties
+jsonfilename = fullfile('Examples','utils', 'data', 'ie_control.json');
+jsonstruct_control = parseBattmoJson(jsonfilename);
 
-% merge the two json structs 
-jsonstruct = mergeJsonStructs({jsonstruct_geometry,
-                               jsonstruct_material}, 'force', true);
+% load json struct for the geometrical properties
+jsonfilename = fullfile('Examples','utils', 'data', 'simulation_parameters.json');
+jsonstruct_simparams = parseBattmoJson(jsonfilename);
 
-% For jellyroll case, we use a iterative linear solver
-if strcmp(jsonstruct.Geometry.case, 'jellyRoll')
-    
-    jsonstruct.NonLinearSolver.maxIterations = 10;
-    jsonstruct.NonLinearSolver.verbose = false;
+% load json struct for output extra specifications
+jsonfilename = fullfile('Examples','utils', 'data', 'extra_output.json');
+jsonstruct_output = parseBattmoJson(jsonfilename);
 
-    ne = 'NegativeElectrode';
-    pe = 'PositiveElectrode';
-    am = 'ActiveMaterial';
-    
-    diffusionModelType = jsonstruct.(pe).(am).diffusionModelType;
-    assert(strcmp(jsonstruct.(pe).(am).diffusionModelType, ...
-                  jsonstruct.(ne).(am).diffusionModelType), ...
-           'case where two different diffusion models are used in negative and positive electrode has not been implemented.');
-    
-    switch diffusionModelType
-      case 'simple'
-        jsonfilename = fullfile(battmoDir, 'Utilities/JsonSchemas/Tests/linearsolver5.json');
-      case 'full'
-        jsonfilename = fullfile(battmoDir, 'Utilities/JsonSchemas/Tests/linearsolver4.json');
-      otherwise
-        error('diffusionModelType not covered')
-    end
+% We merge the json structures. The function issues a warning if a parameter is set with different values in the given
+% structures. The rule is that the first value takes precedence.
+jsonstruct = mergeJsonStructs({jsonstruct_geometry , ...
+                               jsonstruct_material , ...
+                               jsonstruct_control  , ...
+                               jsonstruct_simparams, ...
+                               jsonstruct_output   , ...                               
+                              });
 
-    jsonstruct.NonLinearSolver.LinearSolver.linearSolverSetup = parseBattmoJson(jsonfilename);
-    
-end
+
+%% We adjust the total time with respect to the given CRate.
 
 CRate = jsonstruct.Control.CRate;
-
 jsonstruct.TimeStepping.totalTime = 1.4*hour/CRate;
-jsonstruct.TimeStepping.N = 100;
+jsonstruct.TimeStepping.N = 40;
 
-% Run battery simulation with function that takes json input
+%% Run battery simulation with function that takes json input
+
 output = runBatteryJson(jsonstruct);
 
-%%
+%% Plotting
 
-E             = output.E;
-energyDensity = output.energyDensity;
-energy        = output.energy;
+states = output.states;
 
-figure
-plot(energyDensity, E)
-xlabel('Energy Density [Wh/L]');
-ylabel('Voltage [V]');
+E = cellfun(@(x) x.Control.E, states); 
+I = cellfun(@(x) x.Control.I, states);
+time = cellfun(@(x) x.time, states); 
 
-figure
-plot(energy, E)
-xlabel('Energy [Wh]');
-ylabel('Voltage [V]');
+figure()
+subplot(1,2,1)
+plot(time/hour, E)
+xlabel('time [hours]')
+ylabel('Cell Voltage [V]')
 
+subplot(1,2,2)
+plot(time/hour, I)
+xlabel('time [hours]')
+ylabel('Cell Current [A]')
 
