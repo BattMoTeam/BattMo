baa7fb050965780dfd3a653e1dabb02133c26a23
Erasdna <solheim.andreas@gmail.com>
2023-07-24 15:20:04 +0200

    Minor fix

--- JuliaBridge/runSetupModel.m
+++ JuliaBridge/runSetupModel.m
@@ -18,9 +18,10 @@ for icase = 1 : numel(casenames)
     [model,schedule,state0]=setupMatlabModel(casename, jsonfolder);
 end
 
+mfilename('fullpath')
 jlcall('', ...
-     'project', '/data2/andreas/json_experiment/json_env', ... % activate a local Julia Project
-     'setup', '/data2/andreas/json_experiment/BattMo/JuliaBridge/setup.jl',...
+     'project', 'BattMo.jl/src/MatlabBridge', ... % activate a local Julia Project
+     'setup', 'setup.jl',...
      'modules', {'json_env'}, ... % load a custom module and some modules from Base Julia
      'threads', 'auto', ... % use the default number of Julia threads
      'restart', true, ... % start a fresh Julia server environment
