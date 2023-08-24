2a646c790106ea6672395bccbf7b0e0007e78266
Xavier Raynaud <xavier.raynaud@sintef.no>
2023-08-24 17:47:42 +0200

    added immutable and mutable version of TwoPointFiniteVolumeGeometry

--- Utilities/Assembly/MutableTwoPointFiniteVolumeGeometry.m
+++ Utilities/Assembly/MutableTwoPointFiniteVolumeGeometry.m
@@ -1,6 +1,6 @@
-classdef TwoPointFiniteVolumeGeometry
+classdef MutableTwoPointFiniteVolumeGeometry < matlab.mixin.Copyable
 
-    properties (SetAccess = immutable)
+    properties
 
         % cell data with fields
         % - cells.centroids
@@ -27,7 +27,7 @@ classdef TwoPointFiniteVolumeGeometry
 
     methods
 
-        function tPFVgeometry = TwoPointFiniteVolumeGeometry(tPFVgeometryInput)
+        function tPFVgeometry = MutableTwoPointFiniteVolumeGeometry(tPFVgeometryInput)
         % create handle
 
             tPFVgeometry.cells = tPFVgeometryInput.cells;
@@ -35,12 +35,28 @@ classdef TwoPointFiniteVolumeGeometry
             tPFVgeometry.nodes = tPFVgeometryInput.nodes;
             tPFVgeometry.hT    = tPFVgeometryInput.hT;
 
-            if isfield(tPFVgeometryInput, 'faceArea') | isprop(tPFVgeometryInput, 'faceArea')
+            if isfield(tPFVgeometryInput, 'faceArea') & isprop(tPFVgeometryInput, 'faceArea')
                 tPFVgeometry.faceArea = tPFVgeometryInput.faceArea;
             end
             
         end
 
+    end
+    
+    methods (Access = protected)
+        
+        function cp = copyElement(obj)
+
+            cp = TwoPointFiniteVolumeGeometry;
+            
+            cp.cells    = obj.cells;
+            cp.faces    = obj.faces;
+            cp.nodes    = obj.nodes;            
+            cp.hT       = obj.hT;                        
+            cp.faceArea = obj.faceArea;
+            
+        end
+        
     end
     
 end
