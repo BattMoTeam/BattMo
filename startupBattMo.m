e15f950f597698495604cf26c8bf87f3edeb4b58
Xavier Raynaud <xav.raynaud@gmail.com>
2023-08-29 09:02:14 +0200

    added JuliaBridge in startupBattmo

--- startupBattMo.m
+++ startupBattMo.m
@@ -36,7 +36,7 @@ mrstPath('register', 'upr', fullfile(rootdirname, 'Externals', 'upr'));
 
 %% The BattMo source code directories are now added directly to path
 
-dirnames = {'Battery', 'Electrochemistry', 'Examples', 'ParameterData', 'Physics', 'Utilities', 'Tests', 'Electrolyser', 'SeaWater'};
+dirnames = {'Battery', 'Electrochemistry', 'Examples', 'ParameterData', 'Physics', 'Utilities', 'Tests', 'Electrolyser', 'SeaWater', 'JuliaBridge'};
 
 for ind = 1 : numel(dirnames)
     dirname = fullfile(rootdirname, dirnames{ind});
