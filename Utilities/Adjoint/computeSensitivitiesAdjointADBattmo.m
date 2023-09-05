1a6c7e1ddf4eb2f6a3446b501ba88b2f5af77ba1
Xavier Raynaud <xav.raynaud@gmail.com>
2023-09-05 17:13:12 +0200

    fix in computeSensitivitiesAdjointADBattmo

--- Utilities/Adjoint/computeSensitivitiesAdjointADBattmo.m
+++ Utilities/Adjoint/computeSensitivitiesAdjointADBattmo.m
@@ -106,7 +106,9 @@ function sens = computeSensitivitiesAdjointADBattmo(setup, states, params, getOb
                     sens.(nm) = sens.(nm) + eqdth{nl}.jac{kp}'*lambda{nl};
                 end
             end
-            sens.(nm) = sens.(nm) + objth.jac{kp}';
+            if isa(objth, 'ADI')
+                sens.(nm) = sens.(nm) + objth.jac{kp}';
+            end
         end
         
     end
