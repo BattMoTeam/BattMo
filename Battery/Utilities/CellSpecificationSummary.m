bafc8e5042326423a20367526e2faefbd2707c30
Xavier Raynaud <xavier.raynaud@sintef.no>
2023-09-06 10:15:45 +0200

    setup printSpecifications in CellSpecificationSummary

--- Battery/Utilities/CellSpecificationSummary.m
+++ Battery/Utilities/CellSpecificationSummary.m
@@ -75,13 +75,13 @@ classdef CellSpecificationSummary
             cmax  = itfmodel.cmax;
             cinit = itfmodel.theta100*cmax;
             
-            U = itfmodel.computeOCPFunc(cinit, temperature, cmax);
+            U = -itfmodel.computeOCPFunc(cinit, temperature, cmax);
             
             itfmodel = model.(pe).(am).(itf);
             cmax  = itfmodel.cmax;
             cinit = itfmodel.theta100*cmax;
             
-            U = U - itfmodel.computeOCPFunc(cinit, temperature, cmax);
+            U = U + itfmodel.computeOCPFunc(cinit, temperature, cmax);
 
             %  Assign values
             
@@ -101,7 +101,65 @@ classdef CellSpecificationSummary
         end
 
         function printSpecifications(css)
+
+            ne  = 'NegativeElectrode';
+            pe  = 'PositiveElectrode';
+
+            lines = {};
+            
+            lines = css.addLine(lines, 'Packing mass'               , 'kg'   , css.packingMass);
+            lines = css.addLine(lines, 'Temperature'                , 'C'    , css.temperature -  273.15);
+            lines = css.addLine(lines, 'Mass'                       , 'kg'   , css.mass);
+            lines = css.addLine(lines, 'Volume'                     , 'L'    , css.volume/litre);
+            lines = css.addLine(lines, 'Total Capacity'             , 'Ah'   , css.capacity/hour);
+            lines = css.addLine(lines, 'Negative Electrode Capacity', 'Ah'   , css.capacities.(ne)/hour);
+            lines = css.addLine(lines, 'Positive Electrode Capacity', 'Ah'   , css.capacities.(pe)/hour);
+            lines = css.addLine(lines, 'N/P ratio'                  , '-'    , css.NPratio);
+            lines = css.addLine(lines, 'Energy'                     , 'Wh'   , css.energy/hour);
+            lines = css.addLine(lines, 'Specific Energy'            , 'Wh/kg', css.specificEnergy/hour);
+            lines = css.addLine(lines, 'Energy Density'             , 'Wh/L' , (css.energyDensity/hour)*litre);
+            lines = css.addLine(lines, 'Initial Voltage'            , 'V'    , css.initialVoltage);
+            
+            css.printLines(lines);
             
         end
+        
     end
+
+    methods (Static)
+
+        function lines = addLine(lines, description, unit, value)
+
+            line.description = description;
+            line.unit        = unit;
+            line.value       = value;
+
+            lines{end + 1} = line;
+            
+        end
+
+        function printLines(lines)
+        %% print lines
+            
+            % Setup format for fprintf
+            % Get maximum string length for the description field
+            descriptions = cellfun(@(line) line.description, lines, 'uniformoutput', false);
+            lgths        = cellfun(@(str) length(str), descriptions);
+            s            = max(lgths);
+
+            fmt = sprintf('%%%ds : %%g [%%s]\n', s);
+
+            for iline = 1 : numel(lines)
+                line = lines{iline};
+                fprintf(fmt             , ...
+                        line.description, ...
+                        line.value      , ...
+                        line.unit);
+            end
+            
+        end
+        
+        
+    end
+    
 end
