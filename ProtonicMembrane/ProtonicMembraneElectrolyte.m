50de98b20257df3809151bc69910aaa5bc48bf35
Xavier Raynaud <xav.raynaud@gmail.com>
2023-09-12 17:45:55 +0200

    moved alpha regularization inside exponential

--- ProtonicMembrane/ProtonicMembraneElectrolyte.m
+++ ProtonicMembrane/ProtonicMembraneElectrolyte.m
@@ -207,9 +207,8 @@ classdef ProtonicMembraneElectrolyte < BaseModel
             alpha = state.alpha;
             
             f = F/(R*T);
-
+            f = alpha*f;
             sigmaEl = sigmaP_0*exp(f*(E - EO2_0)) + sigmaN_0*exp(-f*(E - EH2_0));
-            sigmaEl = alpha*sigmaEl + (1 - alpha)*(sigmaP_0 + sigmaN_0);
             
             state.sigmaEl = sigmaEl;
             
