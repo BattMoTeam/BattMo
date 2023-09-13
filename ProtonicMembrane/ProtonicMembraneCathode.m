d303dc7f965cdc4338b3b8f087183f0cd72b6797
Xavier Raynaud <xav.raynaud@gmail.com>
2023-09-13 20:28:09 +0200

    possibility of more control in ProtonicMembraneCathode

--- ProtonicMembrane/ProtonicMembraneCathode.m
+++ ProtonicMembrane/ProtonicMembraneCathode.m
@@ -21,20 +21,45 @@ classdef ProtonicMembraneCathode < ProtonicMembraneElectrode
         
             model = registerVarAndPropfuncNames@ProtonicMembraneElectrode(model);
             
-            fn = @ProtonicMembraneCathode.updateJ;
+            fn = @ProtonicMembraneCathode.updateJHp;
             inputnames = {'eta'};
             model = model.registerPropFunction({'jHp', fn, inputnames});
-                    
+
+
+            % fn = @ProtonicMembraneCathode.updateJ;
+            % inputnames = {'eta'};
+            % model = model.registerPropFunction({'j', fn, inputnames});
+
+            
+            % fn = @ProtonicMembraneCathode.updatePi;
+            % inputnames = {};
+            % model = model.registerPropFunction({'pi', fn, inputnames});
+
+            % model = model.removeVarName('eta');
+            
         end
         
         
-        function state = updateJ(model, state)
+        function state = updateJHp(model, state)
 
             state.jHp = 1/model.R * state.eta;
-
+            
         end
 
 
+        function state = updateJ(model, state)
+
+            state.j = 1/model.R * state.eta;
+            
+        end
+
+
+        
+        function state = updatePi(model, state)
+
+            state.pi = 0;
+            
+        end
         
     end
     
