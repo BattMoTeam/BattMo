997195e471ae8239c99d697f428245027ac06968
Xavier Raynaud <xav.raynaud@gmail.com>
2023-09-21 16:44:56 +0200

    fixed sign mistake in BV

--- ProtonicMembrane/ProtonicMembraneAnode.m
+++ ProtonicMembrane/ProtonicMembraneAnode.m
@@ -96,7 +96,7 @@ classdef ProtonicMembraneAnode < ProtonicMembraneElectrode
             
             feta = con.F*model.n/(con.R*model.T).*eta;
             
-            jHp = -i0*(exp(-beta*feta) - exp((1 - beta)*feta))./(1 + (i0/ilc)*exp(-beta*feta) - (i0/ila)*exp(-(1 - beta)*feta));
+            jHp = -i0*(exp(-beta*feta) - exp((1 - beta)*feta))./(1 + (i0/ilc)*exp(-beta*feta) - (i0/ila)*exp((1 - beta)*feta));
 
             % jHp = i0*(exp(feta/2) - exp(-feta/2))/2;
             
