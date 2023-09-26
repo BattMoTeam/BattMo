637295628a90c228ddfb4915d9b51faf57ff3460
Xavier Raynaud <xav.raynaud@gmail.com>
2023-09-26 15:05:41 +0200

    cherry pick : added unit support

--- Utilities/JsonUtils/convertUnitBattMo.m
+++ Utilities/JsonUtils/convertUnitBattMo.m
@@ -1,33 +1,15 @@
-function [val, isConverted] = convertUnitBattMo(val)
+function val = convertUnitBattMo(val)
 % We check if the field is a numerical parameter entry with units, that
 % val has two fields
 % - value : The numerical value
 % - unit : A string with unit that can be evaluated in BattMo.
-%          we use MRST support for unit, see "battmoDir()/mrst/mrst-core/utils/units/"
+%          we use MRST support for unit, see "battmoDir()/MRST/mrst-core/utils/units/"
 %          An example is "ampere/(centi*meter)^2"
 
-    isConverted = false;
-    
-    if isfield(val, 'value') && isfield(val, 'unit')
-        if numel(val) == 1
-            isConverted = true;
-            % This is a numerical parameter that requires a unit conversion
-            if ~isempty(val.unit)
-                str = sprintf('val = %g*%s;', val.value, val.unit);
-                eval(str);
-            else
-                % Empty unit. Assume value is given in SI units
-                val = val.value;
-            end
-        else
-            vals = [];
-            for ival = 1 : numel(val)
-                cval = convertUnitBattMo(val(ival));
-                vals(end + 1) = cval;
-            end
-            isConverted = true;
-            val = vals;
-        end
+    if isfield(val, 'value') & isfield(val, 'unit')
+        % This is a numerical parameter that requires a unit conversion
+        str = sprintf('val = %g*%s;', val.value, val.unit);
+        eval(str);
     end
-
+    
 end
