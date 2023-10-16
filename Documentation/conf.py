b3a2a33c883d7664a576a842ce63b7d9909d10a0
Xavier Raynaud <xavier.raynaud@sintef.no>
2023-10-16 11:06:17 +0200

    update in conf.py for doc

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -32,9 +32,12 @@ autodoc_member_order = 'bysource'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-# extensions = ['sphinxcontrib.bibtex']
-# bibtex_bibfiles = ['refs.bib']
+extensions = ['sphinxcontrib.globalsubs', 'sphinxcontrib.bibtex']
+bibtex_bibfiles = ['refs.bib']
 
+global_substitutions = {
+    'battmo': '**BattMo**'
+}
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -109,7 +112,14 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+    }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -124,17 +134,23 @@ html_theme = 'sphinx_rtd_theme'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 html_logo = 'battmologo.png'
+html_favicon = 'battmologo.ico'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #html_favicon = None
 
+rst_prolog = """
+.. role:: todo
+   :class: todo
+"""
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-# html_css_files = ['css/custom.css']
+html_static_path = ['_static']
+html_css_files = ['css/custom.css']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
