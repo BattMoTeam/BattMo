8bc23a9916e08727ff5a01826c1068adedd0ead2
Xavier Raynaud <xav.raynaud@gmail.com>
2023-10-17 13:45:27 +0200

    more in runSiliconGraphiteBattery

--- Examples/composite/runSiliconGraphiteBattery.m
+++ Examples/composite/runSiliconGraphiteBattery.m
@@ -1,12 +1,11 @@
-% clear the workspace and close open figures
-clear all
-close all
+%% Composite Silicon Graphite electrode
 
 %% Import the required modules from MRST
 % load MRST modules
-mrstModule add ad-core mrst-gui mpfa matlab_bgl
+mrstModule add ad-core matlab_bgl
 
-%% shortcuts
+%% Shortcuts
+% We define shorcuts for the sub-models.
 
 ne = 'NegativeElectrode';
 pe = 'PositiveElectrode';
@@ -22,73 +21,72 @@ sd  = 'SolidDiffusion';
 itf = 'Interface';
 
 %% Setup the properties of Li-ion battery materials and cell design
-jsonstruct_composite_material = parseBattmoJson('ParameterData/BatteryCellParameters/LithiumIonBatteryCell/lithium_ion_battery_nmc_silicon_graphite.json');
-jsonstruct_cell               = parseBattmoJson('ParameterData/BatteryCellParameters/LithiumIonBatteryCell/lithium_ion_battery_nmc_graphite.json');
+%
+%_We_load_the_property_of_a_composite silicon graphite electrode, see :ref:`compositeElectrode`
 
+jsonstruct_composite_material = parseBattmoJson('ParameterData/BatteryCellParameters/LithiumIonBatteryCell/lithium_ion_battery_nmc_silicon_graphite.json');
+
+%%
+% For the remaining properties, we consider a standard data set 
+jsonstruct_cell = parseBattmoJson('ParameterData/BatteryCellParameters/LithiumIonBatteryCell/lithium_ion_battery_nmc_graphite.json');
+
+%%
+% We remove form the standard data set the :code:`ActiveMaterial` field. This step is not necessary but is cleaner and
+% we avoid a warning.
 jsonstruct_cell.(ne).(co) = rmfield(jsonstruct_cell.(ne).(co), 'ActiveMaterial');
 
+%%
+% We merge the two json structures
 jsonstruct = mergeJsonStructs({jsonstruct_composite_material, ...
                                jsonstruct_cell});
 
+%%
+% We do not consider the thermal model and remove the current collector
 jsonstruct.use_thermal = false;
 jsonstruct.include_current_collectors = false;
 
+%%
+% We instantiate the battery :code:`InputParams` object
 paramobj = BatteryInputParams(jsonstruct);
+
+%%
+% We set the mass fractions of the different material in the coating of the negative electrode. This information could
+% have been passed in the json file earlier (:ref:`compositeElectrode`)
 
 paramobj.(ne).(co).(am1).massFraction = 0.9;
 paramobj.(ne).(co).(am2).massFraction = 0.08;
 paramobj.(ne).(co).(bd).massFraction  = 0.01;
 paramobj.(ne).(co).(ad).massFraction  = 0.01;
 
+%%
+% We change the given CRate
 paramobj.Control.CRate = 0.1;
 
+%%
+% We validate the :code:`InputParams` using the method :code:`validateInputParams` which belongs to the parent class. This step 
 Paramobj = paramobj.validateInputParams();
 
 gen = BatteryGenerator1D();
 
+%% 
 % Now, we update the paramobj with the properties of the mesh. 
 paramobj = gen.updateBatteryInputParams(paramobj);
 
+%% Model Instantiation
 % We instantiate the model
 model = Battery(paramobj);
 
-inspectgraph = false;
-if inspectgraph
-    cgt = model.computationalGraph;
-    return
-end
 
-% model.Control.Imax = 1e-1;
 
-%% Setup the initial state of the model
-% The initial state of the model is setup using the model.setupInitialState() method.
 
-initstate = model.setupInitialState();
-
-%% setup schedule
-
-%% Compute the nominal cell capacity and choose a C-Rate
-% The nominal capacity of the cell is calculated from the active materials.
-% This value is then combined with the user-defined C-Rate to set the cell
-% operational current. 
+%% Setup the time step schedule 
+% We will run two period a discharge followed by a charge
+%
+% We start with the charge period
 
 CRate = model.Control.CRate;
 
-%% Setup the time step schedule 
-% Smaller time steps are used to ramp up the current from zero to its
-% operational value. Larger time steps are then used for the normal
-% operation.
-
-ctrl = 'Control';
-
-switch model.(ctrl).controlPolicy
-  case 'CCCV'
-    total = 3.5*hour/CRate;
-  case 'IEswitch'
-    total = 1.4*hour/CRate;
-  otherwise
-    error('control policy not recognized');
-end
+total = 1.4*hour/CRate;
 
 n  = 100;
 dt = total/n;
@@ -98,86 +96,94 @@ tup = 0.1; % rampup value for the current function, see rampupSwitchControl
 srcfunc = @(time, I, E) rampupSwitchControl(time, tup, I, E, ...
                                             model.Control.Imax, ...
                                             model.Control.lowerCutoffVoltage);
-% we setup the control by assigning a source and stop function.
 control = struct('src', srcfunc, 'IEswitch', true);
 
-% This control is used to set up the schedule
 schedule = struct('control', control, 'step', step); 
 
-%% Run simulation
+%% Setup the initial state of the model
+%
+% We use the default initialisation given by a method in the model
 
-initstate = model.setupInitialState(); 
+initstate = model.setupInitialState();
 
 %% Setup the properties of the nonlinear solver 
+% We adjust some settings for the nonlinear solver 
 nls = NonLinearSolver();
-
-CRate = model.Control.CRate;
-
-ctrl = 'Control';
-
-switch model.(ctrl).controlPolicy
-  case 'CCCV'
-    total = 3.5*hour/CRate;
-  case 'IEswitch'
-    total = 1.4*hour/CRate;
-  otherwise
-    error('control policy not recognized');
-end
-
+%%
 % Change default maximum iteration number in nonlinear solver
 nls.maxIterations = 10;
+%%
 % Change default behavior of nonlinear solver, in case of error
 nls.errorOnFailure = false;
+%%
+% We use a time step selector based on relative change of a target value, in our case the output voltage
 nls.timeStepSelector=StateChangeTimeStepSelector('TargetProps', {{'Control','E'}}, 'targetChangeAbs', 0.03);
-% Change default tolerance for nonlinear solver
+%%
+% We adjust the nonlinear tolerance
 model.nonlinearTolerance = 1e-3*model.Control.Imax;
-% Set verbosity
+%% 
+% We use verbosity
 model.verbose = true;
 
-%% Run the simulation
+%% Run the simulation for the discharge
 
 [wellSols, states, report] = simulateScheduleAD(initstate, model, schedule, 'OutputMinisteps', true, 'NonLinearSolver', nls); 
 
 dischargeStates = states;
 
-%%
+%% Setup charge schedule
 
+%% 
+% We use the last computed state of the discharge as the initial state for the charge period.
 initstate = states{end};
 
+%%
+% We use a new control. Note the minus sign in front of :code:`model.Control.Imax`
 srcfunc = @(time, I, E) rampupSwitchControl(time, tup, I, E, ...
                                             -model.Control.Imax, ...
                                             model.Control.upperCutoffVoltage);
 control = struct('src', srcfunc, 'IEswitch', true);
 schedule = struct('control', control, 'step', step); 
 
+%% Run the simulation for the charge perios
 [wellSols, states, report] = simulateScheduleAD(initstate, model, schedule, 'OutputMinisteps', true, 'NonLinearSolver', nls);
 
 chargeStates = states;
 
-%%
+%% Visualisation
 
+%%
+% We concatenate the states we have computed
 allStates = vertcat(dischargeStates, chargeStates); 
 
+%%
+% Some ploting setup
 set(0, 'defaultlinelinewidth', 3);
 set(0, 'DefaultAxesFontSize', 16);
 set(0, 'defaulttextfontsize', 18);
 
-E = cellfun(@(x) x.Control.E, allStates); 
-I = cellfun(@(x) x.Control.I, allStates);
-Tmax = cellfun(@(x) max(x.ThermalModel.T), allStates);
+%%
+% We extract the voltage, current and time from the simulation output
+E    = cellfun(@(x) x.Control.E, allStates); 
+I    = cellfun(@(x) x.Control.I, allStates);
 time = cellfun(@(x) x.time, allStates); 
 
+%%
+%  We plot the voltage and current
 figure
+subplot(2, 1, 1);
 plot(time/hour, E);
 xlabel('Time / h');
 ylabel('Voltage / V');
 title('Voltage')
-
-figure
+subplot(2, 1, 2);
 plot(time/hour, I);
 xlabel('Time / h');
 ylabel('Current / I');
 title('Current')
+
+%%
+% We compute and plot the state of charges in the different material
 
 figure
 hold on
