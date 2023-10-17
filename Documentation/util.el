3b7808335933c61d127684c678b915154f741a51
Xavier Raynaud <xav.raynaud@gmail.com>
2023-10-17 13:05:20 +0200

    minor improvement in util.el

--- Documentation/util.el
+++ Documentation/util.el
@@ -3,13 +3,22 @@
 ;; Use user-login-name to get user name
 ;; (cond '(compare-strings user-login-name "xavier")
 (pyvenv-activate "~/Python/battmodoc-env/")
-(defconst docdir "/home/xavier/Matlab/Projects/battmo/Documentation/")
-(defconst utilsdir "/home/xavier/Matlab/Projects/battmo/Documentation/utils/")
-(defconst testdir "/home/xavier/Matlab/Projects/battmo-doc-test/")
 
-(defconst docdir "/home/august/Projects/Battery/BattMo-dev/Documentation/")
-(defconst utilsdir "/home/august/Projects/Battery/BattMo-dev/Documentation/utils/")
-(defconst testdir "/home/august/Projects/Battery/battmo-doc-test/")
+(pcase (user-login-name)
+    ("xavier" (progn
+                (setq docdir "/home/xavier/Matlab/Projects/battmo/Documentation/")
+                (setq utilsdir "/home/xavier/Matlab/Projects/battmo/Documentation/utils/")
+                (setq testdir "/home/xavier/Matlab/Projects/battmo-doc-test/")))
+
+    ("august" (progn
+                (setq docdir "/home/august/Projects/Battery/BattMo-dev/Documentation/")
+                (setq utilsdir "/home/august/Projects/Battery/BattMo-dev/Documentation/utils/")
+                (setq testdir "/home/august/Projects/Battery/battmo-doc-test/")
+                
+                )
+     )
+    )
+
 
      
 (defun battmodoc-build ()
