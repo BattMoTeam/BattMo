a3477b02012ccbf188654f26c52da5da8aabceec
Xavier Raynaud <xavier.raynaud@sintef.no>
2023-11-21 15:58:03 +0100

    merge fix in FullSolidDiffusionModel (following main branch)

--- Electrochemistry/FullSolidDiffusionModel.m
+++ Electrochemistry/FullSolidDiffusionModel.m
@@ -116,7 +116,7 @@ classdef FullSolidDiffusionModel < SolidDiffusionModel
             model = model.registerPropFunction({'cAverage', fn, {'c'}});
 
             % we remove this declaration as it is not used in assembly (otherwise it may be computed but not used)
-            model = model.registerExtraVarName('cAverage');
+            model = model.setAsExtraVarName('cAverage');
             
         end
 
