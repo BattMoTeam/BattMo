e3debd7d42191c49eae2b1425ec564924d344842
August Johansson <august.johansson@sintef.no>
2023-11-22 15:46:23 +0100

    Possibly temporary assertions for better checks of coating data

--- Electrochemistry/Coating.m
+++ Electrochemistry/Coating.m
@@ -4,7 +4,7 @@ classdef Coating < ElectronicComponent
 
         %% Sub-Models
 
-        ActiveMaterial 
+        ActiveMaterial
         Binder
         ConductingAdditive
 
@@ -19,7 +19,7 @@ classdef Coating < ElectronicComponent
         effectiveDensity     % the mass density of the material (symbol: rho). Important : the density is computed with respect to total volume (including the empty pores)
         bruggemanCoefficient % the Bruggeman coefficient for effective transport in porous media (symbol: beta)
         active_material_type % 'default' (only one particle type) or 'composite' (two different particles)
-        
+
         % Advanced parameters (used if given, otherwise computed)
         volumeFractions                 % mass fractions of each components (if not given computed subcomponent and density)
         volumeFraction
@@ -30,18 +30,18 @@ classdef Coating < ElectronicComponent
 
         % external coupling parameters
         externalCouplingTerm % structure to describe external coupling (used in absence of current collector)
-        
+
         %% Computed parameters at model setup
         compInds            % index of the sub models in the massFractions structure
-        
+
     end
 
     methods
-        
+
         function model = Coating(paramobj)
 
             model = model@ElectronicComponent(paramobj);
-            
+
             fdnames = {'effectiveDensity'               , ...
                        'bruggemanCoefficient'           , ...
                        'active_material_type'           , ...
@@ -71,14 +71,15 @@ classdef Coating < ElectronicComponent
               otherwise
                 error('active_material_type not recognized.');
             end
-            
+
             model.subModelNameList = compnames;
-            
+
 
             %% We setup the volume fractions of each components
-            
+
             % Compute the specific volumes of each component based on the density and mass fraction
             % If components or values are missing, the volume fraction is set to zero
+            specificVolumes = zeros(numel(compnames), 1);
             for icomp = 1 : numel(compnames)
                 compname = compnames{icomp};
                 compInds.(compname) = icomp;
@@ -87,18 +88,14 @@ classdef Coating < ElectronicComponent
                     mf  = paramobj.(compname).massFraction;
                     if ~isempty(rho) & ~isempty(mf)
                         specificVolumes(icomp) = mf/rho;
-                    else
-                        specificVolumes(icomp) = 0;
                     end
-                else
-                    specificVolumes(icomp) = 0;                    
                 end
             end
 
             model.compInds = compInds;
-            
+
             % We treat special cases
-            
+
             switch model.active_material_type
               case 'default'
                 if all(specificVolumes == 0)
@@ -122,23 +119,25 @@ classdef Coating < ElectronicComponent
 
             if isempty(model.volumeFractions)
 
+                volumeFractions = zeros(numel(compnames), 1);
                 sumSpecificVolumes = sum(specificVolumes);
                 for icomp = 1 : numel(compnames)
                     volumeFractions(icomp) = specificVolumes(icomp)/sumSpecificVolumes;
                 end
 
                 model.volumeFractions = volumeFractions;
-                
+
             end
 
             %% We setup the volume fraction of coating
-            
+
             if isempty(model.volumeFraction)
-                
+
                 % If the volumeFraction is given, we use it otherwise it is computed from the density and the specific
                 % volumes of the components
+                assert(~isempty(model.effectiveDensity), 'At this point we need an effective density in the model');
                 model.volumeFraction = sumSpecificVolumes*model.effectiveDensity;
-                
+
             end
 
             %% We setup the electronic conductivity
@@ -148,12 +147,11 @@ classdef Coating < ElectronicComponent
                 kappa = 0;
                 for icomp = 1 : numel(compnames)
                     compname = compnames{icomp};
-                    if ~isempty(paramobj.(compname))
-                        assert(~isempty(paramobj.(compname)), 'missing electronic conductivity');
+                    if ~isempty(paramobj.(compname)) && ~isempty(paramobj.(compname).electronicConductivity)
                         kappa = kappa + model.volumeFractions(icomp)*paramobj.(compname).electronicConductivity;
                     end
                 end
-
+                assert(abs(kappa) > 0, 'The electronicConductivity must be provided for at least one component');
                 model.electronicConductivity = kappa;
             end
 
@@ -162,17 +160,17 @@ classdef Coating < ElectronicComponent
                 kappa = model.electronicConductivity;
                 vf    = model.volumeFraction;
                 bg    = model.bruggemanCoefficient;
-                
+
                 model.effectiveElectronicConductivity = kappa*vf^bg;
-                
+
             end
 
             %% We setup the thermal parameters
-            
+
             if model.use_thermal
-                
+
                 %% We setup the thermal conductivities
-                
+
                 if isempty(model.thermalConductivity)
                     bg = model.bruggemanCoefficient;
                     thermalConductivity = 0;
@@ -204,9 +202,9 @@ classdef Coating < ElectronicComponent
                 end
 
             end
-            
+
             %% Setup the submodels
-            
+
             np = paramobj.G.cells.num;
             switch paramobj.active_material_type
               case 'default'
@@ -228,10 +226,10 @@ classdef Coating < ElectronicComponent
               otherwise
                 error('active_material_type not recognized');
             end
-            
+
             model.Binder             = Binder(paramobj.Binder);
             model.ConductingAdditive = ConductingAdditive(paramobj.ConductingAdditive);
-            
+
         end
 
         function model = registerVarAndPropfuncNames(model)
@@ -243,12 +241,12 @@ classdef Coating < ElectronicComponent
 
             itf = 'Interface';
             sd  = 'SolidDiffusion';
-            
+
             varnames = {'jCoupling', ...
                         'jExternal', ...
                         'SOC'};
 
-            model = model.registerVarNames(varnames);            
+            model = model.registerVarNames(varnames);
 
             % We declare SOC as an extra variable, as it is not used in assembly (otherwise it will be systematically
             % computed but not used)
@@ -259,10 +257,10 @@ classdef Coating < ElectronicComponent
               case 'default'
 
                 am = 'ActiveMaterial';
-                
+
                 fn = @Coating.updateEsource;
                 model = model.registerPropFunction({'eSource', fn, {{am, sd, 'Rvol'}}});
-                
+
                 fn = @Coating.updatePhi;
                 model = model.registerPropFunction({{am, itf, 'phiElectrode'}, fn, {'phi'}});
 
@@ -271,7 +269,7 @@ classdef Coating < ElectronicComponent
 
                 fn = @Coating.updateSOC;
                 model = model.registerPropFunction({'SOC', fn, {{am, sd, 'cAverage'}}});
-            
+
               case 'composite'
 
                 am1 = 'ActiveMaterial1';
@@ -279,21 +277,21 @@ classdef Coating < ElectronicComponent
 
                 varnames = {{am1, 'SOC'}, ...
                             {am2, 'SOC'}};
-                
+
                 model = model.registerVarNames(varnames);
                 model = model.setAsExtraVarNames(varnames);
-                
+
                 % We remove the dUdT variable (not used for non thermal simulation)
                 varnames = {{am1, itf, 'dUdT'}, ...
                             {am2, itf, 'dUdT'}};
 
                 model = model.removeVarNames(varnames);
-                
+
                 fn = @Coating.updateCompositeEsource;
                 inputnames = {{am1, sd, 'Rvol'}, ...
                               {am2, sd, 'Rvol'}};
                 model = model.registerPropFunction({'eSource', fn, inputnames});
-                
+
                 fn = @Coating.updateCompositePhi;
                 model = model.registerPropFunction({{am1, itf, 'phiElectrode'}, fn, {'phi'}});
                 model = model.registerPropFunction({{am2, itf, 'phiElectrode'}, fn, {'phi'}});
@@ -301,27 +299,27 @@ classdef Coating < ElectronicComponent
                 fn = @Coating.dispatchCompositeTemperature;
                 model = model.registerPropFunction({{am1, 'T'}, fn, {'T'}});
                 model = model.registerPropFunction({{am2, 'T'}, fn, {'T'}});
-                
+
                 fn = @Coating.updateCompositeSOC;
                 inputnames = {{am1, sd, 'cAverage'}, ...
                               {am2, sd, 'cAverage'}};
                 model = model.registerPropFunction({'SOC', fn, inputnames});
                 model = model.registerPropFunction({{am1, 'SOC'}, fn, inputnames});
                 model = model.registerPropFunction({{am2, 'SOC'}, fn, inputnames});
-                
+
               otherwise
 
                 error('active material type not recognized.')
 
             end
-            
+
             if model.use_thermal
                 varnames = {'jFaceCoupling', ...
                             'jFaceExternal'};
                 model = model.registerVarNames(varnames);
 
             end
-            
+
             fn = @Coating.updatejBcSource;
             model = model.registerPropFunction({'jBcSource', fn, {'jCoupling', 'jExternal'}});
 
@@ -329,13 +327,13 @@ classdef Coating < ElectronicComponent
                 fn = @Coating.updatejFaceBc;
                 model = model.registerPropFunction({'jFaceBc', fn, {'jFaceCoupling', 'jFaceExternal'}});
             end
-            
+
             fn = @Coating.updatejExternal;
             model = model.registerPropFunction({'jExternal', fn, {}});
             if model.use_thermal
                 model = model.registerPropFunction({'jFaceExternal', fn, {}});
             end
-            
+
             fn = @Coating.updatejCoupling;
             model = model.registerPropFunction({'jCoupling', fn, {}});
             if model.use_thermal
@@ -343,32 +341,32 @@ classdef Coating < ElectronicComponent
             end
 
         end
-        
-        
+
+
         function state = updatejBcSource(model, state)
-            
+
             state.jBcSource = state.jCoupling + state.jExternal;
-            
+
         end
-        
+
         function state = updatejFaceBc(model, state)
-            
+
             state.jFaceBc = state.jFaceCoupling + state.jFaceExternal;
-            
+
         end
-        
+
         function state = updatejExternal(model, state)
-            
+
             state.jExternal     = 0;
             state.jFaceExternal = 0;
-            
+
         end
 
         function state = updatejCoupling(model, state)
-            
+
             state.jCoupling     = 0;
             state.jFaceCoupling = 0;
-            
+
         end
 
         function state = updateEsource(model, state)
@@ -380,7 +378,7 @@ classdef Coating < ElectronicComponent
             F    = model.constants.F;
             n    = model.(am).(itf).numberOfElectronsTransferred;
             vols = model.G.cells.volumes;
-            
+
             state.eSource =  - n*F*vols.*state.(am).(sd).Rvol;
 
         end
@@ -394,49 +392,49 @@ classdef Coating < ElectronicComponent
 
             F    = model.constants.F;
             vols = model.G.cells.volumes;
-            
+
             ams = {am1, am2};
 
             Rvol = 0;
-            
+
             for iam = 1 : numel(ams)
-                
+
                 amc = ams{iam};
-                
+
                 n    = model.(amc).(itf).numberOfElectronsTransferred;
                 Rvol = Rvol + n*F*state.(amc).(sd).Rvol;
-                
-            end 
+
+            end
 
             state.eSource = - vols.*Rvol;
-                        
+
         end
 
-        
+
         function state = updateCompositePhi(model, state)
 
             am1 = 'ActiveMaterial1';
             am2 = 'ActiveMaterial2';
             itf = 'Interface';
-            
+
             ams = {am1, am2};
-                
+
             for iam = 1 : numel(ams)
 
                 amc = ams{iam};
                 state.(amc).(itf).phiElectrode = state.phi;
-                
+
             end
-            
+
         end
-            
+
         function state = updatePhi(model, state)
-            
+
             am  = 'ActiveMaterial';
             itf = 'Interface';
-            
+
             state.(am).(itf).phiElectrode = state.phi;
-            
+
         end
 
         function state = dispatchCompositeTemperature(model, state)
@@ -450,7 +448,7 @@ classdef Coating < ElectronicComponent
 
                 amc = ams{iam};
                 state.(amc).T = state.T;
-                
+
             end
 
         end
@@ -460,7 +458,7 @@ classdef Coating < ElectronicComponent
             state.(am).T = state.T;
         end
 
-        
+
         function state = updateSOC(model, state)
 
             % shortcut
@@ -474,7 +472,7 @@ classdef Coating < ElectronicComponent
             cmax     = model.(am).(itf).saturationConcentration;
             theta100 = model.(am).(itf).guestStoichiometry100;
             theta0   = model.(am).(itf).guestStoichiometry0;
-            
+
             c = state.(am).(sd).cAverage;
 
             theta = c/cmax;
@@ -482,11 +480,11 @@ classdef Coating < ElectronicComponent
             b     = -m .* theta0;
             SOC   = theta*m + b;
             vol   = am_frac*vf.*vols;
-            
+
             SOC = sum(SOC.*vol)/sum(vol);
 
             state.SOC = SOC;
-            
+
         end
 
         function state = updateCompositeSOC(model, state)
@@ -500,37 +498,36 @@ classdef Coating < ElectronicComponent
             vols = model.G.cells.volumes;
 
             ams = {am1, am2};
-            
+
             for iam = 1 : numel(ams)
-                
+
                 amc = ams{iam};
-                
+
                 am_frac  = model.volumeFractions(model.compInds.(amc));
                 cmax     = model.(amc).(itf).saturationConcentration;
                 theta100 = model.(amc).(itf).guestStoichiometry100;
                 theta0   = model.(amc).(itf).guestStoichiometry0;
-                
+
                 c = state.(amc).(sd).cAverage;
 
                 vol = am_frac*vf.*vols;
-                
+
                 molvals(iam)    = sum(c.*vol);
                 molval0s(iam)   = theta0*cmax*sum(vol);
                 molval100s(iam) = theta100*cmax*sum(vol);
 
                 state.(amc).SOC = (molvals(iam) - molval0s(iam))/(molval100s(iam) - molval0s(iam));
-                
+
             end
 
             molval    = sum(molvals);
             molval0   = sum(molval0s);
             molval100 = sum(molval100s);
-            
+
             state.SOC = (molval - molval0)/(molval100 - molval0);
-                
+
         end
 
-        
+
     end
 end
-
