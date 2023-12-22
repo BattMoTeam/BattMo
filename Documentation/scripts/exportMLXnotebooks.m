b6985382f1db48886b8906feb08a3f869fb2af3e
Xavier Raynaud <xav.raynaud@gmail.com>
2023-12-22 13:05:24 +0100

    exported notebooks in ipynb format

---
+++ Documentation/scripts/exportMLXnotebooks.m
@@ -0,0 +1,33 @@
+%% Export to Python notebooks require MATLAB versions >= 2023
+
+notebooknames = {'tutorial_1_a_simple_p2d_model_live.mlx'              , ...
+                 'tutorial_2_changing_control_protocol_live.mlx'       , ...
+                 'tutorial_3_modify_structural_parameters_live.mlx'    , ...
+                 'tutorial_4_modify_material_parameters_live.mlx'      , ...
+                 'tutorial_5_simulate_CCCV_cycling_live.mlx'           , ...
+                 'tutorial_6_simulate_thermal_performance_live.mlx'    , ...
+                 'tutorial_7_a_simple_p4d_model_live.mlx'              , ...
+                 'tutorial_8_simulate_a_multilayer_pouch_cell_live.mlx', ...
+                 'tutorial_9_simulate_a_cylindrical_cell_live.mlx'};
+
+inputdir  = fullfile(battmoDir(), 'Examples', 'Basic');
+outputdir = fullfile(battmoDir(), 'Documentation', 'notebooks');
+
+%% setup options
+% Most of the options are in fact the default options.
+options = {'format', 'ipynb', ...
+           'run', false     , ...
+           'embedImages', true};
+
+for inote = 1 : numel(notebooknames)
+
+    notebookname = notebooknames{inote};
+    inputfile = fullfile(inputdir, notebookname);
+    
+    notebookname = strrep(notebookname, '.mlx', '.ipynb');
+    outputfile = fullfile(outputdir, notebookname);
+    
+    export(inputfile, outputfile, options{:});
+    fprintf('Exported %s\n', notebookname);
+    
+end
