f95c40bc25e1234ce15427019498c7ab72220aa8
August Johansson <august.johansson@sintef.no>
2024-01-03 14:39:45 +0100

    Test both json file linting and validation against schemas

--- Tests/TestExamples/TestJsonFiles.m
+++ Tests/TestExamples/TestJsonFiles.m
@@ -2,45 +2,48 @@ classdef TestJsonFiles < matlab.unittest.TestCase
 
     properties (TestParameter)
 
-        jsonfile = {fullfile('ParameterData', 'BatteryCellParameters', 'LithiumIonBatteryCell', 'lithium_ion_battery_nmc_graphite.json'),
-                    fullfile('ParameterData', 'ParameterSets', 'Xu2015', 'lfp.json'),
+        jsonfile = {fullfile('ParameterData', 'BatteryCellParameters', 'LithiumIonBatteryCell', 'lithium_ion_battery_nmc_graphite.json'), ...
                     fullfile('ParameterData', 'ParameterSets', 'Chen2020', 'chen2020_lithium_ion_battery.json')};
+
+        jsonlintfile = arrayfun(@(s) fullfile(s.folder, s.name), dir(fullfile(battmoDir(), '**', '*.json')), 'uniformoutput', false);
+
     end
-    
+
+    properties
+
+        jsonlintFails = system('which jsonlint-php');
+
+        exclude = { fullfile('ParameterData', 'ParameterSets', 'Xu2015', 'lfp.json') };
+
+    end
+
     methods (Test)
+
+        function testJsonLint(test, jsonlintfile)
+
+            if ~test.jsonlintFails && ~contains(jsonlintfile, test.exclude)
+                s = sprintf('jsonlint-php %s', jsonlintfile);
+                %disp(s);
+                [st, res] = system(s);
+                assert(st == 0, res);
+            end
+
+        end
 
         function testJson(test, jsonfile)
 
-            loadModule('validationJsonScript')
-            is_valid = py.validationJsonScript.validate(jsonfile);
-            assert(is_valid);
-            
+            if ~contains(jsonfile, test.exclude)
+                %fprintf('Testing %s\n', jsonfile);
+                loadModule('validationJsonScript')
+                is_valid = py.validationJsonScript.validate(jsonfile);
+                assert(is_valid);
+            end
+
         end
 
     end
 
 end
-
-%{
-Copyright 2009-2022 SINTEF Digital, Mathematics & Cybernetics.
-
-This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
-
-MRST is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-MRST is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with MRST.  If not, see <http://www.gnu.org/licenses/>.
-%}
-
-
 
 %{
 Copyright 2021-2023 SINTEF Industry, Sustainable Energy Technology
