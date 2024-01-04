35e4284c6ab1d7e86047819ac2c9ed1c9bca2821
August Johansson <august.johansson@sintef.no>
2024-01-04 11:38:52 +0100

    Make writeOutput conform to refactoring

--- Utilities/writeOutput.m
+++ Utilities/writeOutput.m
@@ -6,14 +6,14 @@ function writeOutput(model, states, name)
     pe      = 'PositiveElectrode';
     elyte   = 'Electrolyte';
     thermal = 'ThermalModel';
+    co      = 'Coating';
     am      = 'ActiveMaterial';
     itf     = 'Interface';
     sd      = 'SolidDiffusion';
     ctrl    = 'Control';
     cc      = 'CurrentCollector';
 
-    directory = fileparts(which('startupBattMo'));
-    file_name = string([directory, '\', name]);
+    file_name = fullfile(battmoDir(), name);
 
     % Check if the file exists, and if so, delete it
     if exist(file_name, 'file') == 2
@@ -21,7 +21,7 @@ function writeOutput(model, states, name)
         delete(file_name);
     end
 
-    [~] = H5F.create(file_name);
+    fid = H5F.create(file_name);
 
     CRate = model.Control.CRate;
 
@@ -30,13 +30,13 @@ function writeOutput(model, states, name)
     voltage = zeros(size(states));
     current = zeros(size(states));
 
-    ne_electricPotential = zeros(length(states), length(states{1}.(ne).(am).phi));
-    ne_concentration = zeros(length(states), length(states{1}.(ne).(am).(sd).c));
-    ne_surfaceConcentration = zeros(length(states), length(states{1}.(ne).(am).(sd).cSurface));
+    ne_electricPotential = zeros(length(states), length(states{1}.(ne).(co).phi));
+    ne_concentration = zeros(length(states), length(states{1}.(ne).(co).(am).(sd).c));
+    ne_surfaceConcentration = zeros(length(states), length(states{1}.(ne).(co).(am).(sd).cSurface));
 
-    pe_electricPotential = zeros(length(states), length(states{1}.(pe).(am).phi));
-    pe_concentration = zeros(length(states), length(states{1}.(pe).(am).(sd).c));
-    pe_surfaceConcentration = zeros(length(states), length(states{1}.(pe).(am).(sd).cSurface));
+    pe_electricPotential = zeros(length(states), length(states{1}.(pe).(co).phi));
+    pe_concentration = zeros(length(states), length(states{1}.(pe).(co).(am).(sd).c));
+    pe_surfaceConcentration = zeros(length(states), length(states{1}.(pe).(co).(am).(sd).cSurface));
 
     elyte_electricPotential = zeros(length(states), length(states{1}.(elyte).phi));
     elyte_concentration = zeros(length(states), length(states{1}.(elyte).c));
@@ -49,14 +49,14 @@ function writeOutput(model, states, name)
         current(ind, 1) = states{ind}.Control.I;
 
         % Get local state variables - Negative Electrode
-        ne_electricPotential(ind, :) = states{ind}.(ne).(am).phi;
-        ne_surfaceConcentration(ind, :) = states{ind}.(ne).(am).(sd).cSurface;
-        ne_concentration(ind, :) = states{ind}.(ne).(am).(sd).c;
+        ne_electricPotential(ind, :) = states{ind}.(ne).(co).phi;
+        ne_surfaceConcentration(ind, :) = states{ind}.(ne).(co).(am).(sd).cSurface;
+        ne_concentration(ind, :) = states{ind}.(ne).(co).(am).(sd).c;
 
         % Get local state variables - Positive Electrode
-        pe_electricPotential(ind, :) = states{ind}.(pe).(am).phi;
-        pe_surfaceConcentration(ind, :) = states{ind}.(pe).(am).(sd).cSurface;
-        pe_concentration(ind, :) = states{ind}.(pe).(am).(sd).c;
+        pe_electricPotential(ind, :) = states{ind}.(pe).(co).phi;
+        pe_surfaceConcentration(ind, :) = states{ind}.(pe).(co).(am).(sd).cSurface;
+        pe_concentration(ind, :) = states{ind}.(pe).(co).(am).(sd).c;
 
         % Get local state variables - Electrolyte
         elyte_electricPotential(ind, :) = states{ind}.(elyte).phi;
@@ -254,112 +254,6 @@ function writeOutput(model, states, name)
     h5write(file_name, '/local/Electrolyte/grid/cartDims', model.(elyte).G.cartDims);
     h5write(file_name, '/local/Electrolyte/grid/griddim', model.(elyte).G.griddim);
 
-
-    % if exist(specs_name, 'file') == 2
-    %     % Delete the file
-    %     specs = load(specs_name, 'specs');
-    %     elde_specs = density_computation();
-    % 
-    %     cell_mass = specs.specs.mass .* 1000;
-    %     ne_mass = (specs.specs.masses.NegativeElectrode.ActiveMaterial.val + specs.specs.masses.NegativeElectrode.CurrentCollector.val) .* 1000;
-    %     pe_mass = (specs.specs.masses.PositiveElectrode.ActiveMaterial.val + specs.specs.masses.PositiveElectrode.CurrentCollector.val) .* 1000;
-    %     elyte_mass = (specs.specs.masses.Electrolyte.val) .* 1000;
-    %     sep_mass = (specs.specs.masses.Electrolyte.Separator.val) .* 1000;
-    %     np_ratio = specs.specs.NPratio;
-    % 
-    %     ne_spec_cap = specs.specs.cap_neg/3600/specs.specs.masses.NegativeElectrode.ActiveMaterial.val;
-    %     pe_spec_cap = specs.specs.cap_pos/3600/specs.specs.masses.PositiveElectrode.ActiveMaterial.val;
-    % 
-    %     cell_capacity = specs.specs.cap ./ 3600;
-    %     cell_energy = specs.specs.effectiveEnergy ./ 3600;
-    %     cell_specificEnergy = cell_energy ./ (cell_mass ./ 1000);
-    % 
-    %     ne_thickness = output_data.output.jsonstruct.NegativeElectrode.ActiveMaterial.thickness;
-    %     pe_thickness = output_data.output.jsonstruct.PositiveElectrode.ActiveMaterial.thickness;
-    % 
-    %     ne_am_density = elde_specs.densities.NegativeElectrode.ActiveMaterial;
-    %     ne_bind_density = elde_specs.densities.NegativeElectrode.Binder;
-    %     ne_add_density = elde_specs.densities.NegativeElectrode.ConductingAdditive;
-    % 
-    %     ne_am_wt = elde_specs.compositions.NegativeElectrode.ActiveMaterial;
-    %     ne_bind_wt = elde_specs.compositions.NegativeElectrode.Binder;
-    %     ne_add_wt = elde_specs.compositions.NegativeElectrode.ConductingAdditive;
-    % 
-    %     pe_am_density = elde_specs.densities.PositiveElectrode.ActiveMaterial;
-    %     pe_bind_density = elde_specs.densities.PositiveElectrode.Binder;
-    %     pe_add_density = elde_specs.densities.PositiveElectrode.ConductingAdditive;
-    % 
-    %     pe_am_wt = elde_specs.compositions.PositiveElectrode.ActiveMaterial;
-    %     pe_bind_wt = elde_specs.compositions.PositiveElectrode.Binder;
-    %     pe_add_wt = elde_specs.compositions.PositiveElectrode.ConductingAdditive;
-    % 
-    %     ne_coating_density = elde_specs.density.NegativeElectrode ./ 1000;
-    %     pe_coating_density = elde_specs.density.PositiveElectrode ./ 1000;
-    % 
-    %     ne_mass_loading = elde_specs.convertionfactor.NegativeElectrode .* ne_thickness ./ 0.01;
-    %     pe_mass_loading = elde_specs.convertionfactor.PositiveElectrode .* pe_thickness ./ 0.01;
-    % 
-    % 
-    %     h5create(file_name, '/cell_mass', size(cell_mass));
-    %     h5create(file_name, '/ne_mass', size(ne_mass));
-    %     h5create(file_name, '/pe_mass', size(pe_mass));
-    %     h5create(file_name, '/elyte_mass', size(elyte_mass));
-    %     h5create(file_name, '/sep_mass', size(sep_mass));
-    %     h5create(file_name, '/np_ratio', size(np_ratio));
-    %     h5create(file_name, '/ne_spec_cap', size(ne_spec_cap));
-    %     h5create(file_name, '/pe_spec_cap', size(pe_spec_cap));
-    %     h5create(file_name, '/cell_capacity', size(cell_capacity));
-    %     h5create(file_name, '/cell_energy', size(cell_energy));
-    %     h5create(file_name, '/cell_specificEnergy', size(cell_specificEnergy));
-    %     h5create(file_name, '/ne_thickness', size(ne_thickness));
-    %     h5create(file_name, '/pe_thickness', size(pe_thickness));
-    %     h5create(file_name, '/ne_am_density', size(ne_am_density));
-    %     h5create(file_name, '/ne_bind_density', size(ne_bind_density));
-    %     h5create(file_name, '/ne_add_density', size(ne_add_density));
-    %     h5create(file_name, '/ne_am_wt', size(ne_am_wt));
-    %     h5create(file_name, '/ne_bind_wt', size(ne_bind_wt));
-    %     h5create(file_name, '/ne_add_wt', size(ne_add_wt));
-    %     h5create(file_name, '/pe_am_density', size(pe_am_density));
-    %     h5create(file_name, '/pe_bind_density', size(pe_bind_density));
-    %     h5create(file_name, '/pe_add_density', size(pe_add_density));
-    %     h5create(file_name, '/pe_am_wt', size(pe_am_wt));
-    %     h5create(file_name, '/pe_bind_wt', size(pe_bind_wt));
-    %     h5create(file_name, '/pe_add_wt', size(pe_add_wt));
-    %     h5create(file_name, '/ne_coating_density', size(ne_coating_density));
-    %     h5create(file_name, '/pe_coating_density', size(pe_coating_density));
-    %     h5create(file_name, '/ne_mass_loading', size(ne_mass_loading));
-    %     h5create(file_name, '/pe_mass_loading', size(pe_mass_loading));
-    % 
-    %     h5write(file_name, '/cell_mass', (cell_mass));
-    %     h5write(file_name, '/ne_mass', (ne_mass));
-    %     h5write(file_name, '/pe_mass', (pe_mass));
-    %     h5write(file_name, '/elyte_mass', (elyte_mass));
-    %     h5write(file_name, '/sep_mass', (sep_mass));
-    %     h5write(file_name, '/np_ratio', (np_ratio));
-    %     h5write(file_name, '/ne_spec_cap', (ne_spec_cap));
-    %     h5write(file_name, '/pe_spec_cap', (pe_spec_cap));
-    %     h5write(file_name, '/cell_capacity', (cell_capacity));
-    %     h5write(file_name, '/cell_energy', (cell_energy));
-    %     h5write(file_name, '/cell_specificEnergy', (cell_specificEnergy));
-    %     h5write(file_name, '/ne_thickness', (ne_thickness));
-    %     h5write(file_name, '/pe_thickness', (pe_thickness));
-    %     h5write(file_name, '/ne_am_density', (ne_am_density));
-    %     h5write(file_name, '/ne_bind_density', (ne_bind_density));
-    %     h5write(file_name, '/ne_add_density', (ne_add_density));
-    %     h5write(file_name, '/ne_am_wt', (ne_am_wt));
-    %     h5write(file_name, '/ne_bind_wt', (ne_bind_wt));
-    %     h5write(file_name, '/ne_add_wt', (ne_add_wt));
-    %     h5write(file_name, '/pe_am_density', (pe_am_density));
-    %     h5write(file_name, '/pe_bind_density', (pe_bind_density));
-    %     h5write(file_name, '/pe_add_density', (pe_add_density));
-    %     h5write(file_name, '/pe_am_wt', (pe_am_wt));
-    %     h5write(file_name, '/pe_bind_wt', (pe_bind_wt));
-    %     h5write(file_name, '/pe_add_wt', (pe_add_wt));
-    %     h5write(file_name, '/ne_coating_density', (ne_coating_density));
-    %     h5write(file_name, '/pe_coating_density', (pe_coating_density));
-    %     h5write(file_name, '/ne_mass_loading', (ne_mass_loading));
-    %     h5write(file_name, '/pe_mass_loading', (pe_mass_loading));
-
+    H5F.close(fid);
 
 end
-
