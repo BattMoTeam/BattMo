da57c497451312dfcb7677e9d272cca2a72725dd
August Johansson <august.johansson@sintef.no>
2024-01-30 15:32:03 +0100

    Sweep over mass fractions

--- Utilities/JuliaBridge/JuliaInterface/RunFromMatlab/src/parameter_sweep_utils.jl
+++ Utilities/JuliaBridge/JuliaInterface/RunFromMatlab/src/parameter_sweep_utils.jl
@@ -3,12 +3,14 @@ import BattMo
 export setParameter!, setParameters!
 
 function setParameters!(file::BattMo.JSONFile, values, experiment::String)
+
     if experiment=="Example"
-        file.object["NegativeElectrode"]["ActiveMaterial"]["Interface"]["volumeFraction"]=values[1]
-        file.object["PositiveElectrode"]["ActiveMaterial"]["Interface"]["volumeFraction"]=values[2]
+        file.object["NegativeElectrode"]["Coating"]["ActiveMaterial"]["massFraction"] = values[1]
+        file.object["PositiveElectrode"]["Coating"]["ActiveMaterial"]["massFraction"] = values[2]
     else
         println("Error: Invalid experiment!")
     end
+
 end
 
 function setParameters!(file::BattMo.MatlabFile, values, experiment::String)
