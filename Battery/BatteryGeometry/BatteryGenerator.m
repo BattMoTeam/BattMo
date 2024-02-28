cfa5dd168a08ad8e5fdb195e04c9afefc115401c
Xavier Raynaud <xavier.raynaud@sintef.no>
2024-02-28 14:05:05 +0100

    fix in BatteryGenerator

--- Battery/BatteryGeometry/BatteryGenerator.m
+++ Battery/BatteryGeometry/BatteryGenerator.m
@@ -274,8 +274,8 @@ classdef BatteryGenerator
             cc_coupfaces = tbl.get('faces1');
             am_coupfaces = tbl.get('faces2');
 
-            cc_coupcells = sum(G_cc.parentGrid.topology.faces.neighbors(cc_coupfaces, :), 2);
-            am_coupcells = sum(G_am.parentGrid.topology.faces.neighbors(am_coupfaces, :), 2);
+            cc_coupcells = sum(G_cc.topology.faces.neighbors(cc_coupfaces, :), 2);
+            am_coupcells = sum(G_am.topology.faces.neighbors(am_coupfaces, :), 2);
 
             coupTerm.couplingfaces = [cc_coupfaces, am_coupfaces];
             coupTerm.couplingcells = [cc_coupcells, am_coupcells];
