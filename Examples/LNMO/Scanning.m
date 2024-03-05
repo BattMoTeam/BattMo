530a5e9024fe19ab941db00b3ab664d26e6e367d
Xavier Raynaud <xavier.raynaud@sintef.no>
2024-03-05 14:34:25 +0100

    updated scanning parameters

--- Examples/LNMO/Scanning.m
+++ Examples/LNMO/Scanning.m
@@ -3,6 +3,7 @@ classdef Scanning < handle
     properties (SetAccess = immutable)
         
         jsonstruct
+        packingMass
         
     end
 
@@ -20,29 +21,32 @@ classdef Scanning < handle
 
         function sc = Scanning()
 
-            jsonstruct      = parseBattmoJson('Examples/LNMO/SiGrLnmo.json');
-            jsonstruct_cccv = parseBattmoJson('Examples/JsonDataFiles/cccv_control.json');
-
-            jsonstruct = removeJsonStructFields(jsonstruct, ...
-                                                {'Control', 'DRate'}        , ...
-                                                {'Control', 'controlPolicy'}, ...
-                                                {'Control', 'dEdtLimit'}    , ...
-                                                {'Control', 'dIdtLimit'}    , ...
-                                                {'Control', 'rampupTime'}   , ...
-                                                {'Control', 'useCVswitch'});
-
-            jsonstruct = mergeJsonStructs({jsonstruct, jsonstruct_cccv});
-
+            sc.packingMass = 15*gram;
+            
+            jsonstruct = parseBattmoJson('Examples/LNMO/SiGrLnmo.json');
+            jsonstruct_geometry = parseBattmoJson('Examples/JsonDataFiles/geometryMultiLayerPouch.json');
+            jsonstruct_geometry.Geometry.nLayers = 15;
+            
+            jsonstruct = mergeJsonStructs({jsonstruct_geometry, ...
+                                           jsonstruct});
 
             ne  = 'NegativeElectrode';
             pe  = 'PositiveElectrode';
             sep = 'Separator';
+            cc  = 'CurrentCollector';
             co  = 'Coating';
             am1 = 'ActiveMaterial1';
             am2 = 'ActiveMaterial2';
             am  = 'ActiveMaterial';
             itf = 'Interface';
 
+            % set width and length of pouch
+
+            jsonstruct.Geometry.length = 300*milli*meter;
+            jsonstruct.Geometry.width  = 90*milli*meter;
+            
+            % We change the guest stoichiometries
+            
             jsonstruct.(ne).(co).(am1).(itf).guestStoichiometry100 = 0.99;
             jsonstruct.(ne).(co).(am1).(itf).guestStoichiometry0   = 0.01;
             jsonstruct.(ne).(co).(am2).(itf).guestStoichiometry100 = 0.99;
@@ -51,12 +55,14 @@ classdef Scanning < handle
             jsonstruct.(pe).(co).(am).(itf).guestStoichiometry0   = 0.99;
             jsonstruct.(pe).(co).(am).(itf).guestStoichiometry100 = 0.01;
 
-            % We do not need fine discretization
+            % We do not need fine discretization, set it to minimum
 
             jsonstruct.(ne).(co).N = 1;
             jsonstruct.(pe).(co).N = 1;
+            jsonstruct.(ne).(cc).N = 1;
+            jsonstruct.(pe).(cc).N = 1;
             jsonstruct.(sep).N = 1;
-
+            
             sc.jsonstruct = jsonstruct;
             
         end
@@ -93,7 +99,7 @@ classdef Scanning < handle
             model = setupModelFromJson(jsonstruct);
             
             energy = computeCellEnergy(model);
-            mass   = computeCellMass(model);
+            mass   = computeCellMass(model, 'packingMass', sc.packingMass);
 
             specificEnergy = energy/mass;
 
