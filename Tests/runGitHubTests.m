464e7bf382c26bc9ff62146f1f74d06532eab6c8
Xavier Raynaud <xavier.raynaud@sintef.no>
2024-04-15 18:20:52 +0200

    remove parallel run in runGitHubTests

--- Tests/runGitHubTests.m
+++ Tests/runGitHubTests.m
@@ -46,7 +46,7 @@ import matlab.unittest.TestRunner
 % Setup
 mrstVerbose 'off';
 stopOnError        = false;
-runTestsInParallel = true;
+runTestsInParallel = false;
 doAssertSuccess    = true;
 
 % Define which test cases to run
