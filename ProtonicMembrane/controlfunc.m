a45c1efcf2d028d33bdcacb88bbb73f9503ea331
Xavier Raynaud <xavier.raynaud@sintef.no>
2024-04-16 19:42:17 +0200

    added more controls in controlfunc

--- ProtonicMembrane/controlfunc.m
+++ ProtonicMembrane/controlfunc.m
@@ -6,12 +6,34 @@ function [I, alpha, beta] = controlfunc(time, Imax, tswitch, T, varargin)
     switch opt.order
       case 'alpha-first'
         if time <= tswitch
-            I = 0;
+            I     = 0;
             alpha = time/tswitch;
+            beta  = 0;
         else
-            I = (time - tswitch)/(T - tswitch)*Imax;
+            I     = (time - tswitch)/(T - tswitch)*Imax;
             alpha = 1;
+            beta  = (time - tswitch)/(T - tswitch);
         end
+      case 'alpha-first-beta-constant'
+        if time <= tswitch
+            I     = 0;
+            alpha = time/tswitch;
+            beta  = 0;
+        else
+            I     = (time - tswitch)/(T - tswitch)*Imax;
+            alpha = 1;
+            beta  = 1;
+        end
+      case 'alpha-equal-beta'
+        if time <= tswitch
+            I     = time/tswitch*Imax;
+            alpha = 0;
+            beta  = 0;
+        else
+            I     = Imax;
+            alpha = (time - tswitch)/(T - tswitch);
+            beta  = alpha;
+        end        
       case 'I-first'
         if time <= tswitch
             I     = time/tswitch*Imax;
@@ -19,7 +41,6 @@ function [I, alpha, beta] = controlfunc(time, Imax, tswitch, T, varargin)
             beta  = time/tswitch;
         else
             I     = Imax;
-            % alpha = 0;
             alpha = (time - tswitch)/(T - tswitch);
             beta = 1;
         end
