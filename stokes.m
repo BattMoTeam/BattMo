359fd18be2b84f9a784533aba19c9f56f24a9fc2
Xavier Raynaud <xavier.raynaud@sintef.no>
2024-04-25 17:14:06 +0200

    more in stokes

--- stokes.m
+++ stokes.m
@@ -5,12 +5,17 @@ G = computeGeometry(G);
 
 tbls = setupTables(G, 'includetbls', {'cellnodetbl', 'vectbl'});
 
-cellNodeTbl = tbls.cellnodetbl;
-vecTbl      = tbls.vectbl;
-nodeTbl     = tbls.nodetbl;
-nodeVecTbl  = tbls.nodevectbl;
-cellTbl     = tbls.celltbl;
-cellVecTbl  = tbls.cellvectbl;
+cellNodeTbl    = tbls.cellnodetbl;
+vecTbl         = tbls.vectbl;
+nodeTbl        = tbls.nodetbl;
+nodeVecTbl     = tbls.nodevectbl;
+cellTbl        = tbls.celltbl;
+faceTbl        = tbls.facetbl;
+cellVecTbl     = tbls.cellvectbl;
+faceVecTbl     = tbls.facevectbl;
+cellFaceTbl    = tbls.cellfacetbl;
+cellFaceVecTbl = tbls.cellfacevectbl;
+faceNodeTbl    = tbls.facenodetbl;
 
 cellNodeVecTbl = crossIndexArray(cellNodeTbl, vecTbl, {});
 cellNodeVecTbl = sortIndexArray(cellNodeVecTbl, {'cells', 'vec', 'nodes'});
@@ -134,5 +139,120 @@ tens = tens.setFromTensorProd(cellNodeVecLin, prod);
 M = tens.getMatrix();
 
 % full(M)
+
+%% Setup of R
+
+faceVec = reshape(G.faces.normals', [], 1);
+
+cells = cellFaceTbl.get('cells');
+faces = cellFaceTbl.get('faces');
+sgn = 2*(cells == G.faces.neighbors(faces, 1)) - 1; % sgn is in cellfacetbl
+
+prod = TensorProd();
+prod.tbl1 = cellFaceTbl;
+prod.tbl2 = faceVecTbl;
+prod.tbl3 = cellFaceVecTbl;
+prod.mergefds = {'faces'};
+prod = prod.setup();
+
+cellFaceVec = prod.eval(sgn, faceVec);
+
+prod = TensorProd();
+prod.tbl1 = faceNodeTbl;
+prod.tbl2 = cellFaceVecTbl;
+prod.reducefds = {'faces'};
+prod = prod.setup();
+
+delta = ones(faceNodeTbl.num, 1);
+
+cellNodeVecTbl1 = prod.tbl3;
+cellNodeVec1    = prod.eval(delta, cellFaceVec);
+
+A = [[4 1 1 1; ...
+      5 2 2 1; ...
+      6 1 2 1; ...
+      6 2 1 1]];
+
+clear linVec12Tbl
+linVec12Tbl.lin  = A(:, 1);
+linVec12Tbl.vec1 = A(:, 2);
+linVec12Tbl.vec2 = A(:, 3);
+linVec12Tbl = IndexArray(linVec12Tbl);
+
+linVec12 = A(:, 4);
+
+prod = TensorProd();
+prod.tbl1 = linVec12Tbl;
+prod.tbl2 = cellNodeVecTbl1;
+prod.replacefds1 = {{'vec1', 'vec'}};
+prod.replacefds2 = {{'vec', 'vec2'}};
+prod.reducefds = {'vec2'};
+prod = prod.setup();
+
+cellNodeVecLinTbl1 = prod.tbl3;
+cellNodeVecLin1    = prod.eval(0.5*linVec12, cellNodeVec1);
+
+prod = TensorProd();
+prod.tbl1 = cellNodeVecLinTbl1;
+prod.tbl2 = linTbl;
+prod.tbl3 = cellNodeVecTbl;
+prod.reducefds = {'lin'};
+prod = prod.setup();
+
+tens = SparseTensor();
+tens = tens.setFromTensorProd(cellNodeVecLin1, prod);
+
+M1 = tens.getMatrix();
+
+prod = TensorProd();
+prod.tbl1 = linVec12Tbl;
+prod.tbl2 = cellFaceVecTbl;
+prod.replacefds1 = {{'vec1', 'vec'}};
+prod.replacefds2 = {{'vec', 'vec2'}};
+prod.reducefds = {'vec2'};
+prod = prod.setup();
+
+cellFaceVecLinTbl1 = prod.tbl3;
+cellFaceVecLin1    = prod.eval(linVec12, cellFaceVec);
+
+prod = TensorProd();
+prod.tbl1 = faceVecTbl;
+prod.tbl2 = cellFaceVecLinTbl1;
+prod.reducefds = {'vec'};
+prod.mergefds = {'faces'};
+prod = prod.setup();
+
+cellFaceLinTbl1 = prod.tbl3;
+cellFaceLin1    = prod.eval(faceVec, cellFaceVecLin1);
+
+prod = TensorProd();
+prod.tbl1 = faceTbl;
+prod.tbl2 = cellFaceLinTbl1;
+prod.tbl3 = cellFaceLinTbl1;
+prod.mergefds = {'faces'};
+prod = prod.setup();
+
+cellFaceLin1 = prod.eval(1./G.faces.areas, cellFaceLin1);
+
+prod = TensorProd();
+prod.tbl1 = cellFaceLinTbl1;
+prod.tbl2 = linTbl;
+prod.tbl3 = cellFaceTbl;
+prod.reducefds = {'lin'};
+prod = prod.setup();
+
+tens = SparseTensor();
+tens = tens.setFromTensorProd(cellFaceLin1, prod);
+
+M2 = tens.getMatrix();
+
+M = [M1; M2];
+
+full(M)
+
+
+
+
+
 
 
