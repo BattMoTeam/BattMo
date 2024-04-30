931191e30e5cd128a68efc89fc3b3c116ef002cc
Xavier Raynaud <xav.raynaud@gmail.com>
2024-04-30 11:47:18 +0200

    updated

--- runExampleStokes.m
+++ runExampleStokes.m
@@ -1,6 +1,8 @@
 dim = 2;
+
 close all
 clear all
+
 gridtype = 'pebi';
 
 switch gridtype
@@ -32,12 +34,10 @@ switch gridtype
 
     viscosity = 1;
     
-  case 'complex'
-
-    load('G.mat');
-
   case 'pebi'
 
+    mrstModule add upr
+    
     l = {[0.1,0.42; 0.4,.55; 0.7,0.65], ...
          [0.8,0.13; 0.6,0.4; 0.55,0.6],...
          [0.42,1.08; 0.45,0.9; 0.5,0.8; 0.58,0.6]};
@@ -72,31 +72,42 @@ switch gridtype
 
   case 'radial'
 
+    mrstModule add nwm
     
     %% Make a layered radial-Cartesian hybrid grid with inclination
     % Build the Cartesian grid
-    GC = cartGrid([15, 15], [300, 300]);
-    GC = computeGeometry(GC);
+    G = cartGrid([30, 30], [4, 4]);
+    G = computeGeometry(G);
 
+    tol = 1e-6;
     % Define the well region by logical indices
-    ij = gridLogicalIndices(GC);
-    idxI = ij{1} >= 6 & ij{1} <= 10 & ij{2} >= 6 & ij{2} <= 10;
-    cI = find(idxI); 
+    cI = find(G.cells.centroids(:, 1)   - 1 >= - tol   ...
+              & G.cells.centroids(:, 1) - 3 <= tol ...
+              & G.cells.centroids(:, 2) - 1 >= - tol   ...
+              & G.cells.centroids(:, 2) - 3 <= tol ...
+             );
+
 
     % Place the well at the region center
-    pW  = [150, 150];
+    pW  = [2, 2];
 
     % Define radial parameters
-    [nR, rW, rM] = deal(10, 0.2, 35);
+    [nR, rW, rM] = deal(4, 0.5, 1);
 
     % Get the hybrid grid
-    G = radCartHybridGrid(GC, cI, rW, rM, nR, pW);
-
+    G = radCartHybridGrid(G, cI, rW, rM, nR, pW);
+    
+    doplot = true
+    if doplot
+        plotGrid(G);
+    end
+    
   otherwise
+    
     error('case not recognized');
+    
 end
 
-return
 solver = StokesSolver(G           , ...
                       dirichletBc , ...
                       neumannFaces, ...
