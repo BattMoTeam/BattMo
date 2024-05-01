a54ef45042a3f4712a34371e7fa3da7ec7fe5740
Xavier Raynaud <xav.raynaud@gmail.com>
2024-05-01 18:47:31 +0200

    more in runExampleStokes

--- runExampleStokes.m
+++ runExampleStokes.m
@@ -44,17 +44,21 @@ switch gridtype
 
     mrstModule add upr
     
-    l = {[0.1,0.42; 0.4,.55; 0.7,0.65], ...
-         [0.8,0.13; 0.6,0.4; 0.55,0.6],...
-         [0.42,1.08; 0.45,0.9; 0.5,0.8; 0.58,0.6]};
+    l = {[0.1, 0.42; 0.4,.55; 0.7, 0.65]             , ...
+         [0.8, 0.13; 0.6, 0.4; 0.55, 0.6]            , ...
+         [0.42, 0.91; 0.45, 0.9; 0.5, 0.8; 0.58, 0.6], ...
+         [0.1, 0.1; 0.8, 0.5; 0.9, 0.9]
+        };
     
     gS = [1/30,1/30];
     
     %% Create grid
     % We use the routine compositePebiGrid2D to create the grid. We use the
     % preset options. 
-    G = compositePebiGrid2D(gS, [1,1.15], 'faceConstraints',l, 'useMrstPebi', true);
+    G = compositePebiGrid2D(gS, [1, 1], 'faceConstraints',l, 'useMrstPebi', true);
     G = computeGeometry(G);
+
+    plotGrid(G);
     
     extfaces = find(any(G.faces.neighbors == 0, 2));
 
@@ -82,26 +86,60 @@ switch gridtype
   case 'radial'
 
     mrstModule add nwm
-    
-    %% Make a layered radial-Cartesian hybrid grid with inclination
-    % Build the Cartesian grid
-    G = cartGrid([30, 30], [4, 4]);
-    G = computeGeometry(G);
 
+    backgroundgrid = 'pebi';
+    lx = 1;
+    ly = 1;
+
+    switch backgroundgrid
+
+      case 'cart'
+
+        nx = 30;
+        ny = 30;
+
+        G = cartGrid([nx, nx], [lx, ly]);
+        G = computeGeometry(G);
+        
+      case 'pebi'
+
+        nx = 30;
+        ny = 30;
+
+        G1 = cartGrid([nx, nx], [lx, ly]);
+        G1 = computeGeometry(G1);
+        
+        wl = {[0.2,0.8;0.8,0.2]};
+        G2  = pebiGrid2D(1/10,[1,1],'cellConstraints',wl);
+        G2.nodes.coords = bsxfun(@plus, G2.nodes.coords, -[1, 0]);
+
+        G = glue2DGrid(G1, G2);
+        G.cells = rmfield(G.cells, 'indexMap');
+
+        G = computeGeometry(G);
+        
+      otherwise
+        
+        error('backgroundgrid not recognized');
+        
+    end
+
+    nR = 4;
+    rW = 0.05;
+    rM = 0.1;
+    rE = 0.12;
+    
+    pW = [lx/2, ly/2];
+    
     tol = 1e-6;
     % Define the well region by logical indices
-    cI = find(G.cells.centroids(:, 1)   - 1 >= - tol   ...
-              & G.cells.centroids(:, 1) - 3 <= tol ...
-              & G.cells.centroids(:, 2) - 1 >= - tol   ...
-              & G.cells.centroids(:, 2) - 3 <= tol ...
+    cI = find(G.cells.centroids(:, 1)   - (pW(1) - rE) >= - tol   ...
+              & G.cells.centroids(:, 1) - (pW(1) + rE) <= tol ...
+              & G.cells.centroids(:, 2) - (pW(2) - rE) >= - tol   ...
+              & G.cells.centroids(:, 2) - (pW(2) + rE) <= tol ...
              );
-
-
-    % Place the well at the region center
-    pW  = [2, 2];
-
-    % Define radial parameters
-    [nR, rW, rM] = deal(4, 0.5, 1);
+    % plotGrid(G)
+    % plotGrid(G, cI, 'facecolor', 'red')
 
     % Get the hybrid grid
     G = radCartHybridGrid(G, cI, rW, rM, nR, pW);
@@ -117,12 +155,12 @@ switch gridtype
     cr = bsxfun(@plus, c, -pW);
 
     clear radfaceTbl
-    radfaceTbl.faces = extfaces(sum(cr.^2, 2) <= 1.5);
+    radfaceTbl.faces = extfaces(sum(cr.^2, 2) <= rW + tol);
     radfaceTbl = IndexArray(radfaceTbl);
     radfaceIndTbl = radfaceTbl.addInd('ind', ones(radfaceTbl.num, 1));
     
     clear hdirfaceTbl
-    hdirfaceTbl.faces = extfaces( c(:, 1) <= 0 + tol |   c(:, 1) >= 4 - tol  );
+    hdirfaceTbl.faces = extfaces( c(:, 1) <= 0 + tol |   c(:, 1) >= lx - tol  );
     hdirfaceTbl = IndexArray(hdirfaceTbl);
     hdirfaceIndTbl = hdirfaceTbl.addInd('ind', 2*ones(hdirfaceTbl.num, 1));
 
@@ -191,7 +229,13 @@ switch gridtype
 end
 
 
+u  = solver.getNodalVelocity(x);
+p  = solver.getPressure(x);
 
-u = solver.getNodalVelocity(x);
-p = solver.getPressure(x);
+xn = G.nodes.coords;
+
+figure
+hold on
+plotGrid(G, 'facecolor', 'none');
+quiver(xn(:, 1), xn(:, 2), u(:, 1), u(:, 2), 'linewidth', 2);
 
