3733f3b1f7f9fae926e69389cf7b56832361a7e3
Xavier Raynaud <xavier.raynaud@sintef.no>
2024-05-14 15:35:27 +0200

    update in Chen case for impedance

--- Examples/Advanced/Impedance/runImpedanceChen.m
+++ Examples/Advanced/Impedance/runImpedanceChen.m
@@ -1,6 +1,18 @@
 clear all
 % close all
 
+% We define some shorthand names for simplicity.
+ne      = 'NegativeElectrode';
+pe      = 'PositiveElectrode';
+elyte   = 'Electrolyte';
+thermal = 'ThermalModel';
+co      = 'Coating';
+am      = 'ActiveMaterial';
+itf     = 'Interface';
+sd      = 'SolidDiffusion';
+ctrl    = 'Control';
+cc      = 'CurrentCollector';
+
 mrstModule add ad-core mrst-gui mpfa agmg linearsolvers
 
 jsonstruct_material = parseBattmoJson(fullfile('ParameterData','ParameterSets','Chen2020','chen2020_lithium_ion_battery.json'));
@@ -9,13 +21,21 @@ jsonstruct_geometry = parseBattmoJson(fullfile('Examples', 'JsonDataFiles', 'geo
 jsonstruct = mergeJsonStructs({jsonstruct_material, ...
                                jsonstruct_geometry});
 
+includeImpedance = false;
+
+if includeImpedance
+
+    jsonstruct.(ne).(co).(am).(itf).useDoubleLayerCapacity = true;
+    jsonstruct.(ne).(co).(am).(itf).doubleLayerCapacitance = 0.2;
+
+end
+
 [model, inputparams, ~, gen] = setupModelFromJson(jsonstruct);
 
 c_ne = 29.866*mol/litre; % initial concentration at negative electrode
 c_pe = 17.038*mol/litre; % initial concentration at positive electrode
 
 initstate = initStateChen2020(model, c_ne, c_pe);
-
 
 impsolv = ImpedanceSolver(inputparams, 'initstate', initstate, 'computeSteadyState', false);
 
