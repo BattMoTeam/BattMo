5124d24805af3867e79f431d6c35f51a6190754e
Xavier Raynaud <xav.raynaud@gmail.com>
2024-05-28 10:43:27 +0200

    added plot in runBatteryP2D

--- Examples/Advanced/SEI/runBolaySEI.m
+++ Examples/Advanced/SEI/runBolaySEI.m
@@ -17,11 +17,14 @@ itf   = 'Interface';
 sei   = 'SolidElectrodeInterface';
 sr    = 'SideReaction';
 elyte = 'Electrolyte';
+ctrl  = 'Control';
 
 %% Setup the properties of the Li-ion battery materials and of the cell design
 jsonfilename = fullfile('ParameterData', 'BatteryCellParameters', 'LithiumIonBatteryCell', ...
                         'lithium_ion_battery_nmc_graphite.json');
 jsonstruct = parseBattmoJson(jsonfilename);
+
+jsonstruct.use_thermal = false;
 
 jsonfilename = fullfile('ParameterData', 'ParameterSets', 'Bolay2022', 'bolay_sei_interface.json');
 jsonstruct_bolay = parseBattmoJson(jsonfilename);
@@ -31,6 +34,18 @@ jsonstruct.(ne).(co).(am) = mergeJsonStructs({jsonstruct.(ne).(co).(am), ...
                                               jsonstruct_bolay});
 
 jsonstruct.(ne).(co).(am).SEImodel = 'Bolay';
+
+jsontruct_control = struct( 'controlPolicy'     , 'CCCV'       , ...
+                            'initialControl'    , 'discharging', ...
+                            'numberOfCycles'    , 3            , ...
+                            'CRate'             , 0.1          , ...
+                            'DRate'             , 0.1          , ...
+                            'lowerCutoffVoltage', 3            , ...
+                            'upperCutoffVoltage', 4            , ...
+                            'dIdtLimit'         , 1e-7         , ...
+                            'dEdtLimit'         , 1e-7);
+
+jsonstruct.(ctrl) = jsontruct_control;
 
 inputparams = BatteryInputParams(jsonstruct);
 
@@ -44,13 +59,7 @@ model = model.equipModelForComputation();
 %% Setup the schedule
 %
 
-timestep.numberOfTimeSteps = 100;
-
-step    = model.Control.setupScheduleStep(timestep);
-control = model.Control.setupScheduleControl();
-
-% This control is used to set up the schedule
-schedule = struct('control', control, 'step', step);
+schedule = model.(ctrl).setupSchedule([]);
 
 
 %% Setup the initial state of the model
@@ -70,66 +79,72 @@ model.nonlinearTolerance = 1e-5;
 model.verbose = true;
 [~, states, report] = simulateScheduleAD(initstate, model, schedule, 'OutputMinisteps', true, 'NonLinearSolver', nls);
 
-return
+%% Setup for plotting
+
+ind = cellfun(@(x) not(isempty(x)), states); 
+states = states(ind);
+time = cellfun(@(x) x.time, states); 
+E    = cellfun(@(x) x.Control.E, states); 
+I    = cellfun(@(x) x.Control.I, states);
+
+for istate = 1 : numel(states)
+    states{istate} = model.addVariables(states{istate});
+end
 
 %% Plotting
 
-set(0, 'defaulttextfontsize', 15);
-set(0, 'defaultaxesfontsize', 15);
-set(0, 'defaultlinelinewidth', 3);
-set(0, 'defaultfigureposition', [10, 10, 800, 400]);
+close all
 
-ind = cellfun(@(state) ~isempty(state), states);
-states = states(ind);
+set(0, 'defaultlinelinewidth', 3)
+set(0, 'defaultaxesfontsize', 15)
 
-time = cellfun(@(state) state.time, states);
-
-cSurface = cellfun(@(state) state.(sd).cSurface, states);
-figure
-plot(time/hour, cSurface/(1/litre));
-xlabel('time / h');
-ylabel('Surface concentration / mol/L');
-title('Surface concentration');
-
-E = cellfun(@(state) state.E, states);
 figure
 plot(time/hour, E);
-xlabel('time / h');
-ylabel('Potential / V');
-title('Potential');
+title('Voltage / V')
+xlabel('Time / h')
 
-
-cmin = cellfun(@(state) min(state.(sd).c), states);
-cmax = cellfun(@(state) max(state.(sd).c), states);
-
-for istate = 1 : numel(states)
-    states{istate} = model.evalVarName(states{istate}, {sd, 'cAverage'});
-end
-
-caver = cellfun(@(state) max(state.(sd).cAverage), states);
+figure
+plot(time/hour, I);
+title('Current / A')
+xlabel('Time / h')
 
 figure
 hold on
-plot(time/hour, cmin /(mol/litre), 'displayname', 'cmin');
-plot(time/hour, cmax /(mol/litre), 'displayname', 'cmax');
-plot(time/hour, caver/(mol/litre), 'displayname', 'total concentration');
-title('Concentration in particle / mol/L')
+
+delta = cellfun(@(state) state.(ne).(co).(am).(itf).SEIlength(end), states);
+plot(time/hour, delta/(nano*meter), 'displayname', 'at x_{max}')
+
+delta = cellfun(@(state) state.(ne).(co).(am).(itf).SEIlength(1), states);
+plot(time/hour, delta/(nano*meter), 'displayname', 'at x_{min}')
+
+title('SEI thickness in negative electrode/ nm')
+xlabel('Time / h')
+
 legend show
+figure
+hold on
 
-c = states{end}.(sd).c;
-r = linspace(0, model.(sd).particleRadius, model.(sd).N);
+u = cellfun(@(state) state.(ne).(co).(am).(itf).SEIvoltageDrop(end), states);
+plot(time/hour, u, 'displayname', 'at x_{max}')
+
+u = cellfun(@(state) state.(ne).(co).(am).(itf).SEIvoltageDrop(1), states);
+plot(time/hour, u, 'displayname', 'at x_{min}')
+
+title('SEI voltage drop in negative electrode/ V')
+xlabel('Time / h')
 
 figure
-plot(r, c/(mol/litre));
-xlabel('radius / m')
-ylabel('concentration / mol/L')
-title('Particle concentration profile (last time step)')
 
-seilength = cellfun(@(state) state.(itf).SEIlength, states);
+vols = model.(ne).(co).G.getVolumes();
 
-figure
-plot(time/hour, seilength);
-xlabel('time / hour')
-ylabel('length / m')
-title('SEI layer length')
+for istate = 1 : numel(states)
+    state = states{istate};
+    m(istate) = sum(vols.*state.(ne).(co).(am).(sd).cAverage);
+end
+
+plot(time/hour, m);
+title('total lithium amount in negative electrode / mol')
+xlabel('Time / h')
+
+legend show
 
