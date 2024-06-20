2badbbceac0bee2044ea169af27d08b889c142a0
Xavier Raynaud <xav.raynaud@gmail.com>
2024-06-20 18:54:02 +0200

    minor change in parsing in CcCvControlModel

--- Control/CcCvControlModel.m
+++ Control/CcCvControlModel.m
@@ -290,6 +290,9 @@ classdef CcCvControlModel < ControlModel
             if ~isempty(params.timeStepDuration)
                 dt = params.timeStepDuration
             else
+                if ~isempty(params.numberOfTimeSteps) 
+                    error('No timeStepDuration and numberOfTimeSteps are given');
+                end
                 n  = params.numberOfTimeSteps;
                 dt = totalTime/n;
             end
