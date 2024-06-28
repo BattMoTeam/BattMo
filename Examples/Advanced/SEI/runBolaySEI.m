1a821fa3801626292383825043fe745b0ae09c92
Xavier Raynaud <xav.raynaud@gmail.com>
2024-06-28 17:07:46 +0200

    added example

--- Examples/Advanced/SEI/runBolaySEI.m
+++ Examples/Advanced/SEI/runBolaySEI.m
@@ -37,57 +37,20 @@ jsonstruct.(ne).(co).(am).SEImodel = 'Bolay';
 
 jsontruct_control = struct( 'controlPolicy'     , 'CCCV'       , ...
                             'initialControl'    , 'discharging', ...
-                            'numberOfCycles'    , 1            , ...
+                            'numberOfCycles'    , 4            , ...
                             'CRate'             , 1            , ...
                             'DRate'             , 1            , ...
                             'lowerCutoffVoltage', 3            , ...
                             'upperCutoffVoltage', 4            , ...
-                            'dIdtLimit'         , 1e-2         , ...
-                            'dEdtLimit'         , 1e-2);
+                            'dIdtLimit'         , 1e-4         , ...
+                            'dEdtLimit'         , 1e-4);
 
 jsonstruct.(ctrl) = jsontruct_control;
 
-%% 
+jsonstruct.(ne).(co).(am).(itf).SEIelectronicDiffusionCoefficient = 3.5e-15;
 
-doCompare = false;
+jsonstruct.SOC = 1;
 
-if doCompare
-    
-    jsonfilename = fullfile('ParameterData', 'BatteryCellParameters', 'LithiumIonBatteryCell', ...
-                            'lithium_ion_battery_nmc_graphite.json');
-    jsonstruct_original = parseBattmoJson(jsonfilename);
-
-    jsonstruct_original.use_thermal = false;
-
-    jsonfilename = fullfile('ParameterData', 'ParameterSets', 'Bolay2022', 'bolay_sei_interface.json');
-    jsonstruct_original_bolay = parseBattmoJson(jsonfilename);
-
-
-    jsonstruct_original.(ne).(co).(am) = mergeJsonStructs({jsonstruct_original.(ne).(co).(am), ...
-                                                  jsonstruct_original_bolay});
-
-    jsonstruct_original.(ne).(co).(am).SEImodel = 'Bolay';
-
-    jsonstruct_original.(ctrl) = jsontruct_control;
-
-
-    fjv = compareJson(jsonstruct, jsonstruct_original);
-
-    fjv = fjv.sort('comparison')
-    
-    fjv.print();
-    
-    return
-    
-end
-%%
-
-doPrintJsonStruct = false;
-
-if doPrintJsonStruct
-    fjv = flattenJsonStruct(jsonstruct);
-    % fjv.print('filter', {'parame name', 'SEI'})
-end
 
 %%
 
@@ -99,7 +62,6 @@ inputparams = gen.updateBatteryInputParams(inputparams);
 model = GenericBattery(inputparams);
 
 %% Setup the schedule
-%
 
 % schedule = model.(ctrl).setupSchedule([]);
 jsonstruct.TimeStepping.timeStepDuration = 200;
@@ -192,6 +154,7 @@ vols = model.(ne).(co).G.getVolumes();
 
 for istate = 1 : numel(states)
     state = states{istate};
+    state = model.evalVarName(state, {ne, co, am, itf, 'SEIconcentration'});
     m(istate) = sum(vols.*state.(ne).(co).(am).(sd).cAverage);
 end
 
@@ -205,22 +168,17 @@ legend show
 
 quantities = [];
 
+vols = model.(ne).(co).G.getVolumes();
+
 for timeindex = 1 : numel(states)
 
-	l = states{timeindex}.(ne).(co).(am).(itf).SEIlength;
-
-        indAm = model.(ne).(co).compInds.(am);
-        
-	vols = model.(ne).(co).G.getVolumes();
-
-        vsa = model.(ne).(co).(am).(itf).volumetricSurfaceArea;
-        
-	scoef   = model.(ne).(co).(am).(itf).SEIstochiometricCoeffcient;
-	seimvol = model.(ne).(co).(am).(itf).SEImolarVolume;
-
-        Liqqt = (scoef/seimvol)*sum(l.*(vsa*vols));
-	quantities(end + 1) = Liqqt;
-        
+    state = states{timeindex};
+    state = model.evalVarName(state, {ne, co, am, itf, 'SEIconcentration'});
+    cSEI  = state.(ne).(co).(am).(itf).SEIconcentration;
+    
+    Liqqt = sum(cSEI.*vols);
+    quantities(end + 1) = Liqqt;
+    
 end
 
 figure 
@@ -230,8 +188,6 @@ xlabel('Time / h');
 ylabel('quantity / mol');
 grid on;
 
-%%
-
 PE_Li_quantities          = [];
 NE_Li_quantities          = [];
 Electrolyte_Li_quantities = [];
@@ -239,34 +195,44 @@ Electrodes_Li_quantities  = [];
 Total_Li_quantities       = [];
 
 for timeindex = 1 : numel(states)
+
+
+    amvf     = model.(pe).(co).volumeFractions(1);
+    vf       = model.(pe).(co).volumeFraction;
+    vols     = model.(pe).G.getVolumes;
+    cAverage = states{timeindex}.(pe).(co).(am).(sd).cAverage;
+
+    PE_qtt = sum(amvf.*vf.*vols.*cAverage);
     
-	PE_qtt = sum(model.(pe).(co).volumeFractions(1).*model.(pe).(co).volumeFraction.*model.(pe).G.getVolumes.*states{timeindex}.(pe).(co).(am).(sd).cAverage);
-	NE_qtt = sum(model.(ne).(co).volumeFractions(1).*model.(ne).(co).volumeFraction.*model.(ne).G.getVolumes.*states{timeindex}.(ne).(co).(am).(sd).cAverage);
-	Elyte_qtt = sum(model.Electrolyte.volumeFraction.*model.Electrolyte.G.getVolumes.*states{timeindex}.Electrolyte.c);
+    amvf     = model.(ne).(co).volumeFractions(1);
+    vf       = model.(ne).(co).volumeFraction;
+    vols     = model.(ne).G.getVolumes;
+    cAverage = states{timeindex}.(ne).(co).(am).(sd).cAverage;
 
-	Elode_qtt = PE_qtt + NE_qtt;
-	Tot_Liqqt = PE_qtt + NE_qtt + Elyte_qtt;
-	
-	PE_Li_quantities(end + 1)          = PE_qtt;
-	NE_Li_quantities(end + 1)          = NE_qtt;
-	Electrolyte_Li_quantities(end + 1) = Elyte_qtt;
-	Electrodes_Li_quantities(end + 1)  = Elode_qtt;
-	Total_Li_quantities(end + 1)       = Tot_Liqqt;
+    NE_qtt = sum(amvf.*vf.*vols.*cAverage);
 
+    Elyte_qtt = sum(model.Electrolyte.volumeFraction.*model.Electrolyte.G.getVolumes.*states{timeindex}.Electrolyte.c);
+
+    Elode_qtt = PE_qtt + NE_qtt;
+    Tot_Liqqt = PE_qtt + NE_qtt + Elyte_qtt;
+        
+    PE_Li_quantities(end + 1)          = PE_qtt;
+    NE_Li_quantities(end + 1)          = NE_qtt;
+    Electrolyte_Li_quantities(end + 1) = Elyte_qtt;
+    Electrodes_Li_quantities(end + 1)  = Elode_qtt;
+    Total_Li_quantities(end + 1)       = Tot_Liqqt;
 end
-
-%%
 
 figure
 hold on
 
-plot(time/hour, PE_Li_quantities,'DisplayName','Positive Electrode');
-plot(time/hour, NE_Li_quantities,'DisplayName','Negative Electrode');
-plot(time/hour, Electrolyte_Li_quantities,'DisplayName','Electrolyte');
-plot(time/hour, Electrodes_Li_quantities,'DisplayName','Both Electrodes');
-plot(time/hour, Total_Li_quantities,'DisplayName','Total (except SEI)');
+plot(time/hour, PE_Li_quantities                ,'DisplayName','Positive Electrode');
+plot(time/hour, NE_Li_quantities                ,'DisplayName','Negative Electrode');
+plot(time/hour, Electrolyte_Li_quantities       ,'DisplayName','Electrolyte');
+plot(time/hour, Electrodes_Li_quantities        ,'DisplayName','Both Electrodes');
+plot(time/hour, Total_Li_quantities             ,'DisplayName','Total (except SEI)');
 plot(time/hour, Total_Li_quantities + quantities,'DisplayName','Total (including SEI)');
-plot(time/hour, quantities,'DisplayName','In the SEI');
+plot(time/hour, quantities                      ,'DisplayName','In the SEI');
 title('Lithium quantity');
 xlabel('Time / h');
 ylabel('quantity / mol');
@@ -276,7 +242,15 @@ legend show
 %%
 
 figure 
-plot(time/hour, 100 * quantities / Total_Li_quantities(1));
+
+capacity = computeCellCapacity(model);
+F = PhysicalConstants.F;
+
+qty  = quantities;
+qty  = qty - qty(1);
+
+plot(time/year, 100 * (qty*F) / capacity);
+
 title('Percentage of Lithium consummed');
 xlabel('Time / h');
 ylabel('%');
