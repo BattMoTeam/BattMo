d3cf5d6d4ed119bbf11d90fbe7d2594f454f3ca9
Xavier Raynaud <xav.raynaud@gmail.com>
2024-07-24 18:23:00 +0200

    more in block example script

--- Examples/Experimental/Block/runHomogeneousBlockExample.m
+++ Examples/Experimental/Block/runHomogeneousBlockExample.m
@@ -1,3 +1,40 @@
+el      = 'ElectronicModel';
+thermal = 'ThermalModel';
+ctrl    = 'Control';
+            
 jsonstruct = parseBattmoJson('/home/xavier/Matlab/Projects/battmo/Examples/Experimental/Block/block.json');
 
 inputparams = HomogeneousBlockInputParams(jsonstruct);
+
+gen = HomogeneousBlockGridGenerator();
+inputparams = gen.updateGridInputParams(inputparams);
+
+model = HomogeneousBlock(inputparams);
+
+initstate = model.setupInitialState();
+
+totalTime = 1*minute;
+N = 10;
+
+step = struct('val'    , totalTime/N*ones(N, 1), ...
+              'control', ones(N, 1));
+
+control = struct('src', []);
+
+schedule = struct('step'   , step, ...
+                  'control', control);
+
+cgt = model.cgt;
+cgp = model.cgp;
+
+%% Running the simulation
+
+model.verbose = true;
+[~, states, report] = simulateScheduleAD(initstate, model, schedule);
+
+%% plotting
+
+state = states{end};
+G = model.(el).grid;
+figure
+plotCellData(G, state.(el).phi);
