80c55c02f9ca2a59af93dbe0a3e9a8d9b8bd01a0
Xavier Raynaud <xav.raynaud@gmail.com>
2024-08-20 13:08:35 +0200

    Fix in TabulatedFunction

--- Utilities/FunctionInterface/TabulatedFunction.m
+++ Utilities/FunctionInterface/TabulatedFunction.m
@@ -2,9 +2,6 @@ classdef TabulatedFunction < Function
 % parent class for tabulated function
 
     properties
-
-        numberOfArguments % Number of argument for the tabulated function, at the moment only 1 or 2 are supported
-
         
     end
     
