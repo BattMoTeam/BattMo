e48fa841c8407e9bacd11274299c5028c6b9c9d3
Xavier Raynaud <xav.raynaud@gmail.com>
2024-08-20 14:21:46 +0200

    update in StringExpFunction

--- Utilities/FunctionInterface/StringExpFunction.m
+++ Utilities/FunctionInterface/StringExpFunction.m
@@ -1,36 +1,47 @@
-classdef StringExpFunction
+classdef StringExpFunction < Function
 % The function is given by a string
     
     properties
         
-        stringExpression % The name of the variable should match those given in the argument list 
+        stringExpression % Expression that should be evaluated
+        variableNames % Names of the variables in the expression stringExpression, which corresponds to the order given in the argument list
 
+        %%
+        
+        outputVarName = 'y' % This is the string that will be used to store the value. By default it is 'y' but we have the possibility to change it. NOTE : As the implementation is done now, it should not conflict with other variables.
+        
         %% helper
 
         formattedStringExpression 
-        
     end
 
     methods
 
-        function fn = Function(jsonstruct)
+        function fn = StringExpFunction(jsonstruct)
 
-            fdnames = {'stringExpression'};
+            fn = fn@Function(jsonstruct);
+            
+            fdnames = {'stringExpression', ...
+                       'variableNames'};
 
             fn = dispatchParams(fn, jsonstruct, fdnames);
 
-            nArgs = fn.numberOfArguments;
-            args  = fn.argumentList;
+            varnames = fn.variableNames;
+            nvars    = numel(varnames);
 
-            fmtstr = '';
+            assert(nvars == numel(fn.argumentList), 'The number of variables should be the same as the number of argument in the argument list')
+            
+            fmtstrs  = {};
 
-            for iarg = 1 : nArgs
+            for ivar = 1 : nvars
 
-                fmtstr = sprintf('%s; %s = varargin{%d};', fmtstr, args{iarg}, iarg);
+                fmtstrs{ivar} = sprintf('%s = varargin{%d}', varnames{ivar}, ivar);
                 
             end
 
-            fmtstr = sprintf('%s; y = %s;', fmtstr, fn.stringExpression);
+            fmtstr = join(fmtstrs);
+
+            fmtstr = sprintf('%s; %s = %s;', fmtstr{1}, fn.outputVarName, fn.stringExpression);
 
             fn.formattedStringExpression = fmtstr;
             
