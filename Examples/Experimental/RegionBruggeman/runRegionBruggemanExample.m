621d2f5663a1d9b837f3d2d086feebc78461d0ea
Xavier Raynaud <xav.raynaud@gmail.com>
2024-09-09 13:51:45 +0200

    added runRegionBruggemanExample in test cases

--- Examples/Experimental/RegionBruggeman/runRegionBruggemanExample.m
+++ Examples/Experimental/RegionBruggeman/runRegionBruggemanExample.m
@@ -10,21 +10,37 @@ jsonfilename = fullfile('ParameterData'        , ...
 jsonstruct_material = parseBattmoJson(jsonfilename);
 
 jsonstruct_material.include_current_collectors = true;
+jsonstruct_material.use_thermal = false;
 
 %% Setup geometry
 jsonfilename = fullfile('Examples'     , ...
                         'JsonDataFiles', ...
-                        'geometry3d.json');
+                        'geometry1d.json');
 jsonstruct_geometry = parseBattmoJson(jsonfilename);
 
 %% Setup Control
 jsonfilename = fullfile('Examples', 'JsonDataFiles', 'cc_discharge_control.json');
 jsonstruct_control = parseBattmoJson(jsonfilename);
 
+jsonstruct_timestepping.TimeStepping.numberOfTimeSteps = 40;
+
 jsonstruct = mergeJsonStructs({jsonstruct_bruggeman, ...
                                jsonstruct_geometry , ...
                                jsonstruct_material , ...
-                               jsonstruct_control});
-
+                               jsonstruct_control  , ...
+                               jsonstruct_timestepping});
 
 output = runBatteryJson(jsonstruct);
+
+doplot = true;
+
+if doplot
+
+    states = output.states;
+    E = cellfun(@(state) state.Control.E, states);
+    t = cellfun(@(state) state.time, states);
+
+    figure
+    plot(t, E)
+    
+end
