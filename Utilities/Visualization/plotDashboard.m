907c51f8d2c1af1fd21c4e0ea84457f75bcc9a80
August Johansson <august.johansson@sintef.no>
2024-10-21 10:15:01 +0200

    Plot step in title

--- Utilities/Visualization/plotDashboard.m
+++ Utilities/Visualization/plotDashboard.m
@@ -38,6 +38,7 @@ function [fig] = plotDashboard(model, states, varargin)
     step = p.Results.step;
 
     fig = figure();
+    sgtitle(sprintf('step %g', step))
 
     time = cellfun(@(state) state.time, states);
     Enew = cellfun(@(state) state.(ctrl).E, states);
