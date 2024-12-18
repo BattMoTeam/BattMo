436ae0166f56c6e87522d9185765a26e34866822
Xavier Raynaud <xavier.raynaud@sintef.no>
2024-12-18 13:40:09 +0100

    more in runGasSupply

--- ProtonicMembrane/examples/runGasSupply.m
+++ ProtonicMembrane/examples/runGasSupply.m
@@ -6,17 +6,28 @@
 
 %% json input data
 %
-% We load the input data that is given by json structure
+% We load the input data that is given by json structures. The physical properties of the supply gas layer is given in the json file :battmofile:`gas-supply.json <ProtonicMembrane/jsonfiles/gas-supply.json>` 
 %
 
 filename = fullfile(battmoDir(), 'ProtonicMembrane', 'jsonfiles', 'gas-supply.json');
 jsonstruct_material = parseBattmoJson(filename);
 
+%%
+% The geometry input is loaded from :battmofile:`2d-gas-layer-geometry.json <ProtonicMembrane/jsonfiles/2d-gas-layer-geometry.json>`
+%
 filename = fullfile(battmoDir(), 'ProtonicMembrane', 'jsonfiles', '2d-gas-layer-geometry.json');
 jsonstruct_geometry = parseBattmoJson(filename);
 
+%%
+% The input for the initial state is loaded from :battmofile:`gas-supply-initialization.json <ProtonicMembrane/jsonfiles/gas-supply-initialization.json>`
+%
+
+filename = fullfile(battmoDir(), 'ProtonicMembrane', 'jsonfiles', 'gas-supply-initialization.json');
+jsonstruct_initialization = parseBattmoJson(filename);
+
 jsonstruct = mergeJsonStructs({jsonstruct_material, ...
-                              jsonstruct_geometry});
+                               jsonstruct_geometry, ...
+                               jsonstruct_initialization});
 
 %% Input parameter setup
 %
@@ -50,29 +61,28 @@ G = model.grid;
 plotGrid(G);
 
 %%
-% The boundary conditions have been given by the input json file, see :battmofile:`gas-supply.json <ProtonicMembrane/jsonfiles/gas-supply.json>`
+% The boundary conditions have been given by the input json file, see :battmofile:`gas-supply.json
+% <ProtonicMembrane/jsonfiles/gas-supply.json#16>`. In this case we have rate control at the inlet at the bottom, and
+% pressure control at the outlet at the top.
 %
+% The index of the faces for the inlet and outlet are stored in a :code:`couplingTerm` structure (see :battmo:`here
+% <couplingTerm>` for the base class). Those have been assigned when the grid is setup (for this example we have used
+% :battmo:`GasSupplyGridGenerator2D`).
+%
+% We plot the inlet faces.
 coupTerm = model.couplingTerms{1};
 plotFaces(G, coupTerm.couplingfaces, 'edgecolor', 'blue', 'linewidth', 3)
 
+%%
+% We plot the outlet faces.
 coupTerm = model.couplingTerms{2};
 plotFaces(G, coupTerm.couplingfaces, 'edgecolor', 'red', 'linewidth', 3)
 
-
-return
-
-%% Setupinitial state
+%% Setup initial state
 %
 % The model provides us with a default initialization
 
-initstate = model.setupInitialState();
-
-%%
-% We modify the initial state by changin the initial mass fractions
-
-mfInit = 0.2;
-initstate.massfractions{1}             = mfInit + 0*initstate.massfractions{1};
-initstate.GasSupplyBc.massfractions{1} = mfInit + 0*initstate.GasSupplyBc.massfractions{1};
+initstate = model.setupInitialState(jsonstruct);
 
 %% Schedule setup
 %
@@ -80,7 +90,7 @@ initstate.GasSupplyBc.massfractions{1} = mfInit + 0*initstate.GasSupplyBc.massfr
 % the control to be applied.
 
 T  = 1e-2*second;
-N  = 100;
+N  = 10;
 
 dt = rampupTimesteps(T, T/N, 1);
 
@@ -96,14 +106,42 @@ schedule = struct('control', control, 'step', step);
 % We start the simulation
 [~, states, report] = simulateScheduleAD(initstate, model, schedule);
 
-
-%%
-
+%% Result plots
+% 
+% To analyse the result, we can use the interactive :code:`plotToolbar` command (see :mrstfile:`here <visualization/mrst-gui/plotToolbar.m>`)
+%
 figure
 plotToolbar(model.grid, states);
-% caxis([0.2, 0.4])
-% uit = findobj(gcf, 'Tooltip', 'Freeze caxis');
-% uit.State = 'on';
+%
+%
 
+%%
+% We plot the pressure at the final step.
+%
 
+set(0, 'defaultlinelinewidth', 3);
+set(0, 'defaultaxesfontsize', 15);
+
+state = states{end};
+
+G = model.grid;
+
+figure
+plotCellData(G, state.pressure/barsa);
+colorbar
+title('Pressure / bar');
+
+%%
+% We plot the water mass fractions at three different time step
+
+inds = [1; 5; 10];
+ninds = numel(inds);
+for i = 1 : ninds
+    figure
+    state = states{inds(i)};
+    plotCellData(G, state.massfractions{1}, 'edgecolor', 'none');
+    clim([0.2, 0.4]);
+    title(sprintf('H2O mass fraction\n(timestep:%d)', inds(i)));
+    colorbar
+end
 
