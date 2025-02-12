6d422fe66fa395404f88eb2d14f57d7f2d728ac6
August Johansson <august.johansson@sintef.no>
2025-02-12 23:43:32 +0100

    Adding example mimicking pybamm's control (untested)

---
+++ Examples/Control/runPyBaMMstyle.m
@@ -0,0 +1,17 @@
+clear all
+close all
+
+printer = @(s) disp(jsonencode(s, 'PrettyPrint', true));
+
+experiment = {
+    'Rest for 4000 s';
+    'Discharge at 1 mA until 3.0 V';
+    'Hold at 3.0 V until 1e-4 A';
+    'Charge at 1 A until 4.0 V';
+    'Rest for 1 hour';
+             };
+
+jsonstruct = convertPyBaMMtoJson(experiment);
+printer(jsonstruct);
+
+writeJsonStruct(jsonstruct, 'test.json');
