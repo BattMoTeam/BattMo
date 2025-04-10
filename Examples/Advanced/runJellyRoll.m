e94c37edbb96f6050e340673c81ca5ada390e3ee
Xavier Raynaud <xavier.raynaud@sintef.no>
2025-04-10 17:25:25 +0200

    clean-up in jellyroll example

--- Examples/Advanced/runJellyRoll.m
+++ Examples/Advanced/runJellyRoll.m
@@ -1,276 +1,51 @@
-if mrstPlatform('octave')
-    error('This demo cannot be run from Octave since it does not support the linear solvers');
-end
-
 clear
 close all
 
-% mrstDebug(20);
+%% We load the geometrical parameters for a jellyroll (4680 model)
 
-% Setup mrst modules
-mrstModule add ad-core mrst-gui mpfa agmg linearsolvers
-
-%% We setup the geometrical parameters for a 4680 battery.
-%% Those will be gathered in structure spiralparams (see below) and used by SpiralBatteryGenerator to generate the spiral layered geometry of the jelly roll
-
-% Inner radius of the jelly roll
-rInner = 2*milli*meter;
-
-% widths of each component ordered as
-% - positive current collector
-% - positive electrode
-% - electrolyte separator
-% - negative electrode
-% - negative current collector
-
-widths = [25, 64, 15, 57, 15]*micro*meter;
-
-widthDict = containers.Map(...
-    {'Separator'               , ...
-     'NegativeCoating'         , ...
-     'NegativeCurrentCollector', ...
-     'PositiveCoating'         , ...
-     'PositiveCurrentCollector'}, ...
-    widths);
-
-nwidths = [widthDict('PositiveCoating')          ;...
-           widthDict('PositiveCurrentCollector') ;...
-           widthDict('PositiveCoating')          ;...
-           widthDict('Separator')                ;...
-           widthDict('NegativeCoating')          ;...
-           widthDict('NegativeCurrentCollector') ;...
-           widthDict('NegativeCoating')          ;...
-           widthDict('Separator')];
-
-dr = sum(nwidths);
-
-% Outer radius of the jelly roll
-rOuter = 22.36*milli*meter;
-% Height of the jelly roll
-L = 80*milli*meter;
-
-dR = rOuter - rInner;
-% Computed number of windings
-nwindings = ceil(dR/dr);
-
-% number of discretization cells in radial direction for each component.
-nrDict = containers.Map({'Separator'               , ...
-                         'NegativeCoating'         , ...
-                         'NegativeCurrentCollector', ...
-                         'PositiveCoating'         , ...
-                         'PositiveCurrentCollector'}, ...
-                        [3, 3, 3, 3, 3]);
-
-% Number of discretization cells in the angular direction
-nas = 10;
-
-% Number of discretization cells in the longitudonal
-nL = 3;
-
-% refinement parameter for the top and bottom faces
-refLcoef = 8;
-
-% structure that describes the tab setups (see SpiralBatteryGenerator)
-tabparams.tabcase   = 'aligned tabs';
-tabparams.width     = 3*milli*meter;
-tabparams.fractions = linspace(0.01, 0.9, 6);
+jsonstruct_geometry = parseBattmoJson(fullfile('Examples', 'JsonDataFiles', '4680-geometry.json'));
 
 testing = true;
 if testing
+    % We setup a smaller model for quicker testing
     fprintf('We setup a smaller case for quicker testing\n');
-    rOuter = rInner + 1*milli*meter;
-    dR = rOuter - rInner;
-    % Computed number of windings
-    nwindings = ceil(dR/dr);
-    nL = 2;
+    rOuter = jsonstruct_geometry.Geometry.innerRadius + 1*milli*meter;
+    jsonstruct_geometry.Geometry.outerRadius                         = rOuter;
+    jsonstruct_geometry.Geometry.numberOfDiscretizationCellsVertical =  2;
+
+    tabparams = struct('usetab', false);
+    jsonstruct_geometry.NegativeElectrode.CurrentCollector.tabparams = tabparams;
+    jsonstruct_geometry.PositiveElectrode.CurrentCollector.tabparams = tabparams;
+    
 end
 
-spiralparams = struct('nwindings'   , nwindings, ...
-                      'rInner'      , rInner   , ...
-                      'widthDict'   , widthDict, ...
-                      'nrDict'      , nrDict   , ...
-                      'nas'         , nas      , ...
-                      'L'           , L        , ...
-                      'nL'          , nL       , ...
-                      'refLcoef'    , refLcoef , ...
-                      'tabparams'   , tabparams, ...
-                      'angleuniform', true);
 
-% The input material parameters given in json format are used to populate the inputparams object.
-jsonstruct = parseBattmoJson(fullfile('ParameterData','BatteryCellParameters','LithiumIonBatteryCell','lithium_ion_battery_nmc_graphite.json'));
+%% We load material parameters
 
-% We define some shorthand names for simplicity.
-ne      = 'NegativeElectrode';
-pe      = 'PositiveElectrode';
-elyte   = 'Electrolyte';
-thermal = 'ThermalModel';
-co      = 'Coating';
-am      = 'ActiveMaterial';
-itf     = 'Interface';
-sd      = 'SolidDiffusion';
-ctrl    = 'Control';
-cc      = 'CurrentCollector';
+jsonstruct_material = parseBattmoJson(fullfile('ParameterData'        , ...
+                                               'BatteryCellParameters', ...
+                                               'LithiumIonBatteryCell', ...
+                                               'lithium_ion_battery_nmc_graphite.json'));
+
+jsonstruct_material = removeJsonStructField(jsonstruct_material, {'include_current_collectors'});
+
+%% We load the control parameters
+
+jsonstruct_control = parseBattmoJson(fullfile('Examples', 'JsonDataFiles', 'cc_discharge_control.json'));
+
+%% We merge all the parameters
+
+jsonstruct = mergeJsonStructs({jsonstruct_material, ...
+                               jsonstruct_geometry, ...
+                               jsonstruct_control});
 
 jsonstruct.include_current_collectors = true;
 
-jsonstruct.use_thermal = true;
-
-diffusionModelType = 'full';
-
-jsonstruct.(pe).(co).(am).diffusionModelType = diffusionModelType;
-jsonstruct.(ne).(co).(am).diffusionModelType = diffusionModelType;
-
-simcase = 'CCDischarge';
-
-jsonstruct.(ctrl).controlPolicy = simcase;
-
-inputparams = BatteryInputParams(jsonstruct);
-
-% inputparams.(ne).(am).(sd).N = 5;
-% inputparams.(pe).(am).(sd).N = 5;
-
-DRate = 0.1;
-inputparams.(ctrl).lowerCutoffVoltage = 3;
-inputparams.(ctrl).DRate              = DRate;
-inputparams.(ctrl).rampupTime         = 0.1/DRate;
-
-% th = 'ThermalModel';
-% inputparams.(th).externalHeatTransferCoefficientSideFaces = 100*watt/meter^2;
-% inputparams.(th).externalHeatTransferCoefficientTopFaces = 10*watt/meter^2;
-
-gen = SpiralBatteryGenerator();
-
-inputparams = gen.updateBatteryInputParams(inputparams, spiralparams);
-
-model = GenericBattery(inputparams);
-
-%% Setup schedule
-
-DRate = model.(ctrl).DRate;
-fac   = 2;
-total = 1.1*hour/DRate;
-n     = 10;
-dt0   = total*1e-6;
-times = getTimeSteps(dt0, n, total, fac);
-
-% times = times(times < 4200);
-
-%% We compute the cell capacity, which used to compute schedule from DRate
-
-step = struct('val', diff(times), 'control', ones(numel(times) - 1, 1));
-
-control = model.(ctrl).setupScheduleControl();
-
-schedule  = struct('control', control, 'step', step);
-
-switch simcase
-
-  case 'CCDischarge'
-
-    %% We setup the initial state
-    initstate = model.setupInitialState();
-    initElytec = 1*mol/litre;
-    initstate.(elyte).c = initElytec*ones(model.(elyte).G.getNumberOfCells(), 1);
-
-  case 'CCCharge'
-
-    model.SOC = 0.01;
-    initstate = model.setupInitialState();
-
-  otherwise
-
-    error('simcase not recognized')
-
-end
-
-%% Setup the properties of the nonlinear solver
-nls = NonLinearSolver();
-
-clear setup
-
-casenumber = 1;
-beVerbose = false;
-
-switch casenumber
-
-  case 1
-
-    jsonstruct_solver = parseBattmoJson('Utilities/Linearsolvers/JsonDataFiles/default_direct_linear_solver.json');
-
-  case 2
-
-    switch model.(ne).(co).(am).diffusionModelType
-
-      case  'full'
-        
-        jsonstruct_solver = parseBattmoJson('Utilities/Linearsolvers/JsonDataFiles/default_linear_solver_setup.json');
-        
-        if ~beVerbose
-            
-            jsonstruct_solver.NonLinearSolver.verbose = false;
-            jsonstruct_solver.NonLinearSolver.LinearSolver.linearSolverSetup.verbose = 0;
-            prcs = jsonstruct_solver.NonLinearSolver.LinearSolver.linearSolverSetup.preconditioners;
-            for iprc = 1 : numel(prcs)
-                prcs(iprc).solver.verbose = 0;
-                prcs(iprc).solver.solver.verbose = false;
-            end
-            jsonstruct_solver.NonLinearSolver.LinearSolver.linearSolverSetup.preconditioners = prcs;
-
-        end
-            
-      case 'simple'
-        
-        % jsonstruct_solver = parseBattmoJson('Utilities/Linearsolvers/JsonDataFiles/default_linear_solver_setup_simple_diffusion.json');
-        jsonstruct_solver = parseBattmoJson('Utilities/Linearsolvers/JsonDataFiles/temp.json');
-        
-      otherwise
-
-        error('diffusionModelType not recognized');
-        
-    end
-    
-  otherwise
-
-    error('case number not recognized');
-
-end
-
-
-[model, nls] = setupNonLinearSolverFromJson(model, jsonstruct_solver);
-
-model.verbose = true;
-
-dopacked = false;
-
-if dopacked
-    
-    % Run simulation
-    dataFolder = 'BattMo';
-    problem = packSimulationProblem(initstate, model, schedule, dataFolder, ...
-                                    'Name'           , 'jellyroll', ...
-                                    'NonLinearSolver', nls);
-    problem.SimulatorSetup.OutputMinisteps = true;
-
-    clearSimulation = true;
-    if clearSimulation
-        %% clear previously computed simulation
-        clearPackedSimulatorOutput(problem, 'prompt', false);
-    end
-    simulatePackedProblem(problem);
-    [globvars, states, reports] = getPackedSimulatorOutput(problem);
-
-else
-
-    % fn = afterStepConvergencePlots(nls);
-
-    [~, states, reports] = simulateScheduleAD(initstate, model, schedule, ...
-                                              'NonLinearSolver', nls);
-
-    reports = reports.ControlstepReports;
-
-end
+output = runBatteryJson(jsonstruct);
 
 %% Process output and recover the output voltage and current from the output states.
+
+states = output.states;
 
 E = cellfun(@(x) x.Control.E, states);
 I = cellfun(@(x) x.Control.I, states);
