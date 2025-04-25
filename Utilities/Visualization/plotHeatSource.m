e130deb1f6b2cf9db609ae3c64aacae0359d3dad
August Johansson <august.johansson@sintef.no>
2025-04-25 12:45:06 +0200

    Minor updates to temperature plots

--- Utilities/Visualization/plotHeatSource.m
+++ Utilities/Visualization/plotHeatSource.m
@@ -1,21 +1,24 @@
-function [] = plotHeatSource(model,states,varargin)
+function plotHeatSource(model,states,varargin)
 %UNTITLED7 Summary of this function goes here
 %   Detailed explanation goes here
 
-if model.G.griddim == 2
-    xmin = min(model.ThermalModel.G.nodes.coords(:,1));
-    xmax = max(model.ThermalModel.G.nodes.coords(:,1));
-    ymin = min(model.ThermalModel.G.nodes.coords(:,2));
-    ymax = max(model.ThermalModel.G.nodes.coords(:,2));
-    ylim([ymin, ymax]);
-    xlim([xmin, xmax]);
-    
-    xlabel('X Position  /  m')
-    ylabel('Y Position  /  m')
-end
+    if model.grid.griddim == 2
+        xmin = min(model.ThermalModel.G.nodes.coords(:,1));
+        xmax = max(model.ThermalModel.G.nodes.coords(:,1));
+        ymin = min(model.ThermalModel.G.nodes.coords(:,2));
+        ymax = max(model.ThermalModel.G.nodes.coords(:,2));
+        ylim([ymin, ymax]);
+        xlim([xmin, xmax]);
 
-figureHeatSource = plotCellData(model.ThermalModel.G, states.ThermalModel.jHeatSource);
-figureHeatSource.EdgeColor = 'none';
+        xlabel('X Position  /  m')
+        ylabel('Y Position  /  m')
+    end
+
+    figureHeatSource = plotCellData(model.ThermalModel.grid, states.ThermalModel.jHeatSource);
+
+    if model.grid.griddim > 1
+        figureHeatSource.EdgeColor = 'none';
+    end
 
 end
 
