2ceda9cb2dedabc91afdc8a6544703f531142e8b
Manuel Mota <victor.mota@polytechnique.edu>
2025-04-29 16:46:21 +0200

    Article 3 modification

--- Electrochemistry/LithiumPlating.m
+++ Electrochemistry/LithiumPlating.m
@@ -1,11 +1,11 @@
 classdef LithiumPlating < BaseModel
 
     properties
-
-        alphaC = 0.5
-        alphaA = 0.5
-        kLi = 1e-9
-        
+        alpha = 0.5          % Symmetry factor for Butler-Volmer equation
+        T = 298.15           % Temperature [K]
+        U = 0;               % Open Circuit Voltage (OCV)
+        kLi = 1e-10;         % Reaction rate constant [A m^-2 (mol m^-3)^-1.5]
+        cmax = 1000;         % Maximum lithium concentration [mol/m³]
     end
 
     methods
@@ -14,11 +14,11 @@ classdef LithiumPlating < BaseModel
 
             model = model@BaseModel();
             
-            % fdnames = {'alphaC', ...
-            %            'alphaA', ...
-            %            'kLi'};
-
-            % model = dispatchParams(model, inputparams, fdnames);
+            fdnames = {'alpha', ...
+                       'kLi', ...
+                       'U', ...
+                       'cmax'};
+            model = dispatchParams(model, inputparams, fdnames);
             
         end
 
@@ -43,9 +43,51 @@ classdef LithiumPlating < BaseModel
 
             fn = @LithiumPlating.updateFlux; 
             model = model.registerPropFunction({'flux', fn, {'phiElectrode', 'phiElectrolyte', 'cElectrolyte', 'concentration'}});
-            
         end
-    end
-    
-end
 
+        function state = updateFlux(model, state)
+            
+            F = model.constants.F; 
+            R = model.constants.R;
+            T = model.T;
+
+            phi_s = state.phiElectrode;
+            phi_e = state.phiElectrolyte;
+
+            ce = state.cElectrolyte;
+            cs = state.concentration;
+
+            U = model.U;
+
+            eta = phi_s - phi_e - U; % Overpotential
+
+            k0 = model.kLi;
+            alpha = model.alpha;
+            cmax = model.cmax;
+
+            %exchange current density j0
+            j0 = k0 .* sqrt(ce) .* sqrt(cs) .* sqrt(cmax - cs);
+
+            %butler-Volmer equation (current density, A/m²)
+            j = j0 .* (exp((alpha * F * eta) / (R * T)) - exp((-(1 - alpha) * F * eta) / (R * T)));
+
+            %convert to molar flux (mol/m²/s) by dividing by Faraday constant
+            flux = j ./ F;
+
+            state.flux = flux;
+        end
+
+        function state = updateMassAccum(model, state, state0, dt)
+            state.massAccum = (state.concentration - state0.concentration) ./ dt;         
+        end
+
+        function state = updateMassCons(model, state)
+            flux = state.flux;
+            accum = state.massAccum;
+            cons = assembleConservationEquation(model, flux, 0, 0, accum);
+            state.massCons = cons;
+        end
+    
+    end
+
+end
