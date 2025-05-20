6a6f656857bd2e145a57a5d8fa7f96296ae2171c
Xavier Raynaud <xavier.raynaud@sintef.no>
2025-05-20 14:31:28 +0200

    get original runActiveMaterial

--- Examples/AM_standalone/runActiveMaterial.m
+++ Examples/AM_standalone/runActiveMaterial.m
@@ -1,15 +1,17 @@
 %% run stand-alone active material model
 
+% clear the workspace and close open figures
 clear
 close all
 
 %% Import the required modules from MRST
+% load MRST modules
 mrstModule add ad-core mrst-gui mpfa
 
 %% Setup the properties of Li-ion battery materials and cell design
-
 jsonstruct = parseBattmoJson(fullfile('ParameterData','BatteryCellParameters','LithiumIonBatteryCell','lithium_ion_battery_nmc_graphite.json'));
 
+% We define some shorthand names for simplicity.
 ne      = 'NegativeElectrode';
 pe      = 'PositiveElectrode';
 elyte   = 'Electrolyte';
@@ -22,28 +24,24 @@ ctrl    = 'Control';
 cc      = 'CurrentCollector';
 
 jsonstruct.use_thermal = false;
+
 jsonstruct.include_current_collectors = false;
 
 jsonstruct.(ne).(co).(am).diffusionModelType = 'full';
 jsonstruct.(pe).(co).(am).diffusionModelType = 'full';
 
-jsonstruct.(ne).(co).(am).useLithiumPlating = true;
-
-% Flag pour modèle stand-alone
 jsonstruct.(ne).(co).(am).isRootSimulationModel = true;
 
-% Setup InputParams
 inputparams = BatteryInputParams(jsonstruct);
+
 inputparams = inputparams.(ne).(co).(am);
 
-%% Setup the model
-
 model = ActiveMaterial(inputparams);
-
-%% Equip model for simulation
 model = model.setupForSimulation();
 
 %% Setup initial state
+
+% shortcuts
 
 sd  = 'SolidDiffusion';
 itf = 'Interface';
@@ -52,49 +50,37 @@ cElectrolyte   = 5e-1*mol/litre;
 phiElectrolyte = 0;
 T              = 298;
 
-cElectrodeInit = (model.(itf).guestStoichiometry100)*(model.(itf).saturationConcentration);
+cElectrodeInit   = (model.(itf).guestStoichiometry100)*(model.(itf).saturationConcentration);
+
+% set primary variables
 N = model.(sd).N;
 initState.(sd).c        = cElectrodeInit*ones(N, 1);
 initState.(sd).cSurface = cElectrodeInit;
 
+% set static variable fields
 initState.T = T;
 initState.(itf).cElectrolyte   = cElectrolyte;
 initState.(itf).phiElectrolyte = phiElectrolyte;
 
 initState = model.evalVarName(initState, {itf, 'OCP'});
+
 OCP = initState.(itf).OCP;
 initState.E = OCP + phiElectrolyte;
 
-if model.useLithiumPlating
-    lp = 'LithiumPlating';
-    F = model.LithiumPlating.F;
-    R = model.LithiumPlating.R;
-    
-    nPl0 = model.LithiumPlating.nPl0;
-    r = model.LithiumPlating.particleRadius;
-    vsa = model.LithiumPlating.volumetricSurfaceArea;
-
-    platedConcentration0 = nPl0 * vsa / (4 * pi * r^2);
-
-    initState.(lp).platedConcentration = platedConcentration0/(exp((F*OCP)/(R*T)) - 1)^(1/4);
-    initState.(lp).phiSolid       = initState.E;
-    initState.(lp).phiElectrolyte = phiElectrolyte;
-    initState.(lp).cElectrolyte   = cElectrolyte;
-    initState.(lp).surfaceCoverage = 0; 
-    
-end
-
 %% setup schedule
 
+% Reference rate which roughly corresponds to 1 hour for the data of this example
 Iref = 5e-12;
-Imax = 5e1*Iref;
+
+Imax = 2e1*Iref;
+
 total = 1*hour*(Iref/Imax);
 n     = 100;
 dt    = total/n;
 step  = struct('val', dt*ones(n, 1), 'control', ones(n, 1));
 
-tup = 1*second*(Iref/Imax);
-srcfunc = @(time) rampupControl(time, tup, Imax); %0 pour tourner à vide
+tup = 1*second*(Iref/Imax); % rampup value for the current function, see rampupSwitchControl
+srcfunc = @(time) rampupControl(time, tup, Imax);
 
 cmin = (model.(itf).guestStoichiometry0)*(model.(itf).saturationConcentration);
 control.stopFunction = @(model, state, state0_inner) (state.(sd).cSurface <= cmin);
@@ -106,11 +92,10 @@ schedule = struct('control', control, 'step', step);
 
 nls = NonLinearSolver();
 nls.errorOnFailure = false;
+
 model.nonlinearTolerance = 1e-2;
 
 %% Run simulation
-% model.G = cartGrid([1, 1]);  % grille fictive 1x1 si pas de spatialisation
-% model.G = computeGeometry(model.G); !!!
 
 model.verbose = true;
 [~, states, report] = simulateScheduleAD(initState, model, schedule, 'OutputMinisteps', true, 'NonLinearSolver', nls);
@@ -128,13 +113,11 @@ figure
 plot(time/hour, cSurface/(1/litre));
 xlabel('time [hour]');
 ylabel('Surface concentration [mol/L]');
-title('Surface concentration');
 
 figure
 plot(time/hour, E);
 xlabel('time [hour]');
 ylabel('Potential [mol/L]');
-title('Potential difference');
 
 cmin = cellfun(@(state) min(state.(sd).c), states);
 cmax = cellfun(@(state) max(state.(sd).c), states);
@@ -145,64 +128,41 @@ end
 
 caver = cellfun(@(state) max(state.(sd).cAverage), states);
 
-% figure
-% hold on
-% plot(time/hour, cmin /(mol/litre), 'displayname', 'cmin');
-% plot(time/hour, cmax /(mol/litre), 'displayname', 'cmax');
-% plot(time/hour, caver/(mol/litre), 'displayname', 'total concentration');
-% title('Concentration in particle / mol/L')
-% xlabel('time [hour]');
-% ylabel('Concentration [mol/L]');
-% legend show
-% 
-% c = states{end}.(sd).c;
-% r = linspace(0, model.(sd).particleRadius, model.(sd).N);
-% 
-% figure
-% plot(r, c/(mol/litre));
-% xlabel('radius / m')
-% ylabel('concentration / mol/L')
-% title('Particle concentration profile (last time step)')
+figure
+hold on
+plot(time/hour, cmin /(mol/litre), 'displayname', 'cmin');
+plot(time/hour, cmax /(mol/litre), 'displayname', 'cmax');
+plot(time/hour, caver/(mol/litre), 'displayname', 'total concentration');
+title('Concentration in particle / mol/L')
+legend show
 
-%% Lithium plating plotting
+c = states{end}.(sd).c;
+r = linspace(0, model.(sd).particleRadius, model.(sd).N);
 
-if model.useLithiumPlating
-    lp = 'LithiumPlating';
+figure
+plot(r, c/(mol/litre));
+xlabel('radius / m')
+ylabel('concentration / mol/L')
+title('Particle concentration profile (last time step)')
 
-    varsToEval = { ...
-        {'LithiumPlating', 'surfaceCoverage'}, ...
-        {'LithiumPlating', 'platingFlux'}, ...
-        {'LithiumPlating', 'chemicalFlux'}, ...
-        {'LithiumPlating', 'etaPlating'}, ...
-        {'LithiumPlating', 'etaChemical'} ...
-    };
-    for k = 1:numel(states) %!!!
-        for iv = 1:numel(varsToEval)
-            states{k} = model.evalVarName(states{k}, varsToEval{iv});
-        end
-    end
 
-    surfaceCoverage = cellfun(@(s) s.(lp).surfaceCoverage, states);
-    platingFlux     = cellfun(@(s) s.(lp).platingFlux, states);
-    chemicalFlux    = cellfun(@(s) s.(lp).chemicalFlux, states);
-    platedConcentration             = cellfun(@(s) s.(lp).platedConcentration, states);
 
-    figure
-    plot(time/hour, surfaceCoverage);
-    xlabel('time [hour]');
-    ylabel('Surface coverage');
-    title('Surface Coverage of Plated Lithium');
+%{
+Copyright 2021-2024 SINTEF Industry, Sustainable Energy Technology
+and SINTEF Digital, Mathematics & Cybernetics.
 
-    figure
-    plot(time/hour, platingFlux*model.(itf).volumetricSurfaceArea);
-    xlabel('time [hour]');
-    ylabel('Volumetric Plating Flux [mol/m³/s]');
-    title('Lithium Plating Flux');
+This file is part of The Battery Modeling Toolbox BattMo
 
-    figure
-    plot(time/hour, platedConcentration);
-    xlabel('time [hour]');
-    ylabel('platedConcentration [mol/m²]');
-    title('Accumulated Plated Lithium');
+BattMo is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-end
+BattMo is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BattMo.  If not, see <http://www.gnu.org/licenses/>.
+%}
