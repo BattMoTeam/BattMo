BattMoTeam/BattMo

commit 92f47ee8af2a8b90d444cbca36dd8870a5d1da5c

fixing bug in mass source (wrong direction and surfcov applied 2 times) - Fixing scaling for masscons

Electrochemistry/ActiveMaterial.m
@@ -228,7 +228,7 @@ classdef ActiveMaterial < BaseModel
 
             
             
-            scalings = {{{sd, 'massCons'}, I}                  , ...
+            scalings = {{{sd, 'massCons'}, I/(vf*n*F)}                  , ...
                         {{sd, 'solidDiffusionEq'}, I}, ...
                         {{'chargeCons'}, I}       , ...
                        };
@@ -308,7 +308,7 @@ classdef ActiveMaterial < BaseModel
 
             coef = cElyte.*c;
             
-            th = cmax * 1e-12;
+            th = cmax * 1e-10;
 
             coef(coef < 0) = 0;
             state.(itf).j0 =  model.(lp).kInter*regularizedSqrt(coef, th)*n*F;
@@ -351,7 +351,7 @@ classdef ActiveMaterial < BaseModel
             rp  = model.(sd).particleRadius;
             vsa = model.(itf).volumetricSurfaceArea;
             
-            vp = 4/3*pi*rp^3;
+            vp = (4/3)*pi*rp^3;
 
             interFlux   = state.(itf).intercalationFlux;
             I           = state.I;
@@ -381,9 +381,9 @@ classdef ActiveMaterial < BaseModel
             theta    = state.(lp).surfaceCoverage;
             
             Rvol    = op.mapFromBc*Rvol;
-            chemVol = op.mapFromBc*(vsa*chemFlux);
+            chemVol = op.mapFromBc*(vsa*chemFlux*theta);
             
-            state.(sd).massSource = - ((1 - theta)*Rvol + theta*chemVol).*((4*pi*rp^3)./(3*vf));
+            state.(sd).massSource = (Rvol - chemVol).*((4*pi*rp^3)./(3*vf));
 
         end
         

Examples/Advanced/Plating/runPlating.m
@@ -120,7 +120,8 @@ if model.useLithiumPlating
     r                    = model.LithiumPlating.particleRadius;
     vf                   = model.LithiumPlating.volumeFraction;
     platedConcentration0 = nPl0 * vf / ((4/3)*pi*r^3);
-
+    
+    %initialisation so that overpotential are =0 at the beginning
     platedConcentrationInit = platedConcentration0/(exp((F*OCP)/(R*T)) - 1)^(1/4);
 
     model.(lp).platedConcentrationRef = platedConcentrationInit;
@@ -145,7 +146,7 @@ tup = 1*second*(Iref/Imax);
 
 switch scenario
   case 'charge'
-    srcfunc = @(time) rampupControl(time, tup, -Imax); %0 pour tourner à vide
+    srcfunc = @(time) 0 %rampupControl(time, tup, -Imax); %0 pour tourner à vide
     cmax = (model.(itf).guestStoichiometry100)*(model.(itf).saturationConcentration);
     control.stopFunction = @(model, state, state0_inner) (state.(sd).cSurface >= cmax);
   case 'discharge'

Electrochemistry/LithiumPlatingLatz.m
@@ -66,24 +66,6 @@ classdef LithiumPlatingLatz < BaseModel
                        'useSEI'};
             model = dispatchParams(model, inputparams, fdnames);
 
-            % % Extract physical constants
-            % F = model.F;
-            % R = model.R;
-            % T = model.T;
-            % OCP = inputparams.OCP;
-            % 
-            % % Compute the reference plated concentration based on theoretical initial value
-            % n0 = model.nPl0;
-            % vf = model.volumeFraction;
-            % r = model.particleRadius;
-            % 
-            % % Compute baseline concentration for one particle volume [mol/m^3]
-            % platedConcentration0 = n0 * vf / ((4/3) * pi * r^3);
-            % platedConcentrationInit = platedConcentration0 / (exp((F * OCP) / (R * T)) - 1)^(1/4);
-            % 
-            % % Assign to model property
-            % model.platedConcentrationRef = platedConcentrationInit;
-            
         end
 
         function model = registerVarAndPropfuncNames(model)

commit 596fd1a21422158ee02a6b8d8baa9873e6b9b92b

fixing a bug introduced in 92f47ee8af2a8b90d444cbca36dd8870a5d1da5c (wrong direction of interFlux)

Electrochemistry/ActiveMaterial.m
@@ -383,7 +383,7 @@ classdef ActiveMaterial < BaseModel
             Rvol    = op.mapFromBc*Rvol;
             chemVol = op.mapFromBc*(vsa*chemFlux*theta);
             
-            state.(sd).massSource = (Rvol - chemVol).*((4*pi*rp^3)./(3*vf));
+            state.(sd).massSource = -(Rvol - chemVol).*((4*pi*rp^3)./(3*vf));
 
         end
         

Examples/Advanced/Plating/runPlating.m
@@ -146,7 +146,7 @@ tup = 1*second*(Iref/Imax);
 
 switch scenario
   case 'charge'
-    srcfunc = @(time) 0 %rampupControl(time, tup, -Imax); %0 pour tourner à vide
+    srcfunc = @(time) rampupControl(time, tup, -Imax); %0 pour tourner à vide
     cmax = (model.(itf).guestStoichiometry100)*(model.(itf).saturationConcentration);
     control.stopFunction = @(model, state, state0_inner) (state.(sd).cSurface >= cmax);
   case 'discharge'