0359e433512ad70e36dff19c415ec9b019571251
August Johansson <august.johansson@sintef.no>
2025-08-08 16:51:50 +0200

    Change NMC111 to LCO

--- ParameterData/MaterialProperties/LCO/computeOCP_LCO.m
+++ ParameterData/MaterialProperties/LCO/computeOCP_LCO.m
@@ -1,9 +1,11 @@
-function [OCP, dUdT] = computeOCP_NMC111(c, T, cmax)
-    
+function [OCP, dUdT] = computeOCP_LCO(c, T, cmax)
+
+    % From Ramadass et al 2004. 10.1149/1.1634273
+
     Tref = 298.15;  % [K]
 
     theta = c./cmax;
-    
+
     coeff1_refOCP = [ -4.656   , ...
                       0        , ...
                       + 88.669 , ...
@@ -15,7 +17,7 @@ function [OCP, dUdT] = computeOCP_NMC111(c, T, cmax)
                       - 462.471, ...
                       0        , ...
                       + 433.434];
-    
+
     coeff2_refOCP =[ -1      , ...
                      0       , ...
                      + 18.933, ...
@@ -27,27 +29,27 @@ function [OCP, dUdT] = computeOCP_NMC111(c, T, cmax)
                      - 73.083, ...
                      0       , ...
                      + 95.960];
-    
-    refOCP = polyval(coeff1_refOCP(end:-1:1),theta)./ polyval(coeff2_refOCP(end:-1:1),theta);    
-    
+
+    refOCP = polyval(coeff1_refOCP(end:-1:1),theta)./ polyval(coeff2_refOCP(end:-1:1),theta);
+
     % Calculate the entropy change at the given lithiation
-    
+
     coeff1_dUdT = [0.199521039        , ...
                    - 0.928373822      , ...
                    + 1.364550689000003, ...
                    - 0.611544893999998];
-    
+
     coeff2_dUdT = [1                  , ...
                    - 5.661479886999997, ...
-                   + 11.47636191      , ... 
+                   + 11.47636191      , ...
                    - 9.82431213599998 , ...
                    + 3.048755063];
-    
+
     dUdT = -1e-3.*polyval(coeff1_dUdT(end:-1:1),theta)./ polyval(coeff2_dUdT(end:-1:1),theta);
-    
+
     % Calculate the open-circuit potential of the active material
     OCP = refOCP + (T - Tref) .* dUdT;
-    
+
 end
 
 
