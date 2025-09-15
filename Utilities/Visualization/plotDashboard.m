7ba766f79124f6f5fcafb94b3e6cb45fdc18a310
August Johansson <august.johansson@sintef.no>
2025-09-15 17:14:53 +0200

    Separate axis scaling fcn

--- Utilities/Visualization/plotDashboard.m
+++ Utilities/Visualization/plotDashboard.m
@@ -682,18 +682,6 @@ function setGcaStyle(ax, style)
 end
 
 
-function scaleAxis(ax, dim, scaling)
-
-    % dim is 'x', 'y', or 'z'
-
-    dim = upper(dim);
-    tick = get(ax, sprintf('%sTick', dim));
-    tickvals = tick * scaling;
-    ticklabel = arrayfun(@num2str, tickvals, 'un', false);
-    set(ax, sprintf('%sTickLabel', dim), ticklabel);
-
-end
-
 %{
   Copyright 2021-2024 SINTEF Industry, Sustainable Energy Technology
   and SINTEF Digital, Mathematics & Cybernetics.
