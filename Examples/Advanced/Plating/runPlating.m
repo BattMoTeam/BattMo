097937c12140ed09475ece5201c8f251c1fa39ba
Xavier Raynaud <xavier.raynaud@sintef.no>
2025-09-22 21:09:57 +0200

    clean-up in runPlating

--- Examples/Advanced/Plating/runPlating.m
+++ Examples/Advanced/Plating/runPlating.m
@@ -7,9 +7,15 @@ close all
 
 %% Setup the properties of Li-ion battery materials and cell design
 
-jsonstruct = parseBattmoJson(fullfile('ParameterData','BatteryCellParameters','LithiumIonBatteryCell','lithium_ion_battery_nmc_graphite.json'));
+filename = fullfile('ParameterData'        , ...
+                    'BatteryCellParameters', ...
+                    'LithiumIonBatteryCell', ...
+                    'lithium_ion_battery_nmc_graphite.json');
 
-inputparams = BatteryInputParams(jsonstruct);
+jsonstruct = parseBattmoJson(filename);
+
+%%
+% We define some shortcuts
 
 ne      = 'NegativeElectrode';
 pe      = 'PositiveElectrode';
@@ -31,7 +37,10 @@ jsonstruct.(pe).(co).(am).diffusionModelType = 'full';
 
 jsonstruct.(ne).(co).(am).useLithiumPlating = true;
 
+%%
 % Flag for stand-alone model
+%
+
 jsonstruct.(ne).(co).(am).isRootSimulationModel = true;
 
 jsonstruct_lithium_plating = parseBattmoJson(fullfile('Examples', 'Advanced', 'Plating', 'lithium_plating.json'));
@@ -41,6 +50,7 @@ jsonstruct.(ne).(co).(am).LithiumPlating = jsonstruct_lithium_plating.LithiumPla
 scenario = 'charge';
 
 %% following is not used at particle level
+%
 
 switch scenario
   case 'charge'
@@ -51,21 +61,25 @@ switch scenario
     error('scenario not recognized');
 end
 
-
+%%
 % Setup InputParams
+
 inputparams = BatteryInputParams(jsonstruct);
 inputparams = inputparams.(ne).(co).(am);
 
-%OCP is computed via a function described in the article (S-5)
+% OCP is computed via a function described in the article (S-5)
 inputparams.Interface.openCircuitPotential.functionname = 'computeOCP_Graphite_Latz';
 
 %% Setup the model
 
 model = ActiveMaterial(inputparams);
 
-%% Equip model for simulation
+%%
+% We equip the model for simulation
+%
 
 model = model.setupForSimulation();
+model.verbose = true;
 
 %% Setup initial state
 
@@ -78,7 +92,6 @@ T              = 298;
 
 switch scenario
   case 'charge'
-    % cElectrodeInit = (model.(itf).guestStoichiometry0)*(model.(itf).saturationConcentration);
     cElectrodeInit = 30*mol/litre;
   case 'discharge'
     cElectrodeInit = (model.(itf).guestStoichiometry100)*(model.(itf).saturationConcentration);
@@ -87,47 +100,46 @@ switch scenario
 end
 
 N = model.(sd).N;
-initState.(sd).c        = cElectrodeInit*ones(N, 1);
-initState.(sd).cSurface = cElectrodeInit;
+initstate.(sd).c        = cElectrodeInit*ones(N, 1);
+initstate.(sd).cSurface = cElectrodeInit;
 
-initState.T = T;
-initState.(itf).cElectrolyte   = cElectrolyte;
-initState.(itf).phiElectrolyte = phiElectrolyte;
+initstate.T = T;
+initstate.(itf).cElectrolyte   = cElectrolyte;
+initstate.(itf).phiElectrolyte = phiElectrolyte;
 
-initState = model.evalVarName(initState, {itf, 'OCP'});
-OCP = initState.(itf).OCP;
-initState.E = OCP + phiElectrolyte;
-
+initstate = model.evalVarName(initstate, {itf, 'OCP'});
+OCP = initstate.(itf).OCP;
+initstate.E = OCP + phiElectrolyte;
 
 F = model.(itf).constants.F;
 R = model.(itf).constants.R;
 
 if model.useLithiumPlating
+    
     nPl0                 = model.LithiumPlating.nPl0;
     r                    = model.LithiumPlating.particleRadius;
     vf                   = model.LithiumPlating.volumeFraction;
     platedConcentration0 = nPl0 * vf / ((4/3)*pi*r^3);
     
-    %initialisation so that overpotential are = 0 and reaction at equilibrium
-    %at the beginning
+    %%
+    % initialisation so that the overpotential are zero at the beginning
     platedConcentrationInit = platedConcentration0/(exp((F*OCP)/(R*T)) - 1)^(1/4);
 
     model.(lp).platedConcentrationRef = platedConcentrationInit;
 
-    initState.(lp).platedConcentrationNorm = platedConcentrationInit / model.(lp).platedConcentrationRef;
-    initState.(lp).phiSolid            = initState.E;
-    initState.(lp).phiElectrolyte      = phiElectrolyte;
-    initState.(lp).cElectrolyte        = cElectrolyte;
-    initState.(lp).nSEI                = 0;
+    initstate.(lp).platedConcentrationNorm = platedConcentrationInit / model.(lp).platedConcentrationRef;
+    initstate.(lp).phiSolid            = initstate.E;
+    initstate.(lp).phiElectrolyte      = phiElectrolyte;
+    initstate.(lp).cElectrolyte        = cElectrolyte;
+    initstate.(lp).nSEI                = 0;
 end
 
 %% setup schedule
-%This part is essential to see the lithium plating effect. Increasing Iref
-%strengthen the effect.
+% This part is essential to see the lithium plating effect. Increasing Iref strengthens the effect.
 
 Iref = 7e-13;
 Imax = Iref;
-total = 6e-3*hour*(Iref/Imax); %total time of the charge
+total = 6e-3*hour*(Iref/Imax); % total time of the charge
 n     = 400;
 dt    = total/n;
 step  = struct('val', dt*ones(n, 1), 'control', ones(n, 1));
@@ -147,64 +159,65 @@ switch scenario
     error('scenario not recognized');
 end
 
-% uncomment to make the simulation run without current (debugging)
-% srcfunc = @(time) 0;
-
 control.src = srcfunc;
 
 schedule = struct('control', control, 'step', step);
 
 scalingparams = struct('I'                  , Imax                              , ...
-                       'elyteConcentration' , initState.(itf).cElectrolyte);
+                       'elyteConcentration' , initstate.(itf).cElectrolyte);
 
 if model.useLithiumPlating
     scalingparams.platedConcentration = platedConcentrationInit;
 end
 
+%%
+% We setup the scaling of the parameters
+%
 
 model = model.setupScalings(scalingparams);
 
-%% setup non-linear solver
 
-nls = NonLinearSolver();
-nls.errorOnFailure  = false;
-nls.maxTimestepCuts = 20;
+%% Run first simulation, particle charge
+%
 
-model.nonlinearTolerance = 1e-6;
+inputSim = struct('model'    , model   , ...
+                  'schedule' , schedule, ...
+                  'initstate', initstate);
+simsetup = SimulationSetup(inputSim);
 
-%% Run simulation by charging the particle
-
-model.verbose = true;
-[~, states, report] = simulateScheduleAD(initState, model, schedule, 'OutputMinisteps', true, 'NonLinearSolver', nls);
+states = simsetup.run();
 
 %%
-% Then discharge it
-%
-% We use the last state of the previous simulation to initialise the new one
-initstate = states{end};
-jsonstruct.(ctrl).DRate = -1;
-jsonstruct.Control.controlPolicy = 'CCDischarge';
-
-% And we save the charging states for later
+% W save the charging states for later
 
 chargeStates = states;
 
-% Setup schedule
+%% Setup discharge simulation
+% 
+%
+% We use the last state of the previous simulation to initialise the new one
 
-% Set up a StopFunction if necessary
-% Control.stopFunction = @(model, state, state0_inner) (1 == 0);
+simsetup.initstate = states{end};
+
+jsonstruct.(ctrl).DRate = -1;
+jsonstruct.Control.controlPolicy = 'CCDischarge';
+
 
 srcfunc = @(time) rampupControl(time, tup, Imax);
 
 control.src = srcfunc;
 
-schedule = struct('control', control, 'step', step);
+simsetup.schedule = struct('control', control, 'step', step);
 
-% Run simulation
+%% Run second simulation, particle discharge
+%
 
-[~, states, report] = simulateScheduleAD(initstate, model, schedule, 'OutputMinisteps', true, 'NonLinearSolver', nls);
 
-% And concatenate the states
+states = simsetup.run();
+
+%%
+% We concatenate the two phases
+
 
 dischargeStates = states;
 states = vertcat(chargeStates, dischargeStates);
