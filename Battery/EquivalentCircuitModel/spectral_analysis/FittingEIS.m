96f329a6f334b2ca8e3987561bffa8c036cc8190
Xavier Raynaud <xavier.raynaud@sintef.no>
2026-05-06 14:31:23 +0200

    some clean-up in FittingEIS

--- Battery/EquivalentCircuitModel/spectral_analysis/FittingEIS.m
+++ Battery/EquivalentCircuitModel/spectral_analysis/FittingEIS.m
@@ -1,14 +1,14 @@
-classdef FittingEIS < handle
+classdef FittingEIS
 
     properties
-       params0
-       Z_re_exp
-       Z_im_exp
-       omega
-       scales
-       
-       best_params_found = [] 
-       fitting_error = []
+        params0
+        Z_re_exp
+        Z_im_exp
+        omega
+        scales
+        
+        % best_params_found = [] 
+        % fitting_error = []
 
     end
 
@@ -23,10 +23,11 @@ classdef FittingEIS < handle
             feis.Z_re_exp = Z_re_exp;
             feis.Z_im_exp = Z_im_exp;
             feis.omega    = omega;
+            
         end
 
 
-        function [min_value, best_params, history] = optimizationBFGS(feis)
+        function [min_value, history, best_params, fitting_error] = optimizationBFGS(feis)
 
             f_opt = @(p_norm) feis.optifunc(p_norm.* feis.scales) ;
             
@@ -43,21 +44,16 @@ classdef FittingEIS < handle
                 'maxIt', 300, ...               
                 'objChangeTol', 1e-6, ...       
                 'gradTol', 1e-5 ...
-            );
+                                                                );
             best_params = best_params_norm(:) .* feis.scales(:);
 
-            feis.best_params_found = best_params;
-            [feis.fitting_error, ~] = feis.optifunc(best_params);
-            
+            fitting_error = feis.optifunc(best_params);
+
         end
 
-        function plotresults(feis)
+        function plotresults(feis, best_params, fitting_error)
 
-            if isempty(feis.best_params_found)
-                feis.optimizationBFGS();
-            end
-            
-            [Z_re_fit, Z_im_fit] = load_nyquist(feis.best_params_found, feis.omega);  
+            [Z_re_fit, Z_im_fit] = load_nyquist(best_params, feis.omega);  
             
             figure;
             subplot(3,1,1);
@@ -90,39 +86,33 @@ classdef FittingEIS < handle
             
             grid on;
             
-            text_error = sprintf('Fitting error : %.2e', feis.fitting_error);
+            text_error = sprintf('Fitting error : %.2e', fitting_error);
             
             subplot(3,1,3);
             
             text(0.05, 0.90, text_error, 'Units', 'normalized', ...
-                'BackgroundColor', 'white', ...   
-                'EdgeColor', 'black', ...        
-                'FontSize', 11, ...               
-                'FontWeight', 'bold');
+                 'BackgroundColor', 'white', ...   
+                 'EdgeColor', 'black', ...        
+                 'FontSize', 11, ...               
+                 'FontWeight', 'bold');
         end
 
-        function GiveResults(feis)
-            if isempty(feis.best_params_found)
-                feis.optimizationBFGS();
-            end
-
+        function printResults(feis, best_params, fitting_error)
 
             fprintf('\n=== FITTING SCORE ===\n');
-            fprintf('Error : %e\n', feis.fitting_error);
+            fprintf('Error : %e\n', fitting_error);
             
             fprintf('\n=== PARAMETERS FOUND ===\n');
-            fprintf('R0 = %.5f Ohms\n', feis.best_params_found(1));
-            fprintf('R1 = %.5f Ohms\n', feis.best_params_found(2));
-            fprintf('C1 = %.1f Farads\n', feis.best_params_found(3));
-            fprintf('R2 = %.5f Ohms\n', feis.best_params_found(4));
-            fprintf('C2 = %.1f Farads\n', feis.best_params_found(5));
+            fprintf('R0 = %.5f Ohms\n', best_params(1));
+            fprintf('R1 = %.5f Ohms\n', best_params(2));
+            fprintf('C1 = %.1f Farads\n', best_params(3));
+            fprintf('R2 = %.5f Ohms\n', best_params(4));
+            fprintf('C2 = %.1f Farads\n', best_params(5));
             
-
         end
 
-
         function [v, g_norm] = optifunc(feis, p)
-                   
+            
             [Z_re_param, Z_Im_param] = load_nyquist(p, feis.omega);
             
             Module_Z = sqrt(feis.Z_re_exp.^2 + feis.Z_im_exp.^2);
@@ -132,26 +122,24 @@ classdef FittingEIS < handle
             err_im = (feis.Z_im_exp(:) - Z_Im_param(:)) ./ Module_Z(:);
             
             v = sum(err_re.^2 + err_im.^2);
-        
-        
-        if nargout > 1
+            
+            if nargout > 1
+                
                 w  = feis.omega(:);
                 R0 = p(1);
                 R1 = p(2);
                 C1 = p(3);
                 R2 = p(4);
                 C2 = p(5);
-            
-            
+                
                 g_re_dR0 = -ones(size(w)); 
                 g_re_dR1 = -(1-(R1*C1.*w).^2) ./ (1+(R1*C1.*w).^2).^2;  
                 g_re_dC1 = (2*C1*R1^3.*w.^2) ./ (1+(R1*C1.*w).^2).^2;  
                 g_re_dR2 = -(1-(R2*C2.*w).^2) ./ (1+(R2*C2.*w).^2).^2;  
                 g_re_dC2 = (2*C2*R2^3.*w.^2) ./ (1+(R2*C2.*w).^2).^2;
-        
+                
                 J_re = [g_re_dR0, g_re_dR1, g_re_dC1, g_re_dR2, g_re_dC2];
                 
-        
                 g_im_dR0 = zeros(size(w));
                 g_im_dR1 = -(2*R1*C1.*w) ./ (1+(R1*C1.*w).^2).^2;  
                 g_im_dC1 = -(R1^2.*w - C1^2*R1^4.*w.^3) ./ (1+(R1*C1.*w).^2).^2;  
@@ -161,12 +149,11 @@ classdef FittingEIS < handle
                 J_im = [g_im_dR0, g_im_dR1, g_im_dC1, g_im_dR2, g_im_dC2];
                 derr_re_dp = J_re ./ Module_Z; 
                 derr_im_dp = J_im ./ Module_Z;
-            
-        
+                
                 g_true = 2 * (derr_re_dp' * err_re) + 2 * (derr_im_dp' * err_im);
                 g_norm = g_true .* feis.scales(:);
+                
+            end
         end
-        end
-
     end
 end
