c22c5c05d0b1becd180144377345aa800effd172
August Johansson <august.johansson@sintef.no>
2026-08-07 12:09:51 +0200

    use tighter estimate

--- Control/GenericControlModel.m
+++ Control/GenericControlModel.m
@@ -1,29 +1,29 @@
 classdef GenericControlModel < ControlModel
 
     properties
-        
+
         controlsteps % cell array, each cell describing a control (see GenericControlModel.schema.json)
-        
+
     end
-    
+
     methods
-        
+
         function model = GenericControlModel(inputparams)
-            
+
             model = model@ControlModel(inputparams);
-            
+
             fdnames = {'controlsteps'};
             model = dispatchParams(model, inputparams, fdnames);
 
             model = model.parseControlSteps();
             model = model.setupTerminationFunctions();
-            
+
         end
-        
+
         function model = registerVarAndPropfuncNames(model)
-            
+
             model = registerVarAndPropfuncNames@ControlModel(model);
-            
+
             varnames = {};
             % Control type : string that can take following value
             % - 'rest'
@@ -38,49 +38,49 @@ classdef GenericControlModel < ControlModel
             varnames{end + 1} = 'ctrlSwitchTime';
             % Time spanned since beginning
             varnames{end + 1} = 'time';
-            
+
             model = model.registerVarNames(varnames);
-            
+
             model = model.setAsStaticVarNames({'ctrlStepIndex', 'ctrlSwitchTime'});
-            
+
             % Register the functions
             fn = @GenericControlModel.updateControlType;
             model = model.registerPropFunction({'ctrlType', fn, {'ctrlStepIndex'}});
-            
+
             fn = @GenericControlModel.updateControlTime;
             model = model.registerPropFunction({'ctrlTime', fn, {'time', 'ctrlSwitchTime'}});
 
             fn = @GenericControlModel.updateControlEquation;
             model = model.registerPropFunction({'controlEquation', fn, {'ctrlType', 'E', 'I', 'ctrlTime'}});
-            
+
         end
-        
+
         function cleanState = addStaticVariables(model, cleanState, state)
-            
+
             cleanState.ctrlStepIndex  = state.ctrlStepIndex;
             cleanState.ctrlSwitchTime = state.ctrlSwitchTime;
 
         end
-        
+
         function newstate = addVariablesAfterConvergence(model, newstate, state)
 
             newstate.ctrlTime = state.ctrlTime;
-            
+
         end
 
 
         function model = parseControlSteps(model)
 
             model.controlsteps = GenericControlModel.unfoldCycle(model.controlsteps);
-            
+
         end
 
-        
+
         function model = setupTerminationFunctions(model)
-            
+
         % For each control step termination, setup the function that determines if the termination condition is fulfilled. This
         % function is stored under controlstep.termination.function.
-            
+
             controlsteps = model.controlsteps;
 
             for ictrl = 1 : numel(controlsteps)
@@ -89,9 +89,9 @@ classdef GenericControlModel < ControlModel
             end
 
             model.controlsteps = controlsteps;
-            
+
         end
-        
+
         function schedule = setupSchedule(model, jsonstruct)
 
             if nargin > 1 && isfield(jsonstruct, 'TimeStepping')
@@ -115,12 +115,15 @@ classdef GenericControlModel < ControlModel
             else
                 params = [];
             end
+            params = model.parseTimeSteppingStruct(params);
 
             controlsteps = model.controlsteps;
             dts = cellfun(@(ctrlstep) ctrlstep.timeStepSize, controlsteps);
 
             if isAssigned(params, {'totalTime'})
                 totalTime = params.totalTime;
+                % With a global totalTime, future generic control switches are not known when the schedule is
+                % constructed. Take the min(dts) for safety.
                 dt = min(dts);
                 nsteps = ceil(totalTime/dt);
                 vals = repmat(dt, nsteps, 1);
@@ -136,8 +139,7 @@ classdef GenericControlModel < ControlModel
                         ctrlvals = repmat(dt, nsteps, 1);
                         ctrlvals(end) = termination.value - sum(ctrlvals(1:end-1));
                     else
-                        nsteps = 1000;
-                        ctrlvals = repmat(dt, nsteps, 1);
+                        ctrlvals = repmat(dt, params.numberOfTimeSteps, 1);
                     end
                     vals = [vals; ctrlvals]; %#ok<AGROW>
                 end
@@ -169,14 +171,14 @@ classdef GenericControlModel < ControlModel
             state = model.updateValueFromControl(state);
 
         end
-        
+
         function [dt, done, currControl] = getTimeStep(model, itstep, schedule, state)
         % Returns the current time-step and control. Each control step stores its time step size.
 
             ctrlsteps = model.controlsteps;
 
             ctrlind   = state.ctrlStepIndex;
-            
+
             if ctrlind > length(ctrlsteps)
                 done = true;
                 dt = [];
@@ -187,123 +189,123 @@ classdef GenericControlModel < ControlModel
             dt = ctrlsteps{ctrlind}.timeStepSize;
             done = false;
             currControl = 1;
-            
+
         end
-        
+
         function state = updateControlType(model, state)
-            
+
         % Fetch control step index from state, then from this control step, we get the control type. Assign this control
         % type to the state.
             istep    = model.getControlStep(state);
             ctrlstep = model.controlsteps{istep};
-            
+
             state.ctrlType = ctrlstep.controltype;
-            
+
         end
 
         function state = updateControlTime(model, state)
 
         % Control time is the time spent since last control switch
-            
+
             state.ctrlTime = state.time - state.ctrlSwitchTime;
-            
+
         end
-        
+
         function [isCtrlDone, isLastControl] = isControlTerminated(model, state, state0)
 
         % check if control is terminated or if it is the last control step. For that is uses the termintation.function
         % that we setup earlier (preprocsessing). It is called in updateControlState, at the end of each Newton step,
         % and also to check if simulation is terminated in triggerSimulationEnd.
-            
+
             if ~isfield(state, 'ctrlType')
                 state = model.updateControlType(state);
             end
             ctrlType  = state.ctrlType;
-            
+
             [ictrlstep, isLastControl] = model.getControlStep(state);
-            
+
             ctrlstep = model.controlsteps{ictrlstep};
             termination = ctrlstep.termination;
-            
+
             isCtrlDone = termination.function(state);
 
         end
-        
+
         function state = updateControlState(model, state, state0, dt)
-            
+
         % This function is called by the solver at the end of each Newton step.  We check if we have triggered the
         % "termination" criteria from the control. If the termination criteria is met, we increment the control step by
         % one, and proceed with the Newton algorithm. This may create control switches that are too early, because when
         % we have not converged, the termination condition can be met but it does not mean that it will be the case at
         % convergence.
-            
+
             [isCtrlDone, isLastControl] = model.isControlTerminated(state, state0);
-            
+
             if isCtrlDone && ~isLastControl
                 state.ctrlStepIndex  = state.ctrlStepIndex + 1;
                 state.ctrlSwitchTime = state.time;
                 state = model.updateValueFromControl(state);
             end
-            
+
         end
 
-        
+
         function  [arefulfilled, state] = checkConstraints(model, state, state0, dt)
-            
+
         % This function is called when the Newton method has converged, but we want to check if the solution we have
         % obtained does not break the termination criteria. For the moment, we do implement this check, which may lead
         % to control switches that come too early.
-            
+
         end
-        
+
         function doend = triggerSimulationEnd(model, state, state0_inner, drivingForces)
 
         % Returns true if the simulation should end. Called in getTimeStep.
             [isCtrlDone, isLastControl] = model.isControlTerminated(state, state0_inner);
-            
+
             doend = (isCtrlDone && isLastControl);
-            
+
         end
-        
+
         function state = updateValueFromControl(model, state)
-            
+
         % From the given control step, we get the control type which determines which value should be set (current and
         % voltage) and also provides the numerical value. We update the state using those two.
-            
+
             ictrlstep   = model.getControlStep(state);
             controlstep = model.controlsteps{ictrlstep};
             ctrlType    = controlstep.controltype;
-            
+
             state.ctrlType = ctrlType;
-            
+
             switch ctrlType
-                
+
               case 'rest'
-                
+
                 state.I = 0;
-                
+
               case 'current'
-                
+
                 givenI = controlstep.value;
                 state.I = givenI;
-                
+
               case 'voltage'
-                
+
                 givenE = controlstep.value;
                 state.E = givenE;
-                
+
               otherwise
-                
+
                 error('control type not recognized');
-                
+
             end
-            
+
         end
 
         function [ictrlstep, isLastControl] = getControlStep(model, state)
 
         % Utility function to get control step index and, in addition, check if it is the last control step.
-            
+
             ctrlsteps = model.controlsteps;
             ictrlstep = state.ctrlStepIndex;
 
@@ -313,46 +315,46 @@ classdef GenericControlModel < ControlModel
             end
 
         end
-        
+
         function state = updateControlEquation(model, state)
 
         % Assemble the control equation and add it to state.
-            
+
             ctrlType  = state.ctrlType;
             ictrlstep = state.ctrlStepIndex;
-            
+
             controlstep = model.controlsteps{ictrlstep};
-            
+
             switch ctrlType
-                
+
               case 'rest'
-                
+
                 ctrleq = state.I;
-                
+
               case 'current'
-                
+
                 givenI = controlstep.value;
                 if strcmp(controlstep.direction, 'charge')
                     givenI = -givenI;
                 end
                 ctrleq = state.I - givenI;
-                
+
               case 'voltage'
-                
+
                 givenE = controlstep.value;
-                
+
                 ctrleq = (state.E - givenE)*1e5;
-                
+
               otherwise
-                
+
                 error('control type not recognized');
-                
+
             end
-            
+
             state.controlEquation = ctrleq;
-            
+
         end
-        
+
     end
 
     methods (Static)
@@ -371,11 +373,11 @@ classdef GenericControlModel < ControlModel
                 isdone = abs(val) > refval;
               otherwise
                 error('comparison string not recognized')
-                
+
             end
 
         end
-        
+
         function termination = setupComparisonFunction(termination)
         % return a function that takes state as argument and returns true if the termination condition is fullfilled.
 
@@ -391,7 +393,7 @@ classdef GenericControlModel < ControlModel
                     error('termination quantity not recognized')
                 end
             end
-            
+
             switch termination.quantity
               case 'current'
                 tI = termination.value;
@@ -413,10 +415,10 @@ classdef GenericControlModel < ControlModel
             for icontrol = 1 : numel(controlsteps)
 
                 control = controlsteps{icontrol};
-                
+
                 if strcmp(control.controltype, 'cycle')
 
-                    ncycle  = control.numberOfcycles;
+                    ncycle  = control.numberOfCycles;
                     control = control.cycleControlSteps;
                     control = GenericControlModel.unfoldCycle(control);
                     control = repmat(control, ncycle, 1);
@@ -424,7 +426,7 @@ classdef GenericControlModel < ControlModel
                 end
 
             end
-            
+
         end
     end
 
