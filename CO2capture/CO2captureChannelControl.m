72fc2c014b1993c1ac8d41a0706fff181d73adab
Xavier Raynaud <xavier.raynaud@sintef.no>
2026-08-12 21:01:57 +0200

    added missing files

---
+++ CO2capture/CO2captureChannelControl.m
@@ -0,0 +1,19 @@
+classdef CO2captureChannelControl < BaseModel
+
+    properties
+
+    end
+
+    methods
+        
+        function model = CO2captureChannelControl(inputparams)
+            
+            model = model@BaseModel();
+            
+        end
+
+                
+    end
+
+end
+    
---
+++ CO2capture/CO2captureControl.m
@@ -0,0 +1,22 @@
+classdef CO2captureControl < BaseModel
+
+    properties
+
+    end
+
+    methods
+        
+        function model = CO2captureChannelControl(inputparams)
+            
+            model = model@BaseModel();
+            
+        end
+
+        function model = setupSchedule()
+
+        end
+                
+    end
+
+end
+    
