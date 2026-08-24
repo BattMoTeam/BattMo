3af7674e1d169da94ac59af9a47f3ba10cca1244
August Johansson <august.johansson@sintef.no>
2026-08-24 14:53:02 +0200

    Update copyright years

--- Documentation/utils/publishExamplesXML.m
+++ Documentation/utils/publishExamplesXML.m
@@ -100,7 +100,7 @@ function run_publish(filename, opt)
 end
 
 %{
-Copyright 2009-2021 SINTEF Digital, Mathematics & Cybernetics.
+Copyright 2009-2026 SINTEF Digital, Mathematics & Cybernetics.
 
 This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
 
