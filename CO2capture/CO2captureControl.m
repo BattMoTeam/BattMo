382bfa1de856b1806181713d442e268ef98c38de
Xavier Raynaud <xavier.raynaud@sintef.no>
2026-09-23 16:42:22 +0200

    work on CO2capture control model

--- CO2capture/CO2captureControl.m
+++ CO2capture/CO2captureControl.m
@@ -1,22 +1,176 @@
 classdef CO2captureControl < BaseModel
-
-    properties
-
-    end
-
+%%
+% Class is taken from ControlModel
+    
     methods
-        
-        function model = CO2captureChannelControl(inputparams)
-            
+
+        function model = CO2captureControl(inputparams)
+
             model = model@BaseModel();
             
+            % fdnames = {};
+            % model = dispatchParams(model, inputparams, fdnames);
+            
         end
 
-        function model = setupSchedule()
-
+        function cleanState = addStaticVariables(model, cleanState, state)
+        % If the control model includes some static variable (that should be initialized at each Newton step), they can be added here.
+        % Base class behaviour is do nothing.
         end
+
+        function state = prepareStepControl(model, state, state0, dt)
+        % Attach to state the values necessary for the control. This is run only once at the beginning of a time step
+        % Base class behaviour is do nothing.
+        end
+        
+        function state = updateControlEquation(model, state)
+        % Implemented by child model
+        % Base class behaviour is do nothing.
+            state.controlEquation = [];
+        end
+        
+        function state = updateControlState(model, state, state0, dt)
+        % Implemented by child model.
+        % Base class behaviour is do nothing.
+        % Called after each Newton iteration
+            
+        end
+        
+        function state = updateControlAfterConvergence(model, state, state0, dt)
+        % This function is called in updateAfterConvergence, after Newton method has converged.
+        % Base class behaviour is do nothing.
                 
-    end
+        end
 
-end
+
+        function func = setupStopFunction(model)
+        % setup and return a "stop function" for the given control, with signature
+        % func = @(model, state, state_prev) ();
+        % Default is no stop function (the simulation ends at the last given time step)
+
+            func = @(model, state, state_prev) (false);
+            
+        end
+
+        function arefulfilled = checkConstraints(model, state)
+        % Method that can be implemented to check if the constraints that may be part of the control are fulfilled
+
+        % No default behaviour is implemented here
+
+        end
+
+
+        function func = setupControlFunction(model)
+        % Setup and return a "control function". We do not require a special signature for this function. The user
+        % should instead make sure the method :code:`updateControl` in  :code:`Battery` cover the implemented control policy.
+
+            func = [];
+            
+        end
+
+        function step = setupScheduleStep(model, timeSteppingParams)
+
+        % Setup and a return the step structure that is part of the schedule which is used as input for
+        % :mrst:`simulateScheduleAD`. For some control type, there is a natural construction for this structure. This is
+        % why we include this method here, for convenience. It can be overloaded by derived classes. The
+        % timeSteppingParams structure by default is given by the data described in :battmofile:`Utilities/JsonSchemas/TimeStepping.schema.json`
+
+            if (nargin > 1)
+                params = timeSteppingParams;
+            else
+                params = [];
+            end
+
+            params = ControlModel.parseTimeSteppingStruct(params);
+
+            totalTime = getStructField(params, 'totalTime');
+
+            assert(isAssigned(totalTime), 'totalTime must be given');
+
+            if isAssigned(params, {'timeStepDuration'})
+                dt = params.timeStepDuration;
+            else
+                assert(isAssigned(params, 'numberOfTimeSteps'), 'No timeStepDuration and numberOfTimeSteps are given');
+                n  = params.numberOfTimeSteps;
+                dt = totalTime/n;
+            end
+
+            dts = rampupTimesteps(totalTime, dt, 0);
+
+            step = struct('val', dts, 'control', ones(numel(dts), 1));
+
+        end
+
+        function control = setupScheduleControl(model)
+        % Setup and return the control structure that is sent to :mrst:`simulateScheduleAD`
+            
+            control.Control.stopFunction = model.setupStopFunction();
+            control.src                  = model.setupControlFunction();
+            
+        end
+
+        function schedule = setupSchedule(model, jsonstruct)
+        % Convenience function to setup schedule from main jsonstruct with property TimeStepping
+            
+            if nargin > 1 && isfield(jsonstruct, 'TimeStepping')
+                timeSteppingParams = jsonstruct.TimeStepping;
+            else
+                timeSteppingParams = [];
+            end
+
+            step    = model.setupScheduleStep(timeSteppingParams);
+            control = model.setupScheduleControl();
+
+            schedule = struct('step', step, ...
+                              'control', control);
+            
+        end
+
+        function doend = triggerSimulationEnd(model, state, state0_inner, drivingForces)
+            
+            doend = drivingForces.stopFunction(model, state, state0_inner);
+        
+        end
     
+        function [dt, done, currControl] = getTimeStep(model, itstep, schedule, state)
+        % Returns the current time-step and control index
+
+            done = (itstep > numel(schedule.step.val));
+
+            if done
+                dt = [];
+                currControl = [];
+            else
+                % Get the current time-step
+                dt          = schedule.step.val(itstep);
+                currControl = schedule.step.control(itstep);
+            end
+            
+        end
+        
+    end
+    
+        
+end
+
+
+
+%{
+Copyright 2021-2026 SINTEF Industry, Sustainable Energy Technology
+and SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The Battery Modeling Toolbox BattMo
+
+BattMo is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+BattMo is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with BattMo.  If not, see <http://www.gnu.org/licenses/>.
+%}
